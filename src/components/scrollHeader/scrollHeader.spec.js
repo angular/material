b8072093ece9c4266184a0e3b234e34a46c17fc4
Max Lynch <max@drifty.com>
2014-07-08 16:36:05 -0500

    test(scrollHeader): test fix for scrollHeader

--- src/components/scrollHeader/scrollHeader.spec.js
+++ src/components/scrollHeader/scrollHeader.spec.js
@@ -7,16 +7,15 @@ describe('materialScrollHeader', function() {
     inject(function($compile, $document, $rootScope) {
       el = $compile('<div><material-toolbar class="material-theme-light-blue material-medium-tall" scroll-header condensed-height="60"><material-content></material-content></div>')($rootScope.$new());
       $rootScope.$apply();
-      $document[0].body.appendChild(el[0]);
     });
     return el;
   }
 
   describe('directive', function() {
-    iit('Should have attribute', function() {
+    it('Should have attribute', function() {
       var el = setup('');
       var toolbar = el[0].querySelector('material-toolbar');
-      expect(el[0].hasAttribute('scroll-header')).toBe(true);
+      expect(toolbar.hasAttribute('scroll-header')).toBe(true);
     });
   });
 
