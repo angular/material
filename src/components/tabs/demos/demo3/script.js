130eea440961904ef3f23b10b234287b1a2f73c3
Thomas Burleson <ThomasBurleson@Gmail.com>
2014-07-10 16:04:52 -0500

    Demo formatting improvements

--- src/components/tabs/demos/demo3/script.js
+++ src/components/tabs/demos/demo3/script.js
@@ -1,11 +1,10 @@
-
-angular.module('app', ['ngMaterial'] )
-  .controller('AppCtrl', function( $scope ) {
+angular.module('app', ['ngMaterial'])
+  .controller('AppCtrl', function ($scope) {
     var tabs = [
-      { title: 'Polymer', active: true,  disabled: false, content:"Polymer practices are great!" },
-      { title: 'Material', active: false, disabled: true , content:"Material Design practices are better!" },
-      { title: 'Angular', active: false, disabled: true , content:"AngularJS practices are the best!" },
-      { title: 'NodeJS' , active: false, disabled: false, content:"NodeJS practices are amazing!" }
+      { title: 'Polymer', active: true, disabled: false, content: "Polymer practices are great!" },
+      { title: 'Material', active: false, disabled: true, content: "Material Design practices are better!" },
+      { title: 'Angular', active: false, disabled: true, content: "AngularJS practices are the best!" },
+      { title: 'NodeJS', active: false, disabled: false, content: "NodeJS practices are amazing!" }
     ];
 
     $scope.which = 0;
--- src/components/tabs/demos/demo4/script.js
+++ src/components/tabs/demos/demo4/script.js
@@ -1,11 +1,10 @@
-
-angular.module('app', ['ngMaterial'] )
-  .controller('AppCtrl', function( $scope ) {
+angular.module('app', ['ngMaterial'])
+  .controller('AppCtrl', function ($scope) {
     var tabs = [
-      { title: 'Polymer', active: true,  disabled: false, content:"Polymer practices are great!" },
-      { title: 'Material', active: false, disabled: true , content:"Material Design practices are better!" },
-      { title: 'Angular', active: false, disabled: true , content:"AngularJS practices are the best!" },
-      { title: 'NodeJS' , active: false, disabled: false, content:"NodeJS practices are amazing!" }
+      { title: 'Polymer', active: true, disabled: false, content: "Polymer practices are great!" },
+      { title: 'Material', active: false, disabled: true, content: "Material Design practices are better!" },
+      { title: 'Angular', active: false, disabled: true, content: "AngularJS practices are the best!" },
+      { title: 'NodeJS', active: false, disabled: false, content: "NodeJS practices are amazing!" }
     ];
 
     $scope.tabs = tabs;
@@ -18,50 +17,43 @@ angular.module('app', ['ngMaterial'] )
     $scope.announceSelected = announceSelected;
     $scope.announceDeselected = announceDeselected;
 
-    $scope.addTab = function( title, view )
-    {
+    $scope.addTab = function (title, view) {
       view = view || title + " Content View";
-      tabs.push( { title : title, content: view, active:false, disabled: false});
+      tabs.push({ title: title, content: view, active: false, disabled: false});
     };
 
-    $scope.removeTab =function( tab )
-    {
-      for(var j=0; j<tabs.length; j++ )
-      {
-        if ( tab.title == tabs[j].title )
-        {
-          $scope.tabs.splice(j,1);
+    $scope.removeTab = function (tab) {
+      for (var j = 0; j < tabs.length; j++) {
+        if (tab.title == tabs[j].title) {
+          $scope.tabs.splice(j, 1);
           break;
         }
       }
     }
 
-    $scope.submit = function($event)
-    {
-      if ( $event.which !== 13 ) return;
-      if ( $scope.tTitle != "" )
-      {
-        $scope.addTab( $scope.tTitle, $scope.tContent );
+    $scope.submit = function ($event) {
+      if ($event.which !== 13) return;
+      if ($scope.tTitle != "") {
+        $scope.addTab($scope.tTitle, $scope.tContent);
       }
     }
-
 
     // **********************************************************
     // Private Methods
     // **********************************************************
 
-    function onTabSelected( tab ) {
+    function onTabSelected(tab) {
       $scope.selectedIndex = this.$index;
 
-      $scope.announceSelected( tab );
+      $scope.announceSelected(tab);
     }
 
-    function announceDeselected( tab ) {
-      $scope.farewell = supplant( "Goodbye {title}!", tab );
+    function announceDeselected(tab) {
+      $scope.farewell = supplant("Goodbye {title}!", tab);
     }
 
-    function announceSelected( tab ) {
-      $scope.greeting = supplant( "Hello {title}!", tab );
+    function announceSelected(tab) {
+      $scope.greeting = supplant("Hello {title}!", tab);
     }
 
   });
