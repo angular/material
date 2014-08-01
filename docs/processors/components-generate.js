3b5aaa5110c25e9688a179e773d07dd46f039400
Andrew <andytjoslin@gmail.com>
2014-08-01 14:15:24 -0600

    docs(): support `@private` tag

--- docs/processors/components-generate.js
+++ docs/processors/components-generate.js
@@ -112,6 +112,7 @@ module.exports = {
 
     function processDocs(component, docType, docs) {
       _(docs)
+        .omit('private')
         .map(function(doc) {
           return _.pick(doc, [
             'description',
