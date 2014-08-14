76423c36c388c49b3f4fcf3ececaa6227df13b33
Andrew <andytjoslin@gmail.com>
2014-08-14 08:59:18 -0600

    test(throttle): fix some broken tests

--- src/services/throttle/throttle.spec.js
+++ src/services/throttle/throttle.spec.js
@@ -1,18 +1,18 @@
-describe('ngThrottleSpec', function() {
+ddescribe('ngThrottleSpec', function() {
 
   var $animate, body, $rootElement, $throttle, $timeout;
 
-  beforeEach(module('material.services.throttle'));
+  beforeEach(module('material.services.throttle', 'material.animations'));
 
   beforeEach(inject(function(_$throttle_, _$timeout_, _$animate_, _$rootElement_, $document ){
-        $throttle = _$throttle_;
-        $timeout = _$timeout_;
+    $throttle = _$throttle_;
+    $timeout = _$timeout_;
 
-        $animate = _$animate_;
-        body = angular.element($document[0].body);
-        $rootElement = _$rootElement_;
+    $animate = _$animate_;
+    body = angular.element($document[0].body);
+    $rootElement = _$rootElement_;
 
-        body.append($rootElement);
+    body.append($rootElement);
   }));
 
   describe('use $throttle with no configurations', function() {
@@ -74,8 +74,8 @@ describe('ngThrottleSpec', function() {
 
     it("should start but NOT end if throttle does not complete",function() {
       var startFn = function(){ started = true;},
-          endFn = function(){ ended = true;};
-          lockFn = function(done){ ; },  // do not callback for completion
+      endFn = function(){ ended = true;};
+      lockFn = function(done){ ; },  // do not callback for completion
 
 
       $throttle({start:startFn, throttle:lockFn, end:endFn})();
@@ -93,17 +93,17 @@ describe('ngThrottleSpec', function() {
     it("should process n-times correctly",function() {
       var wanted="The Hulk";
       var sCount=0, title="",
-        startFn = function(){
-          sCount++;
-        },
-        processFn = function(text, done){
-          title += text;
-          if ( title == wanted ) {
-            // Conditional termination of throttle phase
-            done();
-          }
-        };
-        concat = $throttle({start:startFn, throttle:processFn})( );
+      startFn = function(){
+        sCount++;
+      },
+      processFn = function(text, done){
+        title += text;
+        if ( title == wanted ) {
+          // Conditional termination of throttle phase
+          done();
+        }
+      };
+      concat = $throttle({start:startFn, throttle:processFn})( );
 
       concat("The ");
       concat("Hulk");
@@ -114,20 +114,20 @@ describe('ngThrottleSpec', function() {
 
     it("should properly process with report callbacks",function() {
       var pCount= 10, total, callCount= 0,
-        processFn = function(count, done){
-          pCount -= count;
-          if ( pCount == 5 ) {
+      processFn = function(count, done){
+        pCount -= count;
+        if ( pCount == 5 ) {
 
-            // Conditional termination of throttle phase
-            // report total value in callback
-            done(pCount);
-          }
-        },
-        /**
-         * only called when ALL processing is done
-         */
-        reportFn = function(count){ total = count; callCount +=1;},
-        subtract = $throttle({throttle:processFn})( );
+          // Conditional termination of throttle phase
+          // report total value in callback
+          done(pCount);
+        }
+      },
+      /**
+       * only called when ALL processing is done
+       */
+      reportFn = function(count){ total = count; callCount +=1;},
+      subtract = $throttle({throttle:processFn})( );
 
       subtract(2, reportFn );
       subtract(3, reportFn );
@@ -140,14 +140,14 @@ describe('ngThrottleSpec', function() {
 
     it("should restart if already finished",function() {
       var total= 0, started = 0,
-          startFn = function(){
-            started += 1;
-          },
-          processFn = function(count, done){
-            total += count;
-            done(); // !!finish throttling
-          },
-          add = $throttle({start:startFn, throttle:processFn})();
+      startFn = function(){
+        started += 1;
+      },
+      processFn = function(count, done){
+        total += count;
+        done(); // !!finish throttling
+      },
+      add = $throttle({start:startFn, throttle:processFn})();
 
       add(1);   $timeout.flush();   // proceed to end()
       add(1);   $timeout.flush();   // proceed to end()
@@ -227,12 +227,12 @@ describe('ngThrottleSpec', function() {
 
     it("should pass with async start()",function() {
       var sCount=0,
-          startFn = function(){
-            return $timeout(function(){
-              sCount++;
-            },100)
-          },
-          concat = $throttle({start:startFn})( done );
+      startFn = function(){
+        return $timeout(function(){
+          sCount++;
+        },100)
+      },
+      concat = $throttle({start:startFn})( done );
 
       concat("The ");
       concat("Hulk");
@@ -244,13 +244,13 @@ describe('ngThrottleSpec', function() {
 
     it("should pass with async start(done)",function() {
       var sCount=0,
-        startFn = function(done){
-          return $timeout(function(){
-            sCount++;
-            done();
-          },100)
-        },
-        concat = $throttle({start:startFn})( done );
+      startFn = function(done){
+        return $timeout(function(){
+          sCount++;
+          done();
+        },100)
+      },
+      concat = $throttle({start:startFn})( done );
 
       concat("The ");
       concat("Hulk");
@@ -262,18 +262,18 @@ describe('ngThrottleSpec', function() {
 
     it("should pass with async start(done) and end(done)",function() {
       var sCount=3, eCount= 0,
-        startFn = function(done){
-          return $timeout(function(){
-            sCount--;
-            done();
-          },100)
-        },
-        endFn = function(done){
-          return $timeout(function(){
-            eCount++;
-            done();
-          },100)
-        };
+      startFn = function(done){
+        return $timeout(function(){
+          sCount--;
+          done();
+        },100)
+      },
+      endFn = function(done){
+        return $timeout(function(){
+          eCount++;
+          done();
+        },100)
+      };
 
       $throttle({start:startFn, end:endFn})( done );
       $timeout.flush();                         // flush to begin 1st deferred start()
@@ -289,19 +289,19 @@ describe('ngThrottleSpec', function() {
 
     it("should pass with async start(done) and process(done)",function() {
       var title="",
-        startFn = function(){
-          return $timeout(function(){
-            done();
-          },200);
-        },
-        processFn = function(data, done){
-          $timeout(function(){
-            title += data;
-          },400);
-        },
-        concat = $throttle({start:startFn, throttle:processFn})( done );
+      startFn = function(){
+        return $timeout(function(){
+          done();
+        },200);
+      },
+      processFn = function(data, done){
+        $timeout(function(){
+          title += data;
+        },400);
+      },
+      concat = $throttle({start:startFn, throttle:processFn})( done );
 
-                        $timeout.flush();     // start()
+      $timeout.flush();     // start()
       concat("The ");   $timeout.flush();     // throttle(1)
       concat("Hulk");   $timeout.flush();     // throttle(2)
 
@@ -311,30 +311,30 @@ describe('ngThrottleSpec', function() {
 
     it("should pass with async process(done) and restart with cancellable end",function() {
       var content="", numStarts= 0, numEnds = 0,
-        startFn = function(done){
-          numStarts++;
-          done("start-done");
-        },
-        throttleFn = function(data, done){
-          $timeout(function(){
-            content += data;
-            if ( data == "e" ) {
-              done("throttle-done");
-            }
-          },400);
-        },
-        endFn = function(done){
-          numEnds++;
+      startFn = function(done){
+        numStarts++;
+        done("start-done");
+      },
+      throttleFn = function(data, done){
+        $timeout(function(){
+          content += data;
+          if ( data == "e" ) {
+            done("throttle-done");
+          }
+        },400);
+      },
+      endFn = function(done){
+        numEnds++;
 
-          var procID = $timeout(function(){
-            done("end-done");
-          },500,false);
+        var procID = $timeout(function(){
+          done("end-done");
+        },500,false);
 
-          return function() {
-            $timeout.cancel( procID );
-          };
-        },
-        concat = $throttle({ start:startFn, throttle:throttleFn, end:endFn })( done );
+        return function() {
+          $timeout.cancel( procID );
+        };
+      },
+      concat = $throttle({ start:startFn, throttle:throttleFn, end:endFn })( done );
 
       $timeout.flush();     // flush to begin 1st start()
 
@@ -372,7 +372,7 @@ describe('ngThrottleSpec', function() {
       var el;
       var tmpl = '' +
         '<div style="width:50px; height:50px;">'+
-          '<canvas class="material-ink-ripple" ></canvas>' +
+        '<canvas class="material-ink-ripple" ></canvas>' +
         '</div>';
 
       inject(function($compile, $rootScope) {
@@ -387,51 +387,51 @@ describe('ngThrottleSpec', function() {
     it('should start, animate, and end.', inject(function($compile, $rootScope, $materialEffects) {
 
       var cntr = setup(),
-          canvas = cntr.find('canvas'),
-          rippler, makeRipple, throttled = 0,
-          config = {
-            start : function() {
-              rippler = rippler || $materialEffects.inkRipple( canvas[0] );
-              cntr.on('mousedown', makeRipple);
-              started = true;
-            },
-            throttle : function(e, done) {
-              throttled += 1;
+      canvas = cntr.find('canvas'),
+      rippler, makeRipple, throttled = 0,
+      config = {
+        start : function() {
+          rippler = rippler || $materialEffects.inkRipple( canvas[0] );
+          cntr.on('mousedown', makeRipple);
+          started = true;
+        },
+        throttle : function(e, done) {
+          throttled += 1;
 
-              switch(e.type)
-              {
-                case 'mousedown' :
-                  rippler.createAt( {x:25,y:25} );
-                  rippler.draw( done );
-                  break;
+          switch(e.type)
+          {
+            case 'mousedown' :
+              rippler.createAt( {x:25,y:25} );
+            rippler.draw( done );
+            break;
 
-                default:
-                  break;
-              }
-            }
+            default:
+              break;
           }
+        }
+      }
 
-        // prepare rippler wave function...
+      // prepare rippler wave function...
 
-        makeRipple = $throttle(config)(done);
-        $timeout.flush();
-        expect(started).toBe(true);
+      makeRipple = $throttle(config)(done);
+      $timeout.flush();
+      expect(started).toBe(true);
 
-        // trigger wave animation...
+      // trigger wave animation...
 
-        cntr.triggerHandler("mousedown");
+      cntr.triggerHandler("mousedown");
 
-        // Allow animation to finish...
+      // Allow animation to finish...
 
-        $timeout(function(){
-          expect(throttled).toBe(1);
-          expect(ended).toBe(true);
-          expect(finished).toBe(true);
-        },10);
+      $timeout(function(){
+        expect(throttled).toBe(1);
+        expect(ended).toBe(true);
+        expect(finished).toBe(true);
+      },10);
 
-        // Remove from $rootElement
+      // Remove from $rootElement
 
-        cntr.remove();
+      cntr.remove();
 
     }));
 
