c1949cf2d26acc13864db3b6667f743f8f1ce09e
Andrew <andrewtjoslin@gmail.com>
2014-08-19 11:35:42 -0600

    docs(): make paramType not accidentally be marked

--- docs/tag-defs/index.js
+++ docs/tag-defs/index.js
@@ -1,14 +1,8 @@
 module.exports = [
   {
-    name: 'order',
-    defaultFn: function(doc) {
-      return doc.name;
-    }
+    name: 'order'
   },
   {
-    name: 'paramType',
-    defaultFn: function(doc) {
-      return doc.name.charAt(0).toUpperCase() + doc.name.substring(1);
-    }
+    name: 'paramType'
   }
 ];
