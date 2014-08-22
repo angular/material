461457647cced154de53c7f13a447995d69e1645
Marcy Sutton <marcy@substantial.com>
2014-08-22 11:46:08 -0700

    docs(material-radio-button): Add Constants config

---
+++ src/base/constants.js
@@ -0,0 +1,20 @@
+var Constants = {
+  aria : {
+    role : {
+      button : 'button',
+      checkbox : 'checkbox',
+      radio : 'radio',
+      radiogroup : 'radiogroup'
+    },
+    prop : {
+      checked : 'aria-checked',
+      hidden : 'aria-hidden',
+      expanded : 'aria-expanded'
+    }
+  },
+  keyCode : {
+    space: 32,
+    left_arrow : 37,
+    right_arrow : 39
+  }
+};
