522a496c89bb2a48fa03a72f1dd89a6ab0010d03
Andrew <andrewtjoslin@gmail.com>
2014-08-25 18:24:07 -0600

    chore: add jQuery dependency, remove test-util.js

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -4,10 +4,11 @@ module.exports = {
   basePath: __dirname + '/..',
   frameworks: ['jasmine'],
   files: [
+    // Include jQuery just for its dom helpers for testing, eg el.trigger()
+    'bower_components/jquery/jquery.js',
     'bower_components/angular/angular.js',
     'bower_components/angular-animate/angular-animate.js',
     'bower_components/angular-mocks/angular-mocks.js',
-    'config/test-util.js'
   ]
     .concat(buildConfig.paths.js)
     .concat(buildConfig.paths.test),
