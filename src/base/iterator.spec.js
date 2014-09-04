0697d4210de2a1c21890efa63ab41503ad8cc6d4
Thomas Burleson <ThomasBurleson@Gmail.com>
2014-09-03 19:23:23 -0500

    docs(demos): source view components not set `display:none` when source is **not** shown.
    Closes #234
    Closes #240

---
+++ src/base/iterator.spec.js
@@ -0,0 +1,46 @@
+describe('materialTabs directive', function() {
+
+  beforeEach(module('material.components.tabs', 'material.decorators', 'material.services.aria'));
+
+  describe('controller', function(){
+
+    function setup( attrs )
+    {
+      var el;
+      inject(function($compile, $rootScope) {
+        el = $compile('<material-tabs '+(attrs || '')+'></material-tabs>')($rootScope);
+        $rootScope.$apply();
+      });
+      return el;
+    }
+
+    it('should create a controller.', function(){
+      var el = setup();
+      var controller = el.controller('materialTabs');
+
+      expect(controller).not.toBe(undefined);
+      expect(controller.$$tabs().length).toBe(0);
+      expect(controller.noink).toBeFalsy();
+      expect(controller.nobar).toBeFalsy();
+
+    });
+
+    it('should assign ARIA roles', function(){
+      // TODO: this needs more coverage for material-tab
+      var el = setup();
+
+      expect(el.eq(0).attr('role')).toBe('tablist');
+    });
+
+    xit('should pass down "nobar" to hide the <div class="selectionBar">', function()
+    {
+      var tabs         = setup(''),
+          selectionBar = tabs.children(0)[2],
+          el           = angular.element(selectionBar )
+
+      expect( el.hasClass('ng-hide') ).toBeTruthy();
+    });
+
+  })
+
+});
