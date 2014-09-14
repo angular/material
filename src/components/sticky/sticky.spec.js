1e9e15fea95fc67215eb8562f442e682f725ea88
Ryan Schmukler <rschmukler@gmail.com>
2014-09-14 15:19:23 -0400

    get rid of ddescribe

--- src/components/sticky/sticky.spec.js
+++ src/components/sticky/sticky.spec.js
@@ -1,5 +1,6 @@
-ddescribe('$materialStickySpec', function() {
+describe('$materialStickySpec', function() {
   beforeEach(module('materia.components.sticky'));
+  // Todo, how to test this stuff?
   it('adds material-sticky-active to active element');
   it('adds material-transition to a transitioning element');
   it('computes the style of transition elements on scroll');
