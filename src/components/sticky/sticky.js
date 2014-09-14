809ebd76006e7f4ddf47107bbeff220710e5a62a
Ryan Schmukler <rschmukler@gmail.com>
2014-09-14 14:42:17 -0400

    switch to use material-container scroll, and keep track of stickies per container

--- src/components/sticky/sticky.js
+++ src/components/sticky/sticky.js
@@ -20,17 +20,23 @@ angular.module('material.components.sticky', [])
  */
 
 function MaterialSticky($window, $document, $$rAF) {
-  var elements = [],
-  orderedElements, // Sorted elements by scroll position
-  targetElementIndex, browserStickySupport;
+  var browserStickySupport;
 
   /**
    * Registers an element as sticky, used internally by directives to register themselves
    */
 
+
   function registerStickyElement(scope, $el) {
-    $el = Util.wrap($el, 'div', 'sticky-container');
+    scope.$on('$destroy', function() { registerStickyElement.$deregister($el); });
+    $el = Util.wrap($el, 'div', 'sticky-container'),
+    $container = $el.controller('materialContent').$element;
+
+    if(!$container) { throw new Error('$materialSticky used outside of material-contant'); }
+
+    var elements = $container.data('$stickyEls') || [];
     elements.push($el);
+    $container.data('$stickyEls', elements);
 
     // check sticky support on first register
     if(browserStickySupport === undefined) {
@@ -39,24 +45,30 @@ function MaterialSticky($window, $document, $$rAF) {
       $el.css({position: browserStickySupport, top: '0px'});
     }
 
+    var debouncedCheck = $container.data('$stickyCheck') || $$rAF.debounce(checkElements.bind(undefined, $container));
+    $container.data('$stickyCheck', debouncedCheck);
+
+
     if(!browserStickySupport) {
       if(elements.length == 1) {
-        $document.on('scroll',  checkElements);
+        $container.on('scroll',  debouncedCheck);
       }
-      scanElements();
+      scanElements($container);
     }
   }
 
 
   // Deregister a sticky element, useful for $destroy event.
   registerStickyElement.$deregister = function($el) {
+    var $container = $el.controller('materialContent').$element;
+    var elements = $container.data('$stickyEls') || [];
     var innerElements = elements.map(function(el) { return el.children(0); });
     var index = innerElements.indexOf($el);
-    if(~index) {
+    if(index !== -1) {
       elements[index].replaceWith($el);
       elements.splice(index, 1);
       if(elements.length === 0) {
-        $document.off('scroll', checkElements);
+        $container.off('scroll', $container.data('$stickyCheck'));
       }
     }
   };
@@ -80,7 +92,8 @@ function MaterialSticky($window, $document, $$rAF) {
    * Function to prepare our lookups so we can go quick!
    * */
 
-  function scanElements() {
+  function scanElements($container) {
+    var elements = $container.data('$stickyEls');
     if(browserStickySupport) return; // don't need to do anything if we have native sticky
     targetElementIndex = 0;
     // Sort based on position in the window, and assign an active index
@@ -88,9 +101,11 @@ function MaterialSticky($window, $document, $$rAF) {
       return rect(a).top - rect(b).top;
     });
 
+    $container.data('$stickyOrderedEls', orderedElements);
+
     // Iterate over our sorted elements and find the one that is active
     (function findTargetElement() {
-      var windowTop = $window.scrollY;
+      var scroll = $container.scrollTop();
       for(var i = 0; i < orderedElements.length ; ++i) {
         if(rect(orderedElements[i].children(0)).bottom > 0) {
           targetElementIndex = i > 0 ? i - 1 : i;
@@ -98,83 +113,86 @@ function MaterialSticky($window, $document, $$rAF) {
           targetElementIndex = i;
         }
       }
+      $container.data('$stickyTarget', targetElementIndex);
     })();
   }
 
-  var lastScroll = 0;
-  function checkElements() {
+  function checkElements($container) {
+    var next; // pointer to next target
+
+    // Convenience getter for the target element
+    var targetElementIndex = $container.data('$stickyTarget'),
+        orderedElements = $container.data('$stickyOrderedEls');
+
     var content = targetElement().children(0);
     var contentRect = rect(content),
     targetRect = rect(targetElement());
 
-    var scrollingDown = false;
-    if($window.scrollY > (lastScroll || 0)) {
+    var scrollingDown = false,
+        currentScroll = $container.scrollTop(),
+        lastScroll = $container.data('$stickyLastScroll');
+
+    if(currentScroll > ($container.data('$stickyLastScroll') || 0)) {
       scrollingDown = true;
     }
-    lastScroll = $window.scrollY;
+    $container.data('$stickyLastScroll', currentScroll);
 
     var stickyActive = content.attr('material-sticky-active');
 
 
+    // If we are scrollingDown, sticky, and are being pushed off screen by a different element, increment
     if(scrollingDown && stickyActive && contentRect.bottom <= 0 && targetElementIndex < orderedElements.length - 1) {
+      targetElement().children(0).removeAttr('material-sticky-active');
+      targetElement().css({height: null});
       incrementElement();
-      $$rAF(function() {
-        targetElement(-1).children(0).removeAttr('material-sticky-active');
-        targetElement(-1).css({height: null});
-      });
-    } else if(!scrollingDown && stickyActive && targetRect.top > 0) {
-      incrementElement(-1);
-      return $$rAF(function() {
-        targetElement(+1).children(0).removeAttr('material-sticky-active');
-        targetElement(+1).css({height: null});
-        content.attr('material-sticky-active', true);
-        content.css({top: -1 * contentRect.height});
-        targetElement().css({height: contentRect.height});
-      });
-    } else if(scrollingDown && contentRect.top <= 0 && !stickyActive) {
-      $$rAF(function() {
-        content.attr('material-sticky-active', true);
-        targetElement().css({height: contentRect.height});
-        contentRect = rect(content);
-        var next = targetElement(+1),
-            offset = 0;
-        if(next) {
-          nextRect = rect(next.children(0));
-          if(rectsAreTouching(contentRect, nextRect)) {
-            offset = nextRect.top - contentRect.bottom;
-          }
-        }
-        content.css({top: Math.min(offset, 0)});
-      });
     } 
-
-    var nextRect;
-    if(scrollingDown) {
-      // check if we need to push
-      var next = targetElement(+1);
+    //If we are going up, and our normal position would be rendered not sticky, un-sticky ourselves
+    else if(!scrollingDown && stickyActive && targetRect.top > 0) {
+      targetElement().children(0).removeAttr('material-sticky-active');
+      targetElement().css({height: null});
+      incrementElement(-1);
+      content.attr('material-sticky-active', true);
+      content.css({top: -1 * contentRect.height});
+      targetElement().css({height: contentRect.height});
+    } 
+    // If we are going off screen and haven't been made sticky yet, go sticky
+    else if(scrollingDown && contentRect.top <= 0 && !stickyActive) {
+      content.attr('material-sticky-active', true);
+      targetElement().css({height: contentRect.height});
+      contentRect = rect(content);
+      next = targetElement(+1);
+      var offset = 0;
       if(next) {
         nextRect = rect(next.children(0));
         if(rectsAreTouching(contentRect, nextRect)) {
-          $$rAF(function() {
-            var offsetAmount = contentRect.bottom - nextRect.top;
-            var currentTop = content.css('top');
-            if(currentTop == 'auto') { currentTop = 0; }
-            else { currentTop = parseInt(currentTop, 10); }
-            var newTop = currentTop - offsetAmount;
-            content.css({top: currentTop - offsetAmount});
-          });
+          offset = nextRect.top - contentRect.bottom;
+        }
+      }
+      content.css({top: Math.min(offset, 0)});
+    } 
+
+    var nextRect, offsetAmount, currentTop;
+    if(scrollingDown) {
+      // check if we need to push
+      next = targetElement(+1);
+      if(next) {
+        nextRect = rect(next.children(0));
+        if(rectsAreTouching(contentRect, nextRect)) {
+          offsetAmount = contentRect.bottom - nextRect.top;
+          currentTop = content.css('top');
+          if(currentTop == 'auto') { currentTop = 0; }
+          else { currentTop = parseInt(currentTop, 10); }
+          content.css({top: currentTop - offsetAmount});
         }
       }
     } else if(targetElementIndex < orderedElements.length - 1 && contentRect.top < 0) {
       // we need to pull
       nextRect = rect(targetElement(+1).children(0));
-      $$rAF(function() {
-        var offsetAmount = contentRect.bottom - nextRect.top;
-        var currentTop = content.css('top');
-        if(currentTop == 'auto') { currentTop = 0; }
-        else { currentTop = parseInt(currentTop, 10); }
-        content.css({top: Math.min(currentTop - offsetAmount, 0)});
-      });
+      offsetAmount = contentRect.bottom - nextRect.top;
+      currentTop = content.css('top');
+      if(currentTop == 'auto') { currentTop = 0; }
+      else { currentTop = parseInt(currentTop, 10); }
+      content.css({top: Math.min(currentTop - offsetAmount, 0)});
     }
 
     function incrementElement(inc) {
@@ -182,16 +200,14 @@ function MaterialSticky($window, $document, $$rAF) {
       targetElementIndex += inc;
       content = targetElement().children(0);
       contentRect = rect(content);
+      $container.data('$stickyTarget', targetElementIndex);
     }
-  }
 
-
-
-  // Convenience getter for the target element
-  function targetElement(indexModifier) {
-    indexModifier = indexModifier || 0;
-    if(targetElementIndex === undefined) return undefined;
-    return orderedElements[targetElementIndex + indexModifier];
+    function targetElement(indexModifier) {
+      indexModifier = indexModifier || 0;
+      if(targetElementIndex === undefined) return undefined;
+      return orderedElements[targetElementIndex + indexModifier];
+    }
   }
 
   function rectsAreTouching(first, second) {
