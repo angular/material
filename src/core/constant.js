05ed42de4fb52ec916b2fcc6e5a78d2d5ea164ad
Marcy Sutton <marcy@substantial.com>
2014-09-22 16:00:56 -0700

    fix(material-radio): Radio button a11y
    Closes #310

--- src/core/constant.js
+++ src/core/constant.js
@@ -1,9 +1,11 @@
 var Constant = {
   KEY_CODE: {
+    ENTER: 13,
     ESCAPE: 27,
     SPACE: 32,
-    LEFT_ARROW: 37,
-    RIGHT_ARROW: 39,
-    ENTER: 13
+    LEFT_ARROW : 37,
+    UP_ARROW : 38,
+    RIGHT_ARROW : 39,
+    DOWN_ARROW : 40
   }
 };
