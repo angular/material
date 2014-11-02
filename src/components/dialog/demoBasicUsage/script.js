01856e72606ffc19a1721eae361e1815b541d150
Thomas Burleson <ThomasBurleson@Gmail.com>
2014-11-02 07:53:45 -0600

    enhance(demos): dialog demo uses  callback. Improved subheader styling

--- src/components/dialog/demoBasicUsage/script.js
+++ src/components/dialog/demoBasicUsage/script.js
@@ -1,32 +1,36 @@
 angular.module('dialogDemo1', ['ngMaterial'])
 
-.controller('AppCtrl', function($scope, $mdDialog) {
-  $scope.alert = '';
+  .controller('AppCtrl', function($scope, $mdDialog, $log) {
+    $scope.alert = '';
 
-  $scope.dialogBasic = function(ev) {
-    $mdDialog.show({
-      templateUrl: 'dialog1.tmpl.html',
-      targetEvent: ev,
-      controller: DialogController
-    }).then(function() {
-      $scope.alert = 'You said "Okay".';
-    }, function() {
-      $scope.alert = 'You cancelled the dialog.';
-    });
-  };
+    $scope.dialogBasic = function(ev) {
+      $mdDialog.show({
+        templateUrl: 'dialog1.tmpl.html',
+        targetEvent: ev,
+        controller: DialogController
+      }).then(function() {
+        $scope.alert = 'You said "Okay".';
+      }, function() {
+        $scope.alert = 'You cancelled the dialog.';
+      });
+    };
 
-  $scope.dialogAdvanced = function(ev) {
-    $mdDialog.show({
-      templateUrl: 'dialog2.tmpl.html',
-      targetEvent: ev,
-      controller: DialogController
-    }).then(function(answer) {
-      $scope.alert = 'You said the information was "' + answer + '".';
-    }, function() {
-      $scope.alert = 'You cancelled the dialog.';
-    });
-  };
-});
+    $scope.dialogAdvanced = function(ev) {
+      $log.debug("dialogAdvanced() preparing to show...");
+      $mdDialog.show({
+        templateUrl: 'dialog2.tmpl.html',
+        targetEvent: ev,
+        controller: DialogController,
+        onComplete:function(){
+          $log.debug("dialogAdvanced() now shown!");
+        }
+      }).then(function(answer) {
+        $scope.alert = 'You said the information was "' + answer + '".';
+      }, function() {
+        $scope.alert = 'You cancelled the dialog.';
+      });
+    };
+  });
 
 function DialogController($scope, $mdDialog) {
   $scope.hide = function() {
