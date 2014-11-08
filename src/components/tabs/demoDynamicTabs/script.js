e16a008b1c2289b75d70daa02b031a02d60d978f
Andrew <andrewtjoslin@gmail.com>
2014-11-08 11:47:31 -0700

    demo(dynamicTabs): add two more so it paginates

--- src/components/tabs/demoDynamicTabs/script.js
+++ src/components/tabs/demoDynamicTabs/script.js
@@ -1,14 +1,16 @@
 angular.module('tabsDemo2', ['ngMaterial'])
   .controller('AppCtrl', function ($scope) {
     var tabs = [
-      { title: 'First Tab', content: "Tabs will become paginated if there isn't enough room for them."},
-      { title: 'Second Tab', content: "You can swipe left and right on a mobile device to change tabs."},
-      { title: 'Third Tab', content: "You can bind the selected tab via the selected attribute on the md-tabs element."},
-      { title: 'Fourth Tab', content: "If you set the selected tab binding to -1, it will leave no tab selected."},
-      { title: 'Fifth Tab', content: "If you remove a tab, it will try to select a new one."},
-      { title: 'Sixth Tab', content: "There's an ink bar that follows the selected tab, you can turn it off if you want."},
-      { title: 'Seventh Tab', content: "If you set ng-disabled on a tab, it becomes unselectable. If the currently selected tab becomes disabled, it will try to select the next tab."},
-      { title: 'Eighth Tab', content: "If you're still reading this, you should just go check out the API docs for tabs!"}
+      { title: 'One', content: "Tabs will become paginated if there isn't enough room for them."},
+      { title: 'Two', content: "You can swipe left and right on a mobile device to change tabs."},
+      { title: 'Three', content: "You can bind the selected tab via the selected attribute on the md-tabs element."},
+      { title: 'Four', content: "If you set the selected tab binding to -1, it will leave no tab selected."},
+      { title: 'Five', content: "If you remove a tab, it will try to select a new one."},
+      { title: 'Six', content: "There's an ink bar that follows the selected tab, you can turn it off if you want."},
+      { title: 'Seven', content: "If you set ng-disabled on a tab, it becomes unselectable. If the currently selected tab becomes disabled, it will try to select the next tab."},
+      { title: 'Eight', content: "If you look at the source, you're using tabs to look at a demo for tabs. Recursion!"},
+      { title: 'Nine', content: "If you set md-theme=\"green\" on the md-tabs element, you'll get green tabs."},
+      { title: 'Ten', content: "If you're still reading this, you should just go check out the API docs for tabs!"}
     ];
 
     $scope.tabs = tabs;
