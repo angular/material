117772bf3e068a054d6c230b89587f870dffe502
Robert Messerle <robertmesserle@gmail.com>
2014-11-18 19:22:48 -0800

    enhance(ripple): improvements to API, sideNav list items, and button effects
    add md-ripple-active support for nav active state,
    add support for ms-ink-color to set a custom ink color, cleans up attribute names and re-arranges code,
    rename method to be more consistent with our naming conventions,
    add test for custom ripple ink color,
    allow invalid entries for md-ink-ripple to be ignored,
    change internal code for checkboxes and radio buttons to use md-ink-ripple-checkbox
    
    Close #689.

--- src/components/checkbox/checkbox.js
+++ src/components/checkbox/checkbox.js
@@ -55,7 +55,7 @@ function MdCheckboxDirective(inputDirective, $mdInkRipple, $mdAria, $mdConstant,
     transclude: true,
     require: '?ngModel',
     template: 
-      '<div class="md-container" md-ink-ripple="checkbox">' +
+      '<div class="md-container" md-ink-ripple md-ink-ripple-checkbox>' +
         '<div class="md-icon"></div>' +
       '</div>' +
       '<div ng-transclude class="md-label"></div>',
--- src/core/services/ripple/ripple.js
+++ src/core/services/ripple/ripple.js
@@ -8,15 +8,18 @@ angular.module('material.core')
   .directive('mdNoBar', attrNoDirective())
   .directive('mdNoStretch', attrNoDirective());
 
-function InkRippleDirective($mdInkRipple) {
-  return function(scope, element, attr) {
-    if (attr.mdInkRipple == 'checkbox') {
-      $mdInkRipple.attachCheckboxBehavior(element);
-    } else {
-      $mdInkRipple.attachButtonBehavior(element);
-    }
-  };
-}
+  function InkRippleDirective($mdInkRipple) {
+    return {
+      controller: angular.noop,
+      link: function (scope, element, attr) {
+        if (attr.hasOwnProperty('mdInkRippleCheckbox')) {
+          $mdInkRipple.attachCheckboxBehavior(scope, element);
+        } else {
+          $mdInkRipple.attachButtonBehavior(scope, element);
+        }
+      }
+    };
+  }
 
 function InkRippleService($window, $timeout) {
 
@@ -26,27 +29,34 @@ function InkRippleService($window, $timeout) {
     attach: attach
   };
 
-  function attachButtonBehavior(element) {
-    return attach(element, {
+  function attachButtonBehavior(scope, element) {
+    return attach(scope, element, {
       center: element.hasClass('md-fab'),
       dimBackground: true
     });
   }
 
-  function attachCheckboxBehavior(element) {
-    return attach(element, {
+  function attachCheckboxBehavior(scope, element) {
+    return attach(scope, element, {
       center: true,
       dimBackground: false
     });
   }
 
-  function attach(element, options) {
-
+  function attach(scope, element, options) {
     if (element.controller('mdNoInk')) return angular.noop;
 
-    var rippleContainer, rippleEl,
+    var rippleContainer,
+        controller = element.controller('mdInkRipple') || {},
+        counter = 0,
+        ripples = [],
+        states = [],
+        isActiveExpr = element.attr('md-active'),
+        isActive = false,
+        isHeld = false,
         node = element[0],
         hammertime = new Hammer(node),
+        color = parseColor(element.attr('md-ink-ripple')) || parseColor($window.getComputedStyle(node).color || 'rgb(0, 0, 0)'),
         contentParent = element.controller('mdContent');
 
     options = angular.extend({
@@ -60,107 +70,215 @@ function InkRippleService($window, $timeout) {
 
     options.mousedown && hammertime.on('hammer.input', onInput);
 
+    controller.createRipple = createRipple;
+
+    if (isActiveExpr) {
+      scope.$watch(
+          function () {
+            return scope.$eval(isActiveExpr);
+          },
+          function (newValue) {
+            isActive = newValue;
+            if (isActive) {
+              if (ripples.length === 0) {
+                createRipple(0, 0);
+              }
+            }
+            angular.forEach(ripples, updateElement);
+          }
+      );
+    }
+
     // Publish self-detach method if desired...
     return function detach() {
       hammertime.destroy();
       rippleContainer && rippleContainer.remove();
     };
 
-    function rippleIsAllowed() {
-      var parent;
-      return !element[0].hasAttribute('disabled') &&
-          !((parent = element[0].parentNode) && parent.hasAttribute('disabled'));
+      function parseColor(color) {
+        if (!color) return;
+        if (color.indexOf('rgba') === 0) return color;
+        if (color.indexOf('rgb')  === 0) return rgbToRGBA(color);
+        if (color.indexOf('#')    === 0) return hexToRGBA(color);
+
+        /**
+         *
+         */
+        function hexToRGBA(color) {
+          var hex = color.charAt(0) === '#' ? color.substr(1) : color,
+            dig = hex.length / 3,
+            red = hex.substr(0, dig),
+            grn = hex.substr(dig, dig),
+            blu = hex.substr(dig * 2);
+          if (dig === 1) {
+            red += red;
+            grn += grn;
+            blu += blu;
+          }
+          return 'rgba(' + parseInt(red, 16) + ',' + parseInt(grn, 16) + ',' + parseInt(blu, 16) + ',0.1)';
+        }
+
+        /**
+         *
+         */
+        function rgbToRGBA(color) {
+          return color.replace(')', ', 0.1)').replace('(', 'a(')
+        }
+
+      }
+
+    function removeElement(elem, wait) {
+      ripples.splice(ripples.indexOf(elem), 1);
+      if (ripples.length === 0) {
+        rippleContainer && rippleContainer.css({ backgroundColor: '' });
+      }
+      $timeout(function () { elem.remove(); }, wait, false);
     }
 
-    function removeElement(element, wait) {
-      $timeout(function () {
-        element.remove();
-      }, wait, false);
+    function updateElement(elem) {
+      var index = ripples.indexOf(elem),
+          state = states[index],
+          elemIsActive = ripples.length > 1 ? false : isActive,
+          elemIsHeld   = ripples.length > 1 ? false : isHeld;
+      if (elemIsActive || state.animating || elemIsHeld) {
+        elem.addClass('md-ripple-visible');
+      } else {
+        elem.removeClass('md-ripple-visible');
+        removeElement(elem, 650);
+      }
     }
 
-    function createRipple(left, top, positionsAreAbsolute) {
+      /**
+       *
+       * @returns {*}
+       */
+      function createRipple(left, top) {
 
-      var rippleEl = angular.element('<div class="md-ripple">');
+      var container = getRippleContainer(),
+          size = getRippleSize(),
+          css = getRippleCss(size, left, top),
+          elem = getRippleElement(css),
+          index = ripples.indexOf(elem),
+          state = states[index];
 
-      if (!rippleContainer) {
-        rippleContainer = angular.element('<div class="md-ripple-container">');
-        element.append(rippleContainer);
-      }
-      rippleContainer.append(rippleEl);
+      state.animating = true;
 
-      var containerWidth = rippleContainer.prop('offsetWidth'),
-          containerHeight = rippleContainer.prop('offsetHeight'),
-          multiplier = element.hasClass('md-fab') ? 1.1 : 0.8,
-          rippleWidth = Math.max(containerWidth, containerHeight) * multiplier;
-
-      if (contentParent) {
-        top += contentParent.$element.prop('scrollTop');
-      }
-
-      var css = {
-        backgroundColor: $window.getComputedStyle(rippleEl[0]).color ||  $window.getComputedStyle(node).color,
-        width: rippleWidth + 'px',
-        height: rippleWidth + 'px',
-        marginLeft: (rippleWidth * -0.5) + 'px',
-        marginTop: (rippleWidth * -0.5) + 'px'
-      };
-
-      if (options.center) {
-        css.left = '50%';
-        css.top = '50%';
-      } else if (positionsAreAbsolute) {
-        var elementRect = node.getBoundingClientRect();
-        left -= elementRect.left;
-        top -= elementRect.top;
-        css.left = Math.round(left / containerWidth * 100) + '%';
-        css.top = Math.round(top / containerHeight * 100) + '%';
-      }
-
-      rippleEl.css(css);
-
-      //-- Use minimum timeout to trigger CSS animation
       $timeout(function () {
         if (options.dimBackground) {
-          rippleContainer.addClass('md-ripple-full md-ripple-visible');
-          rippleContainer.css({ backgroundColor: css.backgroundColor.replace(')', ', 0.1').replace('(', 'a(') });
+          container.css({ backgroundColor: color });
         }
-        rippleEl.addClass('md-ripple-placed md-ripple-visible md-ripple-scaled md-ripple-full');
-        rippleEl.css({ left: '50%', top: '50%' });
+        elem.addClass('md-ripple-placed md-ripple-scaled').css({ left: '50%', top: '50%' });
+        updateElement(elem);
         $timeout(function () {
-          if (rippleEl) {
-            rippleEl.removeClass('md-ripple-full');
-            if (!rippleEl.hasClass('md-ripple-visible')) {
-              removeElement(rippleEl, 650);
-              rippleEl = null;
-            }
-          }
-          rippleEl && rippleEl.removeClass('md-ripple-full');
-          if (rippleContainer && options.dimBackground) {
-            rippleContainer.removeClass('md-ripple-full');
-            if (!rippleContainer.hasClass('md-ripple-visible')) rippleContainer.css({ backgroundColor: '' });
-          }
+          state.animating = false;
+          updateElement(elem);
         }, 225, false);
       }, 0, false);
-      return rippleEl;
-    }
 
-    function onInput(ev) {
-      if (ev.eventType === Hammer.INPUT_START && ev.isFirst && rippleIsAllowed()) {
-        rippleEl = createRipple(ev.center.x, ev.center.y, true);
-      } else if (ev.eventType === Hammer.INPUT_END && ev.isFinal) {
-        if (rippleEl) {
-          rippleEl.removeClass('md-ripple-visible');
-          removeElement(rippleEl, 650);
-          rippleEl = null;
+      return elem;
+
+        /**
+         *
+         * @returns {*}
+         */
+        function getRippleElement(css) {
+          var elem = angular.element('<div class="md-ripple" data-counter="' + counter++ + '">');
+          ripples.unshift(elem);
+          states.unshift({ animating: true });
+          container.append(elem);
+          css && elem.css(css);
+          return elem;
         }
-        if (rippleContainer && options.dimBackground) {
-          rippleContainer.removeClass('md-ripple-visible');
-          if (!rippleContainer.hasClass('md-ripple-full')) rippleContainer.css({ backgroundColor: '' });
+
+        /**
+         *
+         * @returns {*}
+         */
+        function getRippleSize() {
+          var width = container.prop('offsetWidth'),
+            height = container.prop('offsetHeight'),
+            multiplier, size;
+        if (element.hasClass('md-menu-item')) {
+          size = Math.sqrt( Math.pow(width, 2) + Math.pow(height, 2) );
+        } else {
+          multiplier = element.hasClass('md-fab') ? 1.1 : 0.8;
+          size = Math.max(width, height) * multiplier;
         }
+        return size;
+      }
+
+        /**
+         *
+         * @returns {{backgroundColor: *, width: string, height: string, marginLeft: string, marginTop: string}}
+         */
+        function getRippleCss(size, left, top) {
+          var css = {
+            backgroundColor: rgbaToRGB(color),
+            width: size + 'px',
+            height: size + 'px',
+            marginLeft: (size * -0.5) + 'px',
+            marginTop: (size * -0.5) + 'px'
+          };
+
+        contentParent && (top += contentParent.$element.prop('scrollTop'));
+
+        if (options.center) {
+          css.left = css.top = '50%';
+        } else {
+          var rect = node.getBoundingClientRect();
+          css.left = Math.round((left - rect.left) / container.prop('offsetWidth') * 100) + '%';
+          css.top = Math.round((top - rect.top) / container.prop('offsetHeight') * 100) + '%';
+        }
+
+          return css;
+
+          /**
+           *
+           */
+          function rgbaToRGB(color) {
+            return color.replace('rgba', 'rgb').replace(/,[^\)\,]+\)/, ')');
+          }
+        }
+
+        /**
+         *
+         */
+        function getRippleContainer() {
+          if (rippleContainer) return rippleContainer;
+          var container = rippleContainer = angular.element('<div class="md-ripple-container">');
+          element.append(container);
+          return container;
+        }
+      }
+
+      /**
+       *
+       */
+      function onInput(ev) {
+        var ripple, index;
+        if (ev.eventType === Hammer.INPUT_START && ev.isFirst && isRippleAllowed()) {
+          ripple = createRipple(ev.center.x, ev.center.y);
+          isHeld = true;
+        } else if (ev.eventType === Hammer.INPUT_END && ev.isFinal) {
+          isHeld = false;
+          index = ripples.length - 1;
+          ripple = ripples[index];
+          $timeout(function () {
+            updateElement(ripple);
+          }, 0, false);
+        }
+
+        /**
+         *
+         */
+        function isRippleAllowed() {
+          var parent = node.parentNode;
+          return !node.hasAttribute('disabled') && !(parent && parent.hasAttribute('disabled'));
+        }
+
       }
     }
   }
-}
 
 /**
  * noink/nobar/nostretch directive: make any element that has one of
