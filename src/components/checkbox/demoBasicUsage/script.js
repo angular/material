7b147b94021d425450064d1354b0e1bda39f2276
Robert Messerle <robertmesserle@gmail.com>
2014-11-26 16:40:24 -0800

    fix(checkbox): fixes issue where double-clicking checkboxes causes text selection
    Closes #588

--- src/components/checkbox/demoBasicUsage/script.js
+++ src/components/checkbox/demoBasicUsage/script.js
@@ -6,5 +6,8 @@ angular.module('checkboxDemo1', ['ngMaterial'])
   $scope.data = {};
   $scope.data.cb1 = true;
   $scope.data.cb2 = false;
+  $scope.data.cb3 = false;
+  $scope.data.cb4 = false;
+  $scope.data.cb5 = false;
 
 });
