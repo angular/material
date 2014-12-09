f11e259b0a9aceec738a9415d658299c871cb8c3
Andrew <andrewtjoslin@gmail.com>
2014-12-09 12:08:31 -0700

    refactor(): create internal `$mdUtil.pannable(scope, element, opts)` function

--- src/core/util/util.js
+++ src/core/util/util.js
@@ -1,19 +1,21 @@
 (function() {
 'use strict';
 
-/* 
+/*
  * This var has to be outside the angular factory, otherwise when
  * there are multiple material apps on the same page, each app
- * will create its own instance of this array and the app's IDs 
+ * will create its own instance of this array and the app's IDs
  * will not be unique.
  */
 var nextUniqueId = ['0','0','0'];
 
 angular.module('material.core')
-.factory('$mdUtil', ['$cacheFactory', function($cacheFactory) {
+.factory('$mdUtil', function($cacheFactory, $document) {
   var Util;
   return Util = {
     now: window.performance ? angular.bind(window.performance, window.performance.now) : Date.now,
+
+    pannable: pannable,
 
     /**
      * Publish the iterator facade to easily support iteration and accessors
@@ -291,7 +293,7 @@ angular.module('material.core')
     }
 
     /*
-     * Find the next item. If reloop is true and at the end of the list, it will 
+     * Find the next item. If reloop is true and at the end of the list, it will
      * go back to the first item. If given ,the `validate` callback will be used
      * determine whether the next item is valid. If not valid, it will try to find the
      * next item again.
@@ -313,7 +315,7 @@ angular.module('material.core')
     }
 
     /*
-     * Find the previous item. If reloop is true and at the beginning of the list, it will 
+     * Find the previous item. If reloop is true and at the beginning of the list, it will
      * go back to the last item. If given ,the `validate` callback will be used
      * determine whether the previous item is valid. If not valid, it will try to find the
      * previous item again.
@@ -351,6 +353,84 @@ angular.module('material.core')
     }
   }
 
+  function pannable(scope, element, options) {
+    // The state of the current pan
+    var pan;
+    // Whether the pointer is currently down on this element.
+    var pointerIsDown;
+    var START_EVENTS = 'mousedown touchstart pointerdown';
+    var MOVE_EVENTS = 'mousemove touchmove pointermove';
+    var END_EVENTS = 'mouseup mouseleave touchend touchcancel pointerup pointercancel';
+
+    // Listen to move and end events on document. End events especially could have bubbled up
+    // from the child.
+    element.on(START_EVENTS, startPan);
+    $document.on(MOVE_EVENTS, doPan)
+      .on(END_EVENTS, endPan);
+
+    scope.$on('$destroy', cleanup);
+
+    return cleanup;
+
+    function cleanup() {
+      if (cleanup.called) return;
+      cleanup.called = true;
+
+      element.off(START_EVENTS, startPan);
+      $document.off(MOVE_EVENTS, doPan)
+        .off(END_EVENTS, endPan);
+      pan = pointerIsDown = false;
+    }
+
+    function startPan(ev) {
+      if (pointerIsDown) return;
+      pointerIsDown = true;
+
+      pan = {
+        // Restrict this pan to whatever started it: if a mousedown started the pan,
+        // don't let anything but mouse events continue it.
+        pointerType: ev.type.charAt(0),
+        startX: getPosition(ev),
+        startTime: Util.now()
+      };
+
+      element.one('$mdPanStart', function(ev) {
+        // Allow user to cancel by preventing default
+        if (ev.defaultPrevented) pan = null;
+      });
+      element.triggerHandler('$md.panstart', pan);
+    }
+    function doPan(ev) {
+      if (!pan || !isProperEventType(ev)) return;
+
+      updatePanState(ev);
+      element.triggerHandler('$md.pan', pan);
+    }
+    function endPan(ev) {
+      pointerIsDown = false;
+      if (!pan || !isProperEventType(ev)) return;
+
+      updatePanState(ev);
+      element.triggerHandler('$md.panend', pan);
+      pan = null;
+    }
+
+    function updatePanState(ev) {
+      var x = getPosition(ev);
+      pan.distance = pan.startX - x;
+      pan.direction = pan.distance > 0 ? 'left' : (pan.distance < 0 ? 'right' : '');
+      pan.time = pan.startTime - Util.now();
+      pan.velocity = Math.abs(pan.distance) / pan.time;
+    }
+    function getPosition(ev) {
+      ev = ev.originalEvent || ev; //support jQuery events
+      return (ev.touches ? ev.touches[0] : ev).pageX;
+    }
+    function isProperEventType(ev) {
+      return pan && ev && (ev.type || '').charAt(0) === pan.pointerType;
+    }
+  }
+
   /*
    * Angular's $cacheFactory doesn't have a keys() method,
    * so we add one ourself.
@@ -376,9 +456,10 @@ angular.module('material.core')
 
     return cache;
   }
-}]);
 
-/* 
+});
+
+/*
  * Since removing jQuery from the demos, some code that uses `element.focus()` is broken.
  *
  * We need to add `element.focus()`, because it's testable unlike `element[0].focus`.
