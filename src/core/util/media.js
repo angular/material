8736c7ccf019df417e6b7834b55a1cc157a6ac64
Georgios Kalpakas <g.kalpakas@hotmail.com>
2014-12-18 15:44:23 +0200

    fix(mdUtil): remove/delete cacheFactory keys when clearing/destroying cache
    `mdUtil`'s custom `cacheFactory` factory, adds/removes keys when
    putting/removing a single key, but leaves `keys` untouched when calling
    `removeAll()` or `destroy()`. As a result, the following problems arise:
    
    - Keys of destroyed caches continue to take up memory.
    - The `keys()` method return incorrect results after calling `removeAll()`
      (i.e. it continues to return the old keys, although they are not present
      in the cache any more).
    
    This commit fixes these issues, by overriding the `removeAll` and
    `destroy` methods as well.

--- src/core/util/media.js
+++ src/core/util/media.js
@@ -1,5 +1,5 @@
 angular.module('material.core')
-    .factory('$mdMedia', mdMediaFactory);
+.factory('$mdMedia', mdMediaFactory);
 
 /**
  * Exposes a function on the '$mdMedia' service which will return true or false,
@@ -11,41 +11,44 @@ angular.module('material.core')
  * @example $mdMedia('max-width: 300px') == true if device-width <= 300px (sanitizes input, adding parens)
  */
 function mdMediaFactory($window, $mdUtil, $timeout, $mdConstant) {
-    var cache = $mdUtil.cacheFactory('$mdMedia', { capacity: 15 });
+  var queriesCache = $mdUtil.cacheFactory('$mdMedia:queries', {capacity: 15});
+  var resultsCache = $mdUtil.cacheFactory('$mdMedia:results', {capacity: 15});
 
-    angular.element($window).on('resize', updateAll);
+  angular.element($window).on('resize', updateAll);
 
-    return $mdMedia;
+  return $mdMedia;
 
-    function $mdMedia(query) {
-        query = validate(query);
-        var result;
-        if (!angular.isDefined(result = cache.get(query)) ) {
-            return add(query);
-        }
-        return result;
+  function $mdMedia(query) {
+    var validated = queriesCache.get(query);
+    if (angular.isUndefined(validated)) {
+      validated = queriesCache.put(query, validate(query));
     }
 
-    function validate(query) {
-        return $mdConstant.MEDIA[query] || (
-                query.charAt(0) != '(' ?  ('(' + query + ')') : query
-            );
+    var result = resultsCache.get(validated);
+    if (angular.isUndefined(result)) {
+      result = add(validated);
     }
 
-    function add(query) {
-        return cache.put(query, !!$window.matchMedia(query).matches);
+    return result;
+  }
 
+  function validate(query) {
+    return $mdConstant.MEDIA[query] ||
+           ((query.charAt(0) !== '(') ? ('(' + query + ')') : query);
+  }
+
+  function add(query) {
+    return resultsCache.put(query, !!$window.matchMedia(query).matches);
+  }
+
+  function updateAll() {
+    var keys = cache.keys();
+    if (keys.length) {
+      for (var i = 0, ii = keys.length; i < ii; i++) {
+        cache.put(keys[i], !!$window.matchMedia(keys[i]).matches);
+      }
+      // trigger a $digest()
+      $timeout(angular.noop);
     }
-
-    function updateAll() {
-        var keys = cache.keys();
-        if (keys.length) {
-            for (var i = 0, ii = keys.length; i < ii; i++) {
-                cache.put(keys[i], !!$window.matchMedia(keys[i]).matches);
-            }
-            // trigger a $digest()
-            $timeout(angular.noop);
-        }
-    }
-
+  }
 }
