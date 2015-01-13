747c6acb1835ce388215d8ecc0794ec4da67a43b
Ryan Schmukler <rschmukler@gmail.com>
2015-01-13 18:54:56 -0500

    fix(input): fix label inputs with specified types
    closes #993

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -216,6 +216,9 @@ function inputTextareaDirective($mdUtil, $window, $compile, $animate) {
         containerCtrl.setFocused(true);
       })
       .on('blur', function(e) {
+        if (e.target && e.target.validity) {
+          containerCtrl.setHasValue(e.target.validity.badInput);
+        }
         containerCtrl.setFocused(false);
       });
 
