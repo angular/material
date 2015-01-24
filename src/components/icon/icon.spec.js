1a2d2f7729b3ee018dba9d96a792830ebc80506d
Justin DuJardin <justin@dujardinconsulting.com>
2015-01-24 12:57:01 -0800

    test(icon): test loading icons as svg and img
    - load an svg icon from the fixtures path and verify it is properly embedded in icon container.

--- src/components/icon/icon.spec.js
+++ src/components/icon/icon.spec.js
@@ -1,2 +1,34 @@
-describe('mdIcon directive', function() {
+describe('md-icon directive', function() {
+  beforeEach(module('material.components.icon'));
+
+  var validIconUrl = '/fixtures/logo.svg';
+
+  var $rootScope = null;
+  var $compile = null;
+  var $document = null;
+  beforeEach(inject(function($injector) {
+    $rootScope = $injector.get('$rootScope').$new();
+    $compile = $injector.get('$compile');
+    $document = $injector.get('$document');
+  }));
+
+  afterEach(function() {
+    $rootScope.$destroy();
+  });
+
+  it('should load svg icon from file', function() {
+    var el = $compile('<md-icon icon="' + validIconUrl + '"></md-icon>')($rootScope);
+    $rootScope.$apply();
+    waitsFor(function checkLoaded() {
+      return el.find('svg').length > 0  && el.find('path').length > 0;
+    },1500);
+  });
+
+  it('should load static svg icon in img container', function() {
+    var el = $compile('<md-icon type="image" icon="' + validIconUrl + '"></md-icon>')($rootScope);
+    $rootScope.$apply();
+    waitsFor(function checkLoaded() {
+      return el.find('img').length > 0;
+    },1500);
+  });
 });
