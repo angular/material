cecc1e32d108e2b6d996fc725e1c35b9d9e03ec1
Justin DuJardin <justin@dujardinconsulting.com>
2015-01-24 12:53:31 -0800

    feat(icon): load icons as inline svg or img elements
     - Use XMLHttpRequest to load SVG as XML and inject into directive.  Note this will not support executing attached SVG scripts.
     - Use <img> element to load as static (not CSS style capable) SVG images.  This may be desirable in situations dealing with many copies of complex SVG icons, to avoid exploding the element count in the DOM.

--- src/components/icon/icon.js
+++ src/components/icon/icon.js
@@ -10,7 +10,7 @@
 angular.module('material.components.icon', [
   'material.core'
 ])
-  .directive('mdIcon', mdIconDirective);
+.directive('mdIcon', mdIconDirective);
 
 /*
  * @ngdoc directive
@@ -22,21 +22,43 @@ angular.module('material.components.icon', [
  * @description
  * The `<md-icon>` directive is an element useful for SVG icons
  *
+ * @param {string} md-type The type of icon to display, either `image` or `svg`.  If the attribute
+ * is not present, the default will be `svg`.  When using the `image` type, the icon will not be applicable
+ * for customization with CSS styles, but may perform better in some cases.
+ *
  * @usage
  * <hljs lang="html">
- *  <md-icon icon="/img/icons/ic_access_time_24px.svg">
- *  </md-icon>
+ *  <md-icon icon="/img/icons/ic_access_time_24px.svg"></md-icon>
+ *  <md-icon icon="/img/icons/ic_access_time_24px.svg" type="image"></md-icon>
  * </hljs>
  *
  */
 function mdIconDirective() {
   return {
     restrict: 'E',
-    template: '<object class="md-icon"></object>',
+    template: '<div class="md-icon"/>',
     compile: function(element, attr) {
-      var object = angular.element(element[0].children[0]);
-      if(angular.isDefined(attr.icon)) {
-        object.attr('data', attr.icon);
+      if(!angular.isDefined(attr.icon)) {
+        return;
+      }
+      var type = angular.isDefined(attr.type) ? attr.type : 'svg';
+      var child = angular.element(element[0].children[0]);
+      if(type === 'svg') {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function loadIconState() {
+          if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200 && httpRequest.responseXML instanceof Document) {
+              var svg = angular.element(httpRequest.responseXML).find('svg');
+              child.replaceWith(svg);
+            }
+          }
+        };
+        httpRequest.open('GET', attr.icon);
+        httpRequest.send();
+      }
+      else if(type === 'image') {
+        var img = angular.element('<img src="' + attr.icon + '"/>');
+        child.replaceWith(img);
       }
     }
   };
