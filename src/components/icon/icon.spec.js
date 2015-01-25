5275d40439c5f67907567cfcb043fd2f7044e78b
Justin DuJardin <justin@dujardinconsulting.com>
2015-01-25 12:46:30 -0800

    test(icon): add test for default icon type
    - dry up tests with a setup fixture to make implementing more of them simple.

--- src/components/icon/icon.spec.js
+++ src/components/icon/icon.spec.js
@@ -6,29 +6,46 @@ describe('md-icon directive', function() {
   var $rootScope = null;
   var $compile = null;
   var $document = null;
+
+  function setup(type,url,then) {
+    var el = $compile('<md-icon type="' + type + '" icon="' + url + '"></md-icon>')($rootScope);
+    $rootScope.$apply();
+    angular.element($document[0].body).append(el);
+    waitsFor(function() {
+      var loaded = type === 'image' ? el.find('img')[0].complete : el.find('svg').children().length > 0;
+      if(loaded){
+        el.detach();
+        then(el);
+      }
+      return loaded;
+    },1500);
+  }
+
   beforeEach(inject(function($injector) {
     $rootScope = $injector.get('$rootScope').$new();
     $compile = $injector.get('$compile');
     $document = $injector.get('$document');
   }));
-
   afterEach(function() {
     $rootScope.$destroy();
   });
 
+  it('defaults to svg if type is not specified', function(){
+    setup('svg',validIconUrl,function(el) {
+      expect(el.find('svg').length).toBeGreaterThan(0);
+      expect(el.find('path').length).toBeGreaterThan(0);
+    });
+  });
+
   it('should load svg icon from file', function() {
-    var el = $compile('<md-icon icon="' + validIconUrl + '"></md-icon>')($rootScope);
-    $rootScope.$apply();
-    waitsFor(function checkLoaded() {
-      return el.find('svg').length > 0  && el.find('path').length > 0;
-    },1500);
+    setup('svg',validIconUrl,function(el) {
+      expect(el.find('svg').length).toBeGreaterThan(0);
+    });
   });
 
   it('should load static svg icon in img container', function() {
-    var el = $compile('<md-icon type="image" icon="' + validIconUrl + '"></md-icon>')($rootScope);
-    $rootScope.$apply();
-    waitsFor(function checkLoaded() {
-      return el.find('img').length > 0;
-    },1500);
+    setup('image',validIconUrl,function(el) {
+      expect(el.find('img').length).toBeGreaterThan(0);
+    });
   });
 });
