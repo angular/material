c6d24eb2dfb7c3c23f08e09ae7f22812fe395516
Andrew <andrewtjoslin@gmail.com>
2015-01-28 14:39:44 -0800

    fix(gesture): make sure clicks properly support keys

--- src/core/services/gesture/gesture.js
+++ src/core/services/gesture/gesture.js
@@ -141,7 +141,7 @@ angular.module('material.core')
     },
     onEnd: function(ev, pointer) {
       if (pointer.distance < this.state.options.maxDistance) {
-        this.dispatchEvent(ev, 'click');
+        this.dispatchEvent(ev, 'click', null, ev);
       }
     }
   });
@@ -355,16 +355,25 @@ angular.module('material.core')
   /*
    * NOTE: dispatchEvent is very performance sensitive. 
    */
-  function dispatchEvent(srcEvent, eventType, eventPointer) {
+  function dispatchEvent(srcEvent, eventType, eventPointer, /*original DOMEvent */ev) {
     eventPointer = eventPointer || pointer;
     var eventObj;
 
     if (eventType === 'click') {
       eventObj = document.createEvent('MouseEvents');
-      eventObj.initMouseEvent('click', true, true, window, {},
-                              eventPointer.x, eventPointer.y,
-                              eventPointer.x, eventPointer.y,
-                              false, false, false, false, 0, null);
+      eventObj.initMouseEvent(
+        'click', true, true, window, ev.detail,
+        ev.screenX, ev.screenY, ev.clientX, ev.clientY, 
+        ev.ctrlKey, ev.altKey, ev.shiftKey, ev.metaKey,
+        ev.button, ev.relatedTarget || null
+      );
+      angular.extend(eventObj, {
+        which: ev.which,
+        x: ev.x, y: ev.y,
+        offsetX: ev.offsetX, offsetY: ev.offsetY,
+        layerX: ev.layerX, layerY: ev.layerY
+      });
+
     } else {
       eventObj = document.createEvent('CustomEvent');
       eventObj.initCustomEvent(eventType, true, true, {});
