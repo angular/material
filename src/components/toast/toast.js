08eb68fd5e2586de96c47b8491e6f2ff04436464
Ryan Schmukler <rschmukler@gmail.com>
2015-01-29 14:43:34 -0800

    docs(toast): fix typo

--- src/components/toast/toast.js
+++ src/components/toast/toast.js
@@ -49,7 +49,7 @@ function MdToastDirective() {
  * app.controller('MyController', function($scope, $mdToast) {
  *   $scope.openToast = function($event) {
  *     $mdToast.show($mdToast.simple().content('Hello!'));
- *     // Could also do $mdtoast.showSimple('Hello');
+ *     // Could also do $mdToast.showSimple('Hello');
  *   };
  * });
  * </hljs>
