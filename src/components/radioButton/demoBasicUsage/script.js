5586db46cc8542d57dab90b917816c0cc859926e
Marcy Sutton <marcy@substantial.com>
2015-02-08 20:58:19 -0800

    docs(mdRadioButton): demos ariaLabel in use
    Refs #1420.

--- src/components/radioButton/demoBasicUsage/script.js
+++ src/components/radioButton/demoBasicUsage/script.js
@@ -1,31 +1,50 @@
 
-angular.module('radioDemo1', ['ngMaterial'])
+angular
+  .module('radioDemo1', ['ngMaterial'])
+  .controller('AppCtrl', function($scope) {
 
-.controller('AppCtrl', function($scope) {
+    $scope.data = {
+      group1 : 'Banana',
+      group2 : '2',
+      group3 : 'avatar-1'
+    };
 
-  $scope.data = {
-    group1 : 'Banana',
-    group2 : '2'
-  };
+    $scope.avatarData = [{
+        id: "svg-1",
+        title: 'avatar 1',
+        value: 'avatar-1'
+      },{
+        id: "svg-2",
+        title: 'avatar 2',
+        value: 'avatar-2'
+      },{
+        id: "svg-3",
+        title: 'avatar 3',
+        value: 'avatar-3'
+    }];
 
-  $scope.radioData = [
-    { label: '1', value: 1 },
-    { label: '2', value: 2 },
-    { label: '3', value: '3', isDisabled: true },
-    { label: '4', value: '4' }
-  ];
+    $scope.radioData = [
+      { label: '1', value: 1 },
+      { label: '2', value: 2 },
+      { label: '3', value: '3', isDisabled: true },
+      { label: '4', value: '4' }
+    ];
 
-  $scope.submit = function() {
-    alert('submit');
-  };
 
-  $scope.addItem = function() {
-    var r = Math.ceil(Math.random() * 1000);
-    $scope.radioData.push({ label: r, value: r });
-  };
+    $scope.submit = function() {
+      alert('submit');
+    };
 
-  $scope.removeItem = function() {
-    $scope.radioData.pop();
-  };
+    $scope.addItem = function() {
+      var r = Math.ceil(Math.random() * 1000);
+      $scope.radioData.push({ label: r, value: r });
+    };
 
-});
+    $scope.removeItem = function() {
+      $scope.radioData.pop();
+    };
+
+  })
+  .config(function($mdIconProvider) {
+    $mdIconProvider.defaultIconSet('icons/avatar-icons.svg');
+  });
