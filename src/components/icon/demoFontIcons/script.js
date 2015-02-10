ffa11c73815d55e3692ff2af015fdf0c851170fc
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-02-10 17:26:15 -0600

    update(demos): icon demos renamed

--- src/components/icon/demoFontIcons/script.js
+++ src/components/icon/demoFontIcons/script.js
@@ -1,6 +1,7 @@
 
 angular.module('appDemoFontIcons', ['ngMaterial'])
 .controller('DemoCtrl', function( $scope ) {
+    // Create list of font-icon names with color overrides
     var iconData = [
           {name: 'icon-home'        , color: "#777" },
           {name: 'icon-user-plus'   , color: "rgb(89, 226, 168)" },
@@ -9,6 +10,7 @@ angular.module('appDemoFontIcons', ['ngMaterial'])
           {name: 'icon-settings'    , color:"black" }
         ];
 
+    // Create a set of sizes...
     $scope.sizes = [12, 21, 36, 48];
     $scope.fonts = [].concat(iconData);
 
