c93ba1a677d734cbbdc91e28aeea65cf8cb6ed05
Tony Childs <tony.childs@gmail.com>
2015-02-10 06:13:20 -0600

    update(icon): add demo of $templateCache usage

--- src/components/icon/demoSvgIconSets/script.js
+++ src/components/icon/demoSvgIconSets/script.js
@@ -1,12 +1,6 @@
 
 angular.module('appSvgIconSets', ['ngMaterial'])
-  .controller('DemoCtrl', function($scope) {
-    $scope.sizes = [12, 14, 16, 18, 21, 24, 36, 48, 60, 72];
-    $scope.limit = 10;
-    $scope.color = '#00bcd4';
-    $scope.iconType = 'Font';
-    $scope.loading = false;
-  })
+  .controller('DemoCtrl', function($scope) {})
   .config(function($mdIconProvider) {
     $mdIconProvider.iconSet('social', 'img/icons/sets/social-icons.svg')
                    .defaultIconSet('img/icons/sets/core-icons.svg');
