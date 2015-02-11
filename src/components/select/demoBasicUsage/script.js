a9c16e0c2aa54f41ca55261ac9ce81fbbdbc8bfc
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-02-10 22:00:55 -0600

    move select component to WIP branch

--- src/components/select/demoBasicUsage/script.js
+++
@@ -1,4 +0,0 @@
-angular.module('selectDemoBasic', ['ngMaterial'])
-.controller('AppCtrl', function($scope) {
-  $scope.neighborhoods = ['Chelsea', 'Financial District', 'Midtown', 'West Village', 'Williamsburg'];
-});
