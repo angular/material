8d9fbaca3c4c203af4a99b7ee55b5ed0d4d3723a
Chris Griffith <chris.griffith@gmail.com>
2015-02-13 18:41:53 -0800

    Fixed a typo
    The attribute aria-label was referenced as ariaLabel.

--- src/components/radioButton/radioButton.js
+++ src/components/radioButton/radioButton.js
@@ -190,7 +190,7 @@ function mdRadioGroupDirective($mdUtil, $mdConstant, $mdTheming) {
  *    be set when selected.*
  * @param {string} value The value to which the expression should be set when selected.
  * @param {string=} name Property name of the form under which the control is published.
- * @param {string=} ariaLabel Adds label to radio button for accessibility.
+ * @param {string=} aria-label Adds label to radio button for accessibility.
  * Defaults to radio button's text. If no text content is available, a warning will be logged.
  *
  * @usage
