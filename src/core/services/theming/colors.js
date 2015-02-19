07e7ef079aeb9617e446b76d787c57338e00cb94
Sander Elias <SanderElias@users.noreply.github.com>
2015-02-19 16:34:18 +0100

    Reworked the file according to recomondations
    replaced $inject=[xx] with /* @ngInject */ 
    removed an module global
    touched up the regex

--- src/core/services/theming/colors.js
+++ src/core/services/theming/colors.js
@@ -1,8 +1,6 @@
 (function () {
     "use strict";
 
-    //module global to keep an reference to the dynamic style sheet
-    var customSheet;
     //module global to available color pallettes
     var colorStore     = {};
     var defaultPalette = '';
@@ -13,9 +11,9 @@
  * @module material.core.color
  *
  * @description
- * `$mdColors` makes it possible to use the colors of any palette to be available
+ * $mdColors makes it possible to use the colors of any palette to be available
  *  as CSS rules in the form of md-bg-[colorname] and md-fg-[colorname] (as foreground and background)
- *  If you want to activate another palette you can use the $mdColor.loadPalette(palletteName) method.
+ *  If you want to activate another palette you can use the `$mdColor.loadPalette(palletteName)` method.
  *
  *  the $mdColors.palettes array lists all available palettes
  *  the $mdColors.palette property lists the current selected palette
@@ -30,7 +28,7 @@
         .config(configColors)
         .run(loadDefaults);
 
-    configColors.$inject = ['$mdThemingProvider'];
+     /* @ngInject */
     function configColors($mdThemingProvider) {
         // fetch the colors out of the theming provider
         Object.keys($mdThemingProvider._PALETTES).forEach(parsePalette);
@@ -49,22 +47,22 @@
 
             function copyColors(colorName) {
                 // use an regex to look for hex colors, ignore the rest
-                if (/#[0-9A-Fa-f]{6}|0-9A-Fa-f]{8}\b/.exec(palette[colorName])) {
+                if (/#[0-9A-Fa-f]{3}|[0-9A-Fa-f]{6}\b/.test(palette[colorName])) {
                     colors.push({color:colorName,value:palette[colorName]});
                 }
             }
         }
     }
 
-    loadDefaults.$inject=['$mdColors'];
+    /* @ngInject */
     function loadDefaults($mdColors) {
         // this sets the default that is stored in the config-fase into the service!
         $mdColors.palettes = Object.keys(colorStore);
         $mdColors.loadPalette(defaultPalette);
     }
 
-    mdColors.$inject = ['$interpolate'];
-    function mdColors ($interpolate) {
+    /* @ngInject */;
+    function mdColors ($interpolate, $document) {
         // wrap all of the above up in a reusable service.
         var service        = this;
         service.palette    = defaultPalette;
@@ -86,16 +84,11 @@
         function createStyleSheet () {
             var colors = service.colors;
             var fg, bg;
-            if (typeof customSheet === 'undefined') {
-                // use closure for caching the styleSheet
-                newStyleSheet();
-            } else {
-                // remove existing rules
-                // TODO: look into disabling/enabling pre-build style-guides
-                //       in stead of delete and recreate!
-                while (customSheet.cssRules.length > 0 ) {
-                    customSheet.deleteRule(0);
-                }
+            var customSheet = getStyleSheet();
+
+            // clear out old rules from stylesheet
+            while (customSheet.cssRules.length > 0 ) {
+                customSheet.deleteRule(0);
             }
 
             // set up interpolation functions to build css rules.
@@ -109,21 +102,20 @@
                 customSheet.insertRule(bg(color));
             });
         }
-    }
+        
+        function getStyleSheet() {
+            // function to ad an dynamic style-sheet to the document
+            var style = $document.querySelector('style[title="Dynamic-Generated-by-$mdColors"]');
+            if (style === null) {
+                style   = $document.createElement('style');
+                style.title = 'Dynamic-Generated-by-$mdColors';
+                // WebKit hack... (not sure if still needed)
+                style.appendChild($document.createTextNode(''));
+                $document.head.appendChild(style);
+            }
+            return style.sheet;
+        }
 
-    function newStyleSheet() {
-        // function to ad an dynamic style-sheet to the document
-        var style   = document.createElement("style");
-        style.title = 'Dynamic Generated my Angular-Material';
-        // WebKit hack... (not sure if still needed)
-        style.appendChild(document.createTextNode(""));
-
-        document.head.appendChild(style);
-        // store the sheet in the closure for reuse
-        // creating a new sheet is a 'costly' operation, and I
-        // just need one.
-        customSheet = style.sheet;
-        return style.sheet;
     }
 
 }());
