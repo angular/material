6dd1c34a525e9ef52e815fc2b4ac8ac8b7643d9c
Ryan Schmukler <rschmukler@gmail.com>
2015-03-24 16:57:38 -0400

    chore(docs): update docs angular version to 1.3.15

--- config/build.config.js
+++ config/build.config.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var versionFile = __dirname + '/../dist/commit';
 
 module.exports = {
-  ngVersion: '1.3.2',
+  ngVersion: '1.3.15',
   version: pkg.version,
   repository: pkg.repository.url
     .replace(/^git/,'https')
