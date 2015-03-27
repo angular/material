afa47169d8da3c69bc1fb56f17d29153d25e6d53
Marcy Sutton <marcy@substantial.com>
2015-03-27 14:39:15 -0700

    chore(bottomSheet): use md-icon in demos

--- src/components/bottomSheet/demoBasicUsage/script.js
+++ src/components/bottomSheet/demoBasicUsage/script.js
@@ -1,5 +1,17 @@
 angular.module('bottomSheetDemo1', ['ngMaterial'])
-
+.config(function($mdIconProvider) {
+    $mdIconProvider
+      .icon('share-arrow', 'img/icons/share-arrow.svg', 24)
+      .icon('upload', 'img/icons/upload.svg', 24)
+      .icon('copy', 'img/icons/copy.svg', 24)
+      .icon('print', 'img/icons/print.svg', 24)
+      .icon('hangout', 'img/icons/hangout.svg', 24)
+      .icon('mail', 'img/icons/mail.svg', 24)
+      .icon('message', 'img/icons/message.svg', 24)
+      .icon('copy2', 'img/icons/copy2.svg', 24)
+      .icon('facebook', 'img/icons/facebook.svg', 24)
+      .icon('twitter', 'img/icons/twitter.svg', 24);
+  })
 .controller('BottomSheetExample', function($scope, $timeout, $mdBottomSheet) {
   $scope.alert = '';
 
@@ -29,7 +41,7 @@ angular.module('bottomSheetDemo1', ['ngMaterial'])
 .controller('ListBottomSheetCtrl', function($scope, $mdBottomSheet) {
 
   $scope.items = [
-    { name: 'Share', icon: 'share' },
+    { name: 'Share', icon: 'share-arrow' },
     { name: 'Upload', icon: 'upload' },
     { name: 'Copy', icon: 'copy' },
     { name: 'Print this page', icon: 'print' },
@@ -41,12 +53,11 @@ angular.module('bottomSheetDemo1', ['ngMaterial'])
   };
 })
 .controller('GridBottomSheetCtrl', function($scope, $mdBottomSheet) {
-
   $scope.items = [
     { name: 'Hangout', icon: 'hangout' },
     { name: 'Mail', icon: 'mail' },
     { name: 'Message', icon: 'message' },
-    { name: 'Copy', icon: 'copy' },
+    { name: 'Copy', icon: 'copy2' },
     { name: 'Facebook', icon: 'facebook' },
     { name: 'Twitter', icon: 'twitter' },
   ];
@@ -55,4 +66,24 @@ angular.module('bottomSheetDemo1', ['ngMaterial'])
     var clickedItem = $scope.items[$index];
     $mdBottomSheet.hide(clickedItem);
   };
-});
+})
+.run(function($http, $templateCache) {
+
+    var urls = [
+      'img/icons/share-arrow.svg',
+      'img/icons/upload.svg',
+      'img/icons/copy.svg',
+      'img/icons/print.svg',
+      'img/icons/hangout.svg',
+      'img/icons/mail.svg',
+      'img/icons/message.svg',
+      'img/icons/copy2.svg',
+      'img/icons/facebook.svg',
+      'img/icons/twitter.svg'
+    ];
+
+    angular.forEach(urls, function(url) {
+      $http.get(url, {cache: $templateCache});
+    });
+
+  });
