4d714365d6dd5de29ac77e634afeccd6958911b6
Ryan Schmukler <rschmukler@gmail.com>
2015-04-05 15:38:39 -0400

    test(list): migrate tests to new list style

--- src/components/list/list.spec.js
+++ src/components/list/list.spec.js
@@ -1,4 +1,4 @@
-describe('mdItem directive', function() {
+describe('mdListItem directive', function() {
   beforeEach(module('material.components.list', 'material.components.checkbox', 'material.components.switch'));
 
   function setup(html) {
@@ -11,19 +11,19 @@ describe('mdItem directive', function() {
   }
 
   it('forwards click events for md-checkbox', inject(function($rootScope) {
-    var listItem = setup('<md-item><md-checkbox ng-model="modelVal"></md-checkbox></md-item>');
+    var listItem = setup('<md-list-item><md-checkbox ng-model="modelVal"></md-checkbox></md-list-item>');
     listItem[0].querySelector('div').click();
     expect($rootScope.modelVal).toBe(true);
   }));
 
   it('forwards click events for md-switch', inject(function($rootScope) {
-    var listItem = setup('<md-item><md-switch ng-model="modelVal"></md-switch></md-item>');
+    var listItem = setup('<md-list-item><md-switch ng-model="modelVal"></md-switch></md-list-item>');
     listItem[0].querySelector('div').click();
     expect($rootScope.modelVal).toBe(true);
   }));
 
   it('creates buttons when used with ng-click', function() {
-    var listItem = setup('<md-item ng-click="sayHello()"><p>Hello world</p></md-item>');
+    var listItem = setup('<md-list-item ng-click="sayHello()"><p>Hello world</p></md-list-item>');
     var firstChild = listItem.children()[0];
     expect(firstChild.nodeName).toBe('BUTTON');
     expect(firstChild.childNodes[0].nodeName).toBe('DIV');
@@ -31,7 +31,7 @@ describe('mdItem directive', function() {
   });
 
   it('moves md-secondary items outside of the button', function() {
-    var listItem = setup('<md-item ng-click="sayHello()"><p>Hello World</p><md-icon class="md-secondary" ng-click="goWild()"></md-icon></md-item>');
+    var listItem = setup('<md-list-item ng-click="sayHello()"><p>Hello World</p><md-icon class="md-secondary" ng-click="goWild()"></md-icon></md-list-item>');
     var firstChild = listItem.children()[0];
     expect(firstChild.nodeName).toBe('BUTTON');
     expect(firstChild.childNodes.length).toBe(1);
