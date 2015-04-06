bdf03c1f05b914a851f44d2d30ce09a268b33f5c
Marcy Sutton <marcy@substantial.com>
2015-04-06 16:45:35 -0700

    docs(): fix divider demo, mixed content warnings
    Closes #2186

--- src/components/divider/demoBasicUsage/script.js
+++ src/components/divider/demoBasicUsage/script.js
@@ -1,31 +1,31 @@
 angular.module('dividerDemo1', ['ngMaterial'])
   .controller('AppCtrl', function($scope) {
     $scope.messages = [{
-      face : 'http://lorempixel.com/50/50/people',
+      face : '/img/list/60.jpeg',
       what: 'Brunch this weekend?',
       who: 'Min Li Chan',
       when: '3:08PM',
       notes: " I'll be in your neighborhood doing errands"
     }, {
-      face : 'http://lorempixel.com/50/50/people',
+      face : '/img/list/60.jpeg',
       what: 'Brunch this weekend?',
       who: 'Min Li Chan',
       when: '3:08PM',
       notes: " I'll be in your neighborhood doing errands"
     }, {
-      face : 'http://lorempixel.com/50/50/people',
+      face : '/img/list/60.jpeg',
       what: 'Brunch this weekend?',
       who: 'Min Li Chan',
       when: '3:08PM',
       notes: " I'll be in your neighborhood doing errands"
     }, {
-      face : 'http://lorempixel.com/50/50/people',
+      face : '/img/list/60.jpeg',
       what: 'Brunch this weekend?',
       who: 'Min Li Chan',
       when: '3:08PM',
       notes: " I'll be in your neighborhood doing errands"
     }, {
-      face : 'http://lorempixel.com/50/50/people',
+      face : '/img/list/60.jpeg',
       what: 'Brunch this weekend?',
       who: 'Min Li Chan',
       when: '3:08PM',
