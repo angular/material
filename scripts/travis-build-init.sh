50c8581d219969fe4c3f55fc6e966c7f4ab3beab
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-04-10 11:56:11 -0500

    update(trave): use node 0.12, fix travis-build-init.sh

--- scripts/travis-build-init.sh
+++ scripts/travis-build-init.sh
@@ -21,7 +21,7 @@ function run {
 
   NEW_VERSION="$(readJsonProp "package.json" "version")-master-$(echo $SHA | head -c 7)"
 
-  ./scripts/bower-release.sh --version=$NEW_VERSION
+  ./scripts/bower-material-release.sh --version=$NEW_VERSION
 }
 
 source $(dirname $0)/utils.inc
