17151c9475407f59bd4b56b1681254d7c3a5740a
Ryan Schmukler <rschmukler@gmail.com>
2015-04-12 17:32:29 -0400

    amend(list): add md-offset class for list-item text, fix offset, add demo
    closes #2210

--- src/components/list/demoBasicUsage/script.js
+++ src/components/list/demoBasicUsage/script.js
@@ -2,6 +2,10 @@
 angular.module('listDemo1', ['ngMaterial'])
 
 .controller('AppCtrl', function($scope) {
+    $scope.phones = [
+      { type: 'Home', number: '(555) 251-1234' },
+      { type: 'Cell', number: '(555) 786-9841' },
+    ];
     $scope.todos = [
       {
         face : '/img/list/60.jpeg',
