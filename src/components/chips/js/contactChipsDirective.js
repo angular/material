eb9f56466a02e44321c8fa50c09f5689836a5be3
Robert Messerle <rmesserle@google.com>
2015-04-13 12:46:44 -0700

    fix(chips): sets `md-autoselect` for contact chips
    Closes #2294

--- src/components/chips/js/contactChipsDirective.js
+++ src/components/chips/js/contactChipsDirective.js
@@ -59,6 +59,7 @@
               md-items="item in $mdContactChipsCtrl.queryContact($mdContactChipsCtrl.searchText)"\
               md-item-text="$mdContactChipsCtrl.mdContactName"\
               md-no-cache="$mdContactChipsCtrl.filterSelected"\
+              md-autoselect\
               placeholder="{{$mdContactChipsCtrl.contacts.length == 0 ?\
                   $mdContactChipsCtrl.placeholder : $mdContactChipsCtrl.secondaryPlaceholder}}">\
             <div class="md-contact-suggestion">\
