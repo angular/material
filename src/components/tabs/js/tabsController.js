48eeb627ddf497a241ec5e4686f333d5b56769fe
Robert Messerle <rmesserle@google.com>
2015-04-13 12:48:54 -0700

    fix(tabs): fixes flicker issue with dynamic height

--- src/components/tabs/js/tabsController.js
+++ src/components/tabs/js/tabsController.js
@@ -216,10 +216,8 @@
             { height: newHeight + 'px'}
           )
           .then(function () {
-            $timeout(function () {
-              $element.css('height', '');
-              locked = false;
-            }, 0, false);
+            $element.css('height', '');
+            locked = false;
           });
     }
 
