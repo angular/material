17aecd2dbc3acfe48006ff5adb12cad5e15aa70b
Robert Messerle <rmesserle@google.com>
2015-04-15 11:59:09 -0700

    fix(tabs): cleans up label/template fetch logic

--- src/components/tabs/js/tabDirective.js
+++ src/components/tabs/js/tabDirective.js
@@ -87,14 +87,25 @@
       scope.$on('$destroy', function () { ctrl.removeTab(data); });
 
       function getLabel () {
-        return attr.label || (element.find('md-tab-label')[0] || element[0]).innerHTML;
+        var label = attr.label || (element.find('md-tab-label')[0] || element[0]).innerHTML;
+        return getLabelAttribute() || getLabelElement() || getElementContents();
+        function getLabelAttribute () { return attr.label; }
+        function getLabelElement () {
+          var label = element.find('md-tab-label');
+          if (label.length) return label.remove().html();
+        }
+        function getElementContents () {
+          var html = element.html();
+          element.empty();
+          return html;
+        }
       }
 
       function getTemplate () {
         var content = element.find('md-tab-template'),
             template = content.length ? content.html() : attr.label ? element.html() : null;
         if (content.length) content.remove();
-        else element.empty();
+        else if (attr.label) element.empty();
         return template;
       }
     }
