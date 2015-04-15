2d4f3588c66b6af2d15655732da52fee46081d0a
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-04-15 09:46:18 -0500

    update(sidenav): change to element focus when animations are done

--- src/components/sidenav/demoBasicUsage/script.js
+++ src/components/sidenav/demoBasicUsage/script.js
@@ -1,39 +1,38 @@
+angular
+  .module('sidenavDemo1', ['ngMaterial'])
+  .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
 
-angular.module('sidenavDemo1', ['ngMaterial'])
+    $scope.toggleLeft = buildToggler('left');
+    $scope.toggleRight = buildToggler('right');
 
-.controller('AppCtrl', function($scope, $timeout, $mdSidenav, $log) {
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildToggler(navID) {
+      return function() {
+        return $mdSidenav(navID).toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }
+    }
 
-  $scope.toggleLeft = function() {
-    $mdSidenav('left').toggle()
-                      .then(function(){
-                          $log.debug("toggle left is done");
-                      });
-  };
+  })
+  .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
 
-  $scope.toggleRight = function() {
-
-    $mdSidenav('right').toggle()
-                        .then(function(){
-                          $log.debug("toggle RIGHT is done");
-                        });
-  };
-})
-
-.controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
-  $scope.close = function() {
-    $mdSidenav('left').close()
-                      .then(function(){
-                        $log.debug("close LEFT is done");
-                      });
-
-  };
-})
-
-.controller('RightCtrl', function($scope, $timeout, $mdSidenav, $log) {
-  $scope.close = function() {
-    $mdSidenav('right').close()
-                        .then(function(){
-                          $log.debug("close RIGHT is done");
-                        });
-  };
-});
+    };
+  })
+  .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+      $mdSidenav('right').close()
+        .then(function () {
+          $log.debug("close RIGHT is done");
+        });
+    };
+  });
--- src/components/sidenav/sidenav.js
+++ src/components/sidenav/sidenav.js
@@ -291,13 +291,15 @@ function SidenavDirective($timeout, $animate, $parse, $log, $mdMedia, $mdConstan
       disableParentScroll(isOpen);
 
       return promise = $q.all([
-        isOpen ? $animate.enter(backdrop, parent) : $animate.leave(backdrop),
-        $animate[isOpen ? 'removeClass' : 'addClass'](element, 'md-closed').then(function() {
-          if (scope.isOpen) {
-            focusEl && focusEl.focus();
-          }
-        })
-      ]);
+                isOpen ? $animate.enter(backdrop, parent) : $animate.leave(backdrop),
+                $animate[isOpen ? 'removeClass' : 'addClass'](element, 'md-closed')
+              ])
+              .then(function() {
+                // Perform focus when animations are ALL done...
+                if (scope.isOpen) {
+                  focusEl && focusEl.focus();
+                }
+              });
     }
 
     /**
