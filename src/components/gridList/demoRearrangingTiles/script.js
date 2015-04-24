87bca5af4e9af3a70f38d3528eb812ca8dca744e
tarnas14 <tarnas14@gmail.com>
2015-04-24 17:56:37 +0100

    refactor(gridList): make demo code more readable
    rename functions and format code correctly

--- src/components/gridList/demoRearrangingTiles/script.js
+++ src/components/gridList/demoRearrangingTiles/script.js
@@ -3,29 +3,29 @@ angular
   .module('gridListDemoApp', ['ngMaterial'])
   .controller('gridListDemoCtrl', function($scope) {
 
-    this.order = 0;
+    this.order = undefined;
 
     this.tiles = buildGridModel();
     this.sortTiles = sortTiles;
-    this.getTiles = function() {
+    this.getTiles = getTiles;
+
+
+    function getTiles() {
       return this.tiles;
-    }.bind(this);
-
-    function sortTiles() {
-      console.log(this.order);
-      this.tiles.sort(function(a, b) {
-        return b.name - a.name;
-      });
-
-      if (this.order != 0) {
-        this.tiles.reverse();
-      }
-
-      console.log(this.tiles);
     };
 
-    function buildGridModel(){
-      var tiles = [{name: 1},{ name:2},{ name:3},{ name:4},{ name:5},{ name:6},{ name:7}];
+    function sortTiles() {
+      this.tiles.sort(function(a, b) {
+        return a.val - b.val;
+      });
+
+      if (this.order !== 0) {
+        this.tiles.reverse();
+      }
+    };
+
+    function buildGridModel() {
+      var tiles = [{val: 1},{ val:4},{ val:3},{ val:7},{ val:5},{ val:2},{ val:6}];
 
       return tiles;
     }
