cb550871f5cbfc75828e6096bd0f7446f3e512f0
Arlen Anderson <anderson.arlen@gmail.com>
2015-04-28 19:18:28 -0600

    fixed md-autocomplete showing loading indicator when selecting an item with noCache turned on.

--- src/components/autocomplete/js/autocompleteController.js
+++ src/components/autocomplete/js/autocompleteController.js
@@ -322,9 +322,9 @@ function MdAutocompleteCtrl ($scope, $element, $mdUtil, $mdConstant, $timeout, $
     }
     function handleResults (matches) {
       cache[term] = matches;
+      self.loading = false;
       if (searchText !== $scope.searchText) return; //-- just cache the results if old request
       promise = null;
-      self.loading = false;
       self.matches = matches;
       self.hidden = shouldHide();
       updateMessages();
