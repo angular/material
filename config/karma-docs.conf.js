9ada12721988d838cc5577c2b0d4a909274a7f1a
Matthew Findley <findley@substantial.com>
2015-05-06 13:02:52 -0700

    refactor(spec): remove dependency on analytics

--- config/karma-docs.conf.js
+++ config/karma-docs.conf.js
@@ -18,7 +18,6 @@ module.exports = function(config) {
     'node_modules/angular-messages/angular-messages.js',
     'node_modules/angular-route/angular-route.js',
     'node_modules/angular-mocks/angular-mocks.js',
-    'bower_components/angularytics/dist/angularytics.js',
     'dist/angular-material.js',
     'config/test-utils.js',
     'dist/docs/docs.js',
