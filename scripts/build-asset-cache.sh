2e2bbe0ac2b7670bc8a50a7d9e2aa82b3973e9ff
Matthew Findley <findley@substantial.com>
2015-05-06 11:50:01 -0700

    docs(codepen): document build-asset-cache.sh

--- scripts/build-asset-cache.sh
+++ scripts/build-asset-cache.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Intended to generate an object literal of SVG images
+# key - relative path to file.
+# value - contents of the SVG.
+#
+# See the following guide about the process to update the asset cache:
+# https://github.com/angular/material/blob/feature/edit-example-on-codepen/docs/guides/CODEPEN.md
+
 echo {
 for i in $(find docs/app/{img/icons,icons} -type f -name *.svg); do
   filename=`echo $i | sed "s/docs\/app\///"`
