bdb793d8a35f1da415c2fcb6d02b7131bd4490df
Jeremy Elbourn <jelbourn@google.com>
2015-05-19 12:08:16 -0700

    fix(calendar): add remaining missing tests for dateUtil

--- src/components/calendar/dateUtil.spec.js
+++ src/components/calendar/dateUtil.spec.js
@@ -1,5 +1,10 @@
 
 describe('$$mdDateUtil', function() {
+  // When constructing a Date, the month is zero-based. This can be confusing, since people are
+  // used to seeing them one-based. So we create these aliases to make reading the tests easier.
+  var JAN = 0, FEB = 1, MAR = 2, APR = 3, MAY = 4, JUN = 5, JUL = 6, AUG = 7, SEP = 8, OCT = 9,
+      NOV = 10, DEC = 11;
+
   var dateUtil;
 
   beforeEach(module('material.components.calendar'));
@@ -9,7 +14,7 @@ describe('$$mdDateUtil', function() {
   }));
 
   it('should get the first day of a month', function() {
-    var first = dateUtil.getFirstDateOfMonth(new Date(1985, 9, 26));
+    var first = dateUtil.getFirstDateOfMonth(new Date(1985, OCT, 26));
 
     expect(first.getFullYear()).toBe(1985);
     expect(first.getMonth()).toBe(9);
@@ -17,7 +22,7 @@ describe('$$mdDateUtil', function() {
   });
 
   it('should get the first day of the month from the first day of the month', function() {
-    var first = dateUtil.getFirstDateOfMonth(new Date(1985, 9, 1));
+    var first = dateUtil.getFirstDateOfMonth(new Date(1985, OCT, 1));
 
     expect(first.getFullYear()).toBe(1985);
     expect(first.getMonth()).toBe(9);
@@ -26,172 +31,256 @@ describe('$$mdDateUtil', function() {
 
   it('should get the number of days in a month', function() {
     // Month with 31 days.
-    expect(dateUtil.getNumberOfDaysInMonth(new Date(2015, 0, 1))).toBe(31);
+    expect(dateUtil.getNumberOfDaysInMonth(new Date(2015, JAN, 1))).toBe(31);
 
     // Month with 30 days.
-    expect(dateUtil.getNumberOfDaysInMonth(new Date(2015, 3, 1))).toBe(30);
+    expect(dateUtil.getNumberOfDaysInMonth(new Date(2015, APR, 1))).toBe(30);
 
     // Month with 28 days
-    expect(dateUtil.getNumberOfDaysInMonth(new Date(2015, 1, 1))).toBe(28);
+    expect(dateUtil.getNumberOfDaysInMonth(new Date(2015, FEB, 1))).toBe(28);
 
     // Month with 29 days.
-    expect(dateUtil.getNumberOfDaysInMonth(new Date(2012, 1, 1))).toBe(29);
+    expect(dateUtil.getNumberOfDaysInMonth(new Date(2012, FEB, 1))).toBe(29);
   });
 
   it('should get an arbitrary day in the next month', function() {
     // Next month in the same year.
-    var next = dateUtil.getDateInNextMonth(new Date(2015, 0, 1));
+    var next = dateUtil.getDateInNextMonth(new Date(2015, JAN, 1));
     expect(next.getMonth()).toBe(1);
     expect(next.getFullYear()).toBe(2015);
 
     // Next month in the following year.
-    next = dateUtil.getDateInNextMonth(new Date(2015, 11, 1));
+    next = dateUtil.getDateInNextMonth(new Date(2015, DEC, 1));
     expect(next.getMonth()).toBe(0);
     expect(next.getFullYear()).toBe(2016);
   });
 
   it('should get an arbitrary day in the previous month', function() {
     // Previous month in the same year.
-    var next = dateUtil.getDateInPreviousMonth(new Date(2015, 6, 1));
+    var next = dateUtil.getDateInPreviousMonth(new Date(2015, JUL, 1));
     expect(next.getMonth()).toBe(5);
     expect(next.getFullYear()).toBe(2015);
 
     // Previous month in the past year.
-    next = dateUtil.getDateInPreviousMonth(new Date(2015, 0, 1));
+    next = dateUtil.getDateInPreviousMonth(new Date(2015, JAN, 1));
     expect(next.getMonth()).toBe(11);
     expect(next.getFullYear()).toBe(2014);
   });
 
   it('should check whether two dates are in the same month and year', function() {
     // Same month and year.
-    var first = new Date(2015, 3, 30);
-    var second = new Date(2015, 3, 1);
+    var first = new Date(2015, APR, 30);
+    var second = new Date(2015, APR, 1);
     expect(dateUtil.isSameMonthAndYear(first, second)).toBe(true);
 
     // Same exact day.
-    first = new Date(2015, 3, 1);
-    second = new Date(2015, 3, 1);
+    first = new Date(2015, APR, 1);
+    second = new Date(2015, APR, 1);
     expect(dateUtil.isSameMonthAndYear(first, second)).toBe(true);
 
     // Same month, different year.
-    first = new Date(2015, 3, 30);
-    second = new Date(2005, 3, 1);
+    first = new Date(2015, APR, 30);
+    second = new Date(2005, APR, 1);
     expect(dateUtil.isSameMonthAndYear(first, second)).toBe(false);
 
     // Same year, different month.
-    first = new Date(2015, 3, 30);
-    second = new Date(2015, 6, 1);
+    first = new Date(2015, APR, 30);
+    second = new Date(2015, JUL, 1);
     expect(dateUtil.isSameMonthAndYear(first, second)).toBe(false);
 
     // Different month and year.
-    first = new Date(2012, 3, 30);
-    second = new Date(2015, 6, 1);
+    first = new Date(2012, APR, 30);
+    second = new Date(2015, JUL, 1);
     expect(dateUtil.isSameMonthAndYear(first, second)).toBe(false);
   });
 
   it('should check whether two dates are the same day', function() {
     // Same exact day and time.
-    var first = new Date(2015, 3, 1);
-    var second = new Date(2015, 3, 1);
+    var first = new Date(2015, APR, 1);
+    var second = new Date(2015, APR, 1);
     expect(dateUtil.isSameDay(first, second)).toBe(true);
 
     // Same day, different time.
-    first = new Date(2015, 3, 30, 3);
-    second = new Date(2015, 3, 30, 4);
+    first = new Date(2015, APR, 30, 3);
+    second = new Date(2015, APR, 30, 4);
     expect(dateUtil.isSameDay(first, second)).toBe(true);
 
     // Same month and year, different day.
-    first = new Date(2015, 3, 30);
-    second = new Date(2015, 3, 1);
+    first = new Date(2015, APR, 30);
+    second = new Date(2015, APR, 1);
     expect(dateUtil.isSameDay(first, second)).toBe(false);
 
     // Same month, different year.
-    first = new Date(2015, 3, 30);
-    second = new Date(2005, 3, 30);
+    first = new Date(2015, APR, 30);
+    second = new Date(2005, APR, 30);
     expect(dateUtil.isSameDay(first, second)).toBe(false);
 
     // Same year, different month.
-    first = new Date(2015, 3, 30);
-    second = new Date(2015, 6, 30);
+    first = new Date(2015, APR, 30);
+    second = new Date(2015, JUL, 30);
     expect(dateUtil.isSameDay(first, second)).toBe(false);
 
     // Different month and year.
-    first = new Date(2012, 3, 30);
-    second = new Date(2015, 6, 30);
+    first = new Date(2012, APR, 30);
+    second = new Date(2015, JUL, 30);
     expect(dateUtil.isSameDay(first, second)).toBe(false);
   });
 
   it('should check whether a date is in the next month', function() {
     // Next month within the same year.
-    var first = new Date(2015, 6, 15);
-    var second = new Date(2015, 7, 25);
+    var first = new Date(2015, JUL, 15);
+    var second = new Date(2015, AUG, 25);
     expect(dateUtil.isInNextMonth(first, second)).toBe(true);
 
     // Next month across years.
-    first = new Date(2015, 11, 15);
-    second = new Date(2016, 0, 25);
+    first = new Date(2015, DEC, 15);
+    second = new Date(2016, JAN, 25);
     expect(dateUtil.isInNextMonth(first, second)).toBe(true);
 
     // Not in the next month (past, same year).
-    first = new Date(2015, 5, 15);
-    second = new Date(2015, 3, 25);
+    first = new Date(2015, JUN, 15);
+    second = new Date(2015, APR, 25);
     expect(dateUtil.isInNextMonth(first, second)).toBe(false);
 
     // Not in the next month (future, same year).
-    first = new Date(2015, 5, 15);
-    second = new Date(2015, 7, 25);
+    first = new Date(2015, JUN, 15);
+    second = new Date(2015, AUG, 25);
     expect(dateUtil.isInNextMonth(first, second)).toBe(false);
 
     // Not in the next month (month + 1 in different year).
-    first = new Date(2015, 5, 15);
-    second = new Date(2016, 6, 25);
+    first = new Date(2015, JUN, 15);
+    second = new Date(2016, JUL, 25);
     expect(dateUtil.isInNextMonth(first, second)).toBe(false);
   });
 
   it('should check whether a date is in the previous month', function() {
     // Previous month within the same year.
-    var first = new Date(2015, 7, 15);
-    var second = new Date(2015, 6, 25);
+    var first = new Date(2015, AUG, 15);
+    var second = new Date(2015, JUL, 25);
     expect(dateUtil.isInPreviousMonth(first, second)).toBe(true);
 
     // Previous month across years.
-    first = new Date(2015, 0, 15);
-    second = new Date(2014, 11, 25);
+    first = new Date(2015, JAN, 15);
+    second = new Date(2014, DEC, 25);
     expect(dateUtil.isInPreviousMonth(first, second)).toBe(true);
 
     // Not in the previous month (past, same year).
-    first = new Date(2015, 5, 15);
-    second = new Date(2015, 3, 25);
+    first = new Date(2015, JUN, 15);
+    second = new Date(2015, APR, 25);
     expect(dateUtil.isInPreviousMonth(first, second)).toBe(false);
 
     // Not in the previous month (future, same year).
-    first = new Date(2015, 5, 15);
-    second = new Date(2015, 7, 25);
+    first = new Date(2015, JUN, 15);
+    second = new Date(2015, AUG, 25);
     expect(dateUtil.isInPreviousMonth(first, second)).toBe(false);
 
     // Not in the previous month (month - 1 in different year).
-    first = new Date(2015, 5, 15);
-    second = new Date(2016, 4, 25);
+    first = new Date(2015, JUN, 15);
+    second = new Date(2016, MAY, 25);
     expect(dateUtil.isInPreviousMonth(first, second)).toBe(false);
   });
 
   it('should get the midpoint between two dates', function() {
-    var start = new Date(2010, 2, 10);
-    var end = new Date(2010, 2, 20);
+    var start = new Date(2010, MAR, 10);
+    var end = new Date(2010, MAR, 20);
     var midpoint = dateUtil.getDateMidpoint(start, end);
 
-    expect(dateUtil.isSameDay(midpoint, new Date(2010, 2, 15))).toBe(true);
+    expect(dateUtil.isSameDay(midpoint, new Date(2010, MAR, 15))).toBe(true);
   });
 
   it('should get the week of the month in which a given date appears', function() {
+    // May 2015 spans 6 weeks.
+    expect(dateUtil.getWeekOfMonth(new Date(2015, MAY, 1))).toBe(0);
+    expect(dateUtil.getWeekOfMonth(new Date(2015, MAY, 8))).toBe(1);
+    expect(dateUtil.getWeekOfMonth(new Date(2015, MAY, 15))).toBe(2);
+    expect(dateUtil.getWeekOfMonth(new Date(2015, MAY, 22))).toBe(3);
+    expect(dateUtil.getWeekOfMonth(new Date(2015, MAY, 29))).toBe(4);
+    expect(dateUtil.getWeekOfMonth(new Date(2015, MAY, 31))).toBe(5);
+
+    // Feb 2015 spans 4 weeks. Check both the first and last day of each week.
+    expect(dateUtil.getWeekOfMonth(new Date(2015, FEB, 1))).toBe(0);
+    expect(dateUtil.getWeekOfMonth(new Date(2015, FEB, 7))).toBe(0);
+    expect(dateUtil.getWeekOfMonth(new Date(2015, FEB, 8))).toBe(1);
+    expect(dateUtil.getWeekOfMonth(new Date(2015, FEB, 14))).toBe(1);
+    expect(dateUtil.getWeekOfMonth(new Date(2015, FEB, 15))).toBe(2);
+    expect(dateUtil.getWeekOfMonth(new Date(2015, FEB, 21))).toBe(2);
+    expect(dateUtil.getWeekOfMonth(new Date(2015, FEB, 22))).toBe(3);
+    expect(dateUtil.getWeekOfMonth(new Date(2015, FEB, 28))).toBe(3);
   });
 
   it('should increment a date by a number of days', function() {
+    // Increment by one.
+    var start = new Date(2015, MAY, 15);
+    var end = new Date(2015, MAY, 16);
+    expect(dateUtil.isSameDay(dateUtil.incrementDays(start, 1), end)).toBe(true);
+
+    // Negative by negative one.
+    start = new Date(2015, MAY, 15);
+    end = new Date(2015, MAY, 14);
+    expect(dateUtil.isSameDay(dateUtil.incrementDays(start, -1), end)).toBe(true);
+
+    // Into next month.
+    start = new Date(2015, MAY, 31);
+    end = new Date(2015, JUN, 1);
+    expect(dateUtil.isSameDay(dateUtil.incrementDays(start, 1), end)).toBe(true);
+
+    // Into previous month.
+    start = new Date(2015, MAY, 1);
+    end = new Date(2015, APR, 30);
+    expect(dateUtil.isSameDay(dateUtil.incrementDays(start, -1), end)).toBe(true);
+
+    // Into next year.
+    start = new Date(2015, DEC, 31);
+    end = new Date(2016, JAN, 1);
+    expect(dateUtil.isSameDay(dateUtil.incrementDays(start, 1), end)).toBe(true);
+
+    // Into last year.
+    start = new Date(2015, JAN, 1);
+    end = new Date(2014, DEC, 31);
+    expect(dateUtil.isSameDay(dateUtil.incrementDays(start, -1), end)).toBe(true);
   });
 
   it('should increment a date by a number of months', function() {
+    // Increment by one.
+    var start = new Date(2015, MAY, 15);
+    var end = new Date(2015, JUN, 15);
+    expect(dateUtil.isSameDay(dateUtil.incrementMonths(start, 1), end)).toBe(true);
+
+    // Negative by negative one.
+    start = new Date(2015, MAY, 15);
+    end = new Date(2015, APR, 15);
+    expect(dateUtil.isSameDay(dateUtil.incrementMonths(start, -1), end)).toBe(true);
+
+    // Next month has fewer days.
+    start = new Date(2015, JAN, 30);
+    end = new Date(2015, FEB, 28);
+    expect(dateUtil.isSameDay(dateUtil.incrementMonths(start, 1), end)).toBe(true);
+
+    // Previous month has fewer days.
+    start = new Date(2015, MAY, 31);
+    end = new Date(2015, APR, 30);
+    expect(dateUtil.isSameDay(dateUtil.incrementMonths(start, -1), end)).toBe(true);
   });
 
   it('should get the last date of a month', function() {
+    // Normal February
+    var date = new Date(2015, FEB, 1);
+    var lastOfMonth = new Date(2015, FEB, 28);
+    expect(dateUtil.isSameDay(dateUtil.getLastDateOfMonth(date), lastOfMonth)).toBe(true);
+
+    // Leap year February
+    date = new Date(2012, FEB, 1);
+    lastOfMonth = new Date(2012, FEB, 29);
+    expect(dateUtil.isSameDay(dateUtil.getLastDateOfMonth(date), lastOfMonth)).toBe(true);
+
+    // Month with 31 days.
+    date = new Date(2015, DEC, 12);
+    lastOfMonth = new Date(2015, DEC, 31);
+    expect(dateUtil.isSameDay(dateUtil.getLastDateOfMonth(date), lastOfMonth)).toBe(true);
+
+    // Month with 30 days.
+    date = new Date(2015, APR, 3);
+    lastOfMonth = new Date(2015, APR, 30);
+    expect(dateUtil.isSameDay(dateUtil.getLastDateOfMonth(date), lastOfMonth)).toBe(true);
   });
 });
