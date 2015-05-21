5011f383f1c387ee0192755885a70f033f178437
Caleb Eggensperger <calebegg@google.com>
2015-05-21 19:50:53 -0400

    fix(textarea): Start at a height of one line
    Change min-height for textarea to one line height (and remove extra
    padding space), and set rows=“1”, since it defaults to 2. Fixes #2154

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -235,6 +235,7 @@ function inputTextareaDirective($mdUtil, $window) {
       element.on('keydown input', onChangeTextarea);
       element.on('scroll', onScroll);
       angular.element($window).on('resize', onChangeTextarea);
+      element.attr('rows', '1');
 
       scope.$on('$destroy', function() {
         angular.element($window).off('resize', onChangeTextarea);
