e6f19d4868059cd48e3dfd811273cff36c3ef9a4
Robert Messerle <rmesserle@google.com>
2015-05-27 12:24:16 -0700

    fix(tabs): re-adds disconnected scopes for tab content
    Note: This may open up some temporary bugs while fine-tuning the logic

--- src/components/tabs/js/templateDirective.js
+++ src/components/tabs/js/templateDirective.js
@@ -2,19 +2,36 @@ angular
     .module('material.components.tabs')
     .directive('mdTemplate', MdTemplate);
 
-function MdTemplate ($compile) {
+function MdTemplate ($compile, $mdUtil, $timeout) {
   return {
     restrict: 'A',
     link: link,
     scope: {
       template: '=mdTemplate',
-      compileScope: '=mdScope'
+      compileScope: '=mdScope',
+      connected: '=?mdConnectedIf'
     },
     require: '^?mdTabs'
   };
   function link (scope, element, attr, ctrl) {
     if (!ctrl) return;
+    var connected = true;
     element.html(scope.template);
     $compile(element.contents())(scope.compileScope);
+    $timeout(handleScope);
+    function handleScope () {
+      scope.$watch('connected', function (value) { value ? disconnect() : reconnect(); });
+      scope.$on('$destroy', reconnect);
+    }
+    function disconnect () {
+      if (!connected) return;
+      $mdUtil.disconnectScope(scope.compileScope);
+      connected = false;
+    }
+    function reconnect () {
+      if (connected) return;
+      $mdUtil.reconnectScope(scope.compileScope);
+      connected = true;
+    }
   }
 }
