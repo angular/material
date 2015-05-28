40825c2009162934c6743506fdd6c3bf0083abe4
Robert Messerle <rmesserle@google.com>
2015-05-28 09:27:29 -0700

    update(build): revises build method to remove duplicate `goog.provide` lines from `core.js`

--- gulp/tasks/build-all-modules.js
+++ gulp/tasks/build-all-modules.js
@@ -9,33 +9,16 @@ var gulpif = require('gulp-if');
 var utils = require('../../scripts/gulp-utils.js');
 
 exports.task = function() {
-  var stream = series(
-      gulp.src(['src/core/', 'src/components/*' ])
-          .pipe(through2.obj(function(folder, enc, next) {
-            var moduleId = folder.path.indexOf('components') > -1
-                ? 'material.components.' + path.basename(folder.path)
-                : 'material.' + path.basename(folder.path);
-            var stream = BUILD_MODE.useBower
-                ? series(util.buildModule(moduleId, true), util.buildModule(moduleId, false))
-                : util.buildModule(moduleId, false);
-            stream.on('end', function() { next(); });
-          })),
-      util.themeBuildStream()
-          .pipe(
-            gulpif(BUILD_MODE.name != 'closure',
-              BUILD_MODE.transform()
-                .pipe(gulp.dest(path.join(BUILD_MODE.outputDir, 'core')))
-            )
-          )
-  );
-  if (BUILD_MODE.name == 'closure') {
-    stream.pipe(
-      utils.appendToFile(path.join(BUILD_MODE.outputDir, 'core', 'core.js'))
-    )
-    .pipe(BUILD_MODE.transform())
-    .pipe(gulp.dest(path.join(BUILD_MODE.outputDir, 'core')))
-  }
-
-  return stream;
+  return gulp.src(['src/core/', 'src/components/*' ])
+      .pipe(through2.obj(function(folder, enc, next) {
+        var moduleId = folder.path.indexOf('components') > -1
+            ? 'material.components.' + path.basename(folder.path)
+            : 'material.' + path.basename(folder.path);
+        var stream = BUILD_MODE.useBower
+            ? series(util.buildModule(moduleId, true), util.buildModule(moduleId, false))
+            : util.buildModule(moduleId, false);
+        stream.on('end', function() { next(); });
+      }))
+      .pipe(BUILD_MODE.transform());
 
 };
