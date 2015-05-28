7a1ad41f3c1df6cb1dfa750cb817f166f02097ee
Robert Messerle <rmesserle@google.com>
2015-05-28 09:36:36 -0700

    fix(build): fixes bug where versions in 0.9.5 comments were wrong.

--- gulp/config.js
+++ gulp/config.js
@@ -1,4 +1,5 @@
-var VERSION = require('./const').VERSION;
+var args = require('minimist')(process.argv.slice(2));
+var VERSION = args.version || require('../package.json').version;
 
 module.exports = {
   banner:
