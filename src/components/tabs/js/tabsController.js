a0b3d5fcf0bb32448c7e2dae4a5c86970b736061
Robert Messerle <rmesserle@google.com>
2015-06-11 16:07:54 -0700

    update(tabs): replaces hard-coded `transform` with `$mdConstant.CSS.TRANSFORM`
    Closes #3121

--- src/components/tabs/js/tabsController.js
+++ src/components/tabs/js/tabsController.js
@@ -119,7 +119,7 @@ function MdTabsController ($scope, $element, $window, $timeout, $mdConstant, $md
    */
   function handleOffsetChange (left) {
     var newValue = ctrl.shouldCenterTabs ? '' : '-' + left + 'px';
-    angular.element(elements.paging).css('transform', 'translate3d(' + newValue + ', 0, 0)');
+    angular.element(elements.paging).css($mdConstant.CSS.TRANSFORM, 'translate3d(' + newValue + ', 0, 0)');
     $scope.$broadcast('$mdTabsPaginationChanged');
   }
 
