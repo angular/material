5cc9af4c5eb1b7f802f51e44ecedfba2ee0f5c43
Ryan Schmukler <rschmukler@gmail.com>
2015-06-21 17:58:13 -0400

    test(phantomJs): patch in HtmlElement.click for phantomjs

--- test/angular-material-spec.js
+++ test/angular-material-spec.js
@@ -7,6 +7,27 @@
  */
 (function() {
 
+
+  // Patch since PhantomJS does not implement click() on HTMLElement. In some 
+  // cases we need to execute the native click on an element. However, jQuery's 
+  // $.fn.click() does not dispatch to the native function on <a> elements, so we
+  // can't use it in our implementations: $el[0].click() to correctly dispatch.
+  // Borrowed from https://stackoverflow.com/questions/15739263/phantomjs-click-an-element
+  if (!HTMLElement.prototype.click) {
+    HTMLElement.prototype.click = function() {
+      var ev = document.createEvent('MouseEvent');
+      ev.initMouseEvent(
+          'click',
+          /*bubble*/true, /*cancelable*/true,
+          window, null,
+          0, 0, 0, 0, /*coordinates*/
+          false, false, false, false, /*modifier keys*/
+          0/*button=left*/, null
+      );
+      this.dispatchEvent(ev);
+    };
+  }
+
   beforeEach(function() {
 
     /**
