d32270e30f3816bdbdcff21a64234907b7270120
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-08-24 16:24:35 -0500

    update(build): improve module dependency parsing

--- config/ngModuleData.js
+++ config/ngModuleData.js
@@ -1,7 +1,7 @@
 // Regex adapted from https://github.com/ceymard/gulp-ngcompile
 
 module.exports = function processContent(fileContent) {
-  var NG_MODULE_REGEX = /\.module\((['|"]\material\..*'|"[^"]*")\s*,?\s*\[(([^\]]+)\])?/g;
+  var NG_MODULE_REGEX = /\.module\(('material\.[^']*'|"material\.[^"]*")\s*,(?:\s*\[([^\]]+)\])?/g;
   var match = NG_MODULE_REGEX.exec(fileContent || '');
   var module = match && match[1] && match[1].slice(1, -1); //remove quotes
   var depsMatch = match && match[2] && match[2].trim();
