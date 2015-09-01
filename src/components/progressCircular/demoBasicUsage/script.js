7a4c231d1e2aecab54998cc58f804a54b20c9862
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-08-31 21:40:04 -0500

    update(demo): logic improvement.

--- src/components/progressCircular/demoBasicUsage/script.js
+++ src/components/progressCircular/demoBasicUsage/script.js
@@ -12,7 +12,7 @@ angular
        * Turn off or on the 5 themed loaders
        */
       self.toggleActivation = function() {
-          if ( !self.activated ) self.modes =  self.modes.map(function() { return ''; });
+          if ( !self.activated ) self.modes = [ ];
           if (  self.activated ) j = counter = 0;
       };
 
