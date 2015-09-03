ffbcff39caf1c6cc7040e2a38a91ccff3edb2e70
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-09-03 07:12:48 -0500

    fix(dialog): allow Confirm dialogs to have empty/undefined content
    Fixes #4429.

--- src/components/dialog/dialog.js
+++ src/components/dialog/dialog.js
@@ -511,7 +511,7 @@ function MdDialogProvider($$interimElementProvider) {
       function wrapSimpleContent() {
         if ( controller ) {
           var HTML_END_TAG = /<\/[\w-]*>/gm;
-          var content = controller.content;
+          var content = controller.content || "";
 
           var hasHTML = HTML_END_TAG.test(content);
           if (!hasHTML) {
