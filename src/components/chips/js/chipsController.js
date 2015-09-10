62a0542d5593f9d25b4e39270d67fb8042bbd2d3
Jordan Majd <jordan@madtitanstudios.com>
2015-09-10 13:00:17 -0600

    style(md-chips): adds comments and whitespace to appendChip
    md-chips appendChip now is styled with comments and whitespaces to enhance readability.

--- src/components/chips/js/chipsController.js
+++ src/components/chips/js/chipsController.js
@@ -193,16 +193,24 @@ MdChipsCtrl.prototype.getAdjacentChipIndex = function(index) {
  * @param newChip
  */
  MdChipsCtrl.prototype.appendChip = function(newChip) {
+
+   // If useOnAppend and onAppend function is provided call it.
    if (this.useOnAppend && this.onAppend) {
      newChip = this.onAppend({'$chip': newChip});
    }
-   if(typeof newChip === 'object'){
+
+   // If items contains identical object to newChip do not append
+   if(angular.isObject(newChip)){
      var identical = this.items.some(function(item){
        return angular.equals(newChip, item);
      });
      if(identical) return;
    }
+
+   // If items contains newChip do not append
    if (this.items.indexOf(newChip) + 1) return;
+
+   //add newChip to items
    this.items.push(newChip);
  };
 
