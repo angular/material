355ed68e4ff148d94cefd08587107e41246f5c78
Donghwan Kim <flowersinthesand@gmail.com>
2015-09-11 23:39:13 +0900

    feat(layout): allow flex-order to be negative
    Fixes #4482.

--- src/core/services/layout/layout.spec.js
+++ src/core/services/layout/layout.spec.js
@@ -5,7 +5,7 @@ describe('layout directives', function() {
 
     var suffixes = ['sm', 'gt-sm', 'md', 'gt-md', 'lg', 'gt-lg'];
     var directionValues = ['row', 'column'];
-    var flexOrderValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var flexOrderValues = [-9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     var flexValues = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 33, 34, 66, 67];
     var offsetValues = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 33, 34, 66, 67];
     var alignmentValues = [
