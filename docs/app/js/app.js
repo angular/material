48f88e035220190e0e4d0c49b005645437d6147d
Jeremy Elbourn <jelbourn@google.com>
2015-09-16 16:23:48 -0700

    feat(docs): use html5mode routing and simplify urls.

--- docs/app/js/app.js
+++ docs/app/js/app.js
@@ -6,8 +6,12 @@ var DocsApp = angular.module('docsApp', [ 'angularytics', 'ngRoute', 'ngMessages
   'DEMOS',
   'PAGES',
   '$routeProvider',
+  '$locationProvider',
   '$mdThemingProvider',
-function(SERVICES, COMPONENTS, DEMOS, PAGES, $routeProvider, $mdThemingProvider) {
+function(SERVICES, COMPONENTS, DEMOS, PAGES,
+    $routeProvider, $locationProvider, $mdThemingProvider) {
+  $locationProvider.html5Mode(true);
+
   $routeProvider
     .when('/', {
       templateUrl: 'partials/home.tmpl.html'
@@ -18,37 +22,31 @@ function(SERVICES, COMPONENTS, DEMOS, PAGES, $routeProvider, $mdThemingProvider)
       }
     })
     .when('/layout/', {
-      redirectTo: function() {
-        return "/layout/container";
-      }
+      redirectTo:  '/layout/container'
     })
     .when('/demo/', {
-      redirectTo: function() {
-        return DEMOS[0].url;
-      }
+      redirectTo: DEMOS[0].url
     })
     .when('/api/', {
-      redirectTo: function() {
-        return COMPONENTS[0].docs[0].url;
-      }
+      redirectTo: COMPONENTS[0].docs[0].url
     })
     .when('/getting-started', {
       templateUrl: 'partials/getting-started.tmpl.html'
     });
   $mdThemingProvider.definePalette('docs-blue', $mdThemingProvider.extendPalette('blue', {
-      '50':   '#DCEFFF',
-      '100':  '#AAD1F9',
-      '200':  '#7BB8F5',
-      '300':  '#4C9EF1',
-      '400':  '#1C85ED',
-      '500':  '#106CC8',
-      '600':  '#0159A2',
-      '700':  '#025EE9',
-      '800':  '#014AB6',
-      '900':  '#013583',
-      'contrastDefaultColor': 'light',
-      'contrastDarkColors': '50 100 200 A100',
-      'contrastStrongLightColors': '300 400 A200 A400'
+    '50': '#DCEFFF',
+    '100': '#AAD1F9',
+    '200': '#7BB8F5',
+    '300': '#4C9EF1',
+    '400': '#1C85ED',
+    '500': '#106CC8',
+    '600': '#0159A2',
+    '700': '#025EE9',
+    '800': '#014AB6',
+    '900': '#013583',
+    'contrastDefaultColor': 'light',
+    'contrastDarkColors': '50 100 200 A100',
+    'contrastStrongLightColors': '300 400 A200 A400'
   }));
   $mdThemingProvider.definePalette('docs-red', $mdThemingProvider.extendPalette('red', {
     'A100': '#DE3641'
@@ -74,8 +72,7 @@ function(SERVICES, COMPONENTS, DEMOS, PAGES, $routeProvider, $mdThemingProvider)
 
   angular.forEach(COMPONENTS, function(component) {
     angular.forEach(component.docs, function(doc) {
-      doc.url = '/' + doc.url;
-      $routeProvider.when(doc.url, {
+      $routeProvider.when('/' + doc.url, {
         templateUrl: doc.outputPath,
         resolve: {
           component: function() { return component; },
@@ -87,11 +84,10 @@ function(SERVICES, COMPONENTS, DEMOS, PAGES, $routeProvider, $mdThemingProvider)
   });
 
   angular.forEach(SERVICES, function(service) {
-    service.url = '/' + service.url;
-    $routeProvider.when(service.url, {
+    $routeProvider.when('/' + service.url, {
       templateUrl: service.outputPath,
       resolve: {
-        component: function() { return undefined; },
+        component: angular.noop,
         doc: function() { return service; }
       },
       controller: 'ComponentDocCtrl'
@@ -100,13 +96,16 @@ function(SERVICES, COMPONENTS, DEMOS, PAGES, $routeProvider, $mdThemingProvider)
 
   angular.forEach(DEMOS, function(componentDemos) {
     var demoComponent;
-    angular.forEach(COMPONENTS, function(component) {
-      if (componentDemos.name === component.name) {
+
+    COMPONENTS.forEach(function(component) {
+      if (componentDemos.moduleName === component.name) {
         demoComponent = component;
+        component.demoUrl = componentDemos.url;
       }
     });
+
     demoComponent = demoComponent || angular.extend({}, componentDemos);
-    $routeProvider.when(componentDemos.url, {
+    $routeProvider.when('/' + componentDemos.url, {
       templateUrl: 'partials/demo.tmpl.html',
       controller: 'DemoCtrl',
       resolve: {
@@ -123,11 +122,7 @@ function(SERVICES, COMPONENTS, DEMOS, PAGES, $routeProvider, $mdThemingProvider)
    AngularyticsProvider.setEventHandlers(['Console', 'GoogleUniversal']);
 }])
 
-.run([
-   'Angularytics',
-   '$rootScope',
-    '$timeout',
-function(Angularytics, $rootScope,$timeout) {
+.run(['Angularytics', function(Angularytics) {
   Angularytics.init();
 }])
 
@@ -291,7 +286,7 @@ function(SERVICES, COMPONENTS, DEMOS, PAGES, $location, $rootScope, $http, $wind
         var commonVersions = versionId === 'head' ? [] : [ head ];
         var knownVersions = getAllVersions();
         var listVersions = knownVersions.filter(removeCurrentVersion);
-        var currentVersion = getCurrentVersion();
+        var currentVersion = getCurrentVersion() || {name: 'local'};
         version.current = currentVersion;
         sections.unshift({
           name: 'Documentation Version',
@@ -311,17 +306,21 @@ function(SERVICES, COMPONENTS, DEMOS, PAGES, $location, $rootScope, $http, $wind
           }
         }
         function getAllVersions () {
-          return response.versions.map(function(version) {
-            var latest = response.latest === version;
-            return {
-              type: 'version',
-              url: '/' + version,
-              name: getVersionFullString({ id: version, latest: latest }),
-              id: version,
-              latest: latest,
-              github: 'tree/v' + version
-            };
-          });
+          if (response && response.versions && response.versions.length) {
+            return response.versions.map(function(version) {
+              var latest = response.latest === version;
+              return {
+                type: 'version',
+                url: '/' + version,
+                name: getVersionFullString({ id: version, latest: latest }),
+                id: version,
+                latest: latest,
+                github: 'tree/v' + version
+              };
+            });
+          }
+
+          return [];
         }
         function getVersionFullString (version) {
           return version.latest
@@ -367,11 +366,6 @@ function(SERVICES, COMPONENTS, DEMOS, PAGES, $location, $rootScope, $http, $wind
     }
   };
 
-  function sortByHumanName(a,b) {
-    return (a.humanName < b.humanName) ? -1 :
-      (a.humanName > b.humanName) ? 1 : 0;
-  }
-
   function onLocationChange() {
     var path = $location.path();
     var introLink = {
@@ -387,14 +381,14 @@ function(SERVICES, COMPONENTS, DEMOS, PAGES, $location, $rootScope, $http, $wind
     }
 
     var matchPage = function(section, page) {
-      if (path === page.url) {
+      if (path.indexOf(page.url) !== -1) {
         self.selectSection(section);
         self.selectPage(section, page);
       }
     };
 
     sections.forEach(function(section) {
-      if(section.children) {
+      if (section.children) {
         // matches nested section toggles, such as API or Customization
         section.children.forEach(function(childSection){
           if(childSection.pages){
@@ -404,7 +398,7 @@ function(SERVICES, COMPONENTS, DEMOS, PAGES, $location, $rootScope, $http, $wind
           }
         });
       }
-      else if(section.pages) {
+      else if (section.pages) {
         // matches top-level section toggles, such as Demos
         section.pages.forEach(function(page) {
           matchPage(section, page);
@@ -448,8 +442,6 @@ function(SERVICES, COMPONENTS, DEMOS, PAGES, $location, $rootScope, $http, $wind
     templateUrl: 'partials/menu-toggle.tmpl.html',
     link: function($scope, $element) {
       var controller = $element.parent().controller();
-      var $ul = $element.find('ul');
-      var originalHeight;
 
       $scope.isOpen = function() {
         return controller.isOpen($scope.section);
@@ -500,9 +492,7 @@ function(SERVICES, COMPONENTS, DEMOS, PAGES, $location, $rootScope, $http, $wind
   'menu',
   '$location',
   '$rootScope',
-  '$window',
-  '$log',
-function($scope, COMPONENTS, BUILDCONFIG, $mdSidenav, $timeout, $mdDialog, menu, $location, $rootScope, $window, $log) {
+function($scope, COMPONENTS, BUILDCONFIG, $mdSidenav, $timeout, $mdDialog, menu, $location, $rootScope) {
   var self = this;
 
   $scope.COMPONENTS = COMPONENTS;
@@ -525,8 +515,9 @@ function($scope, COMPONENTS, BUILDCONFIG, $mdSidenav, $timeout, $mdDialog, menu,
     enumerable: true,
     configurable: true
   });
-  $rootScope.redirectToUrl = function (url) {
-    $window.location.hash = url;
+
+  $rootScope.redirectToUrl = function(url) {
+    $location.path(url);
     $timeout(function () { $rootScope.relatedPage = null; }, 100);
   };
 
@@ -646,10 +637,7 @@ function($scope, $attrs, $location, $rootScope) {
   'doc',
   'component',
   '$rootScope',
-  '$templateCache',
-  '$http',
-  '$q',
-function($scope, doc, component, $rootScope, $templateCache, $http, $q) {
+function($scope, doc, component, $rootScope) {
   $rootScope.currentComponent = component;
   $rootScope.currentDoc = doc;
 }])
@@ -661,8 +649,7 @@ function($scope, doc, component, $rootScope, $templateCache, $http, $q) {
   'demos',
   '$http',
   '$templateCache',
-  '$q',
-function($rootScope, $scope, component, demos, $http, $templateCache, $q) {
+function($rootScope, $scope, component, demos, $http, $templateCache) {
   $rootScope.currentComponent = component;
   $rootScope.currentDoc = null;
 
--- gulp/tasks/site.js
+++ gulp/tasks/site.js
@@ -5,6 +5,11 @@ exports.task = function () {
   connect.server({
     root: './dist/docs',
     livereload: true,
-    port: LR_PORT
+    port: LR_PORT,
+
+    // For any 404, respond with index.html. This enables html5Mode routing.
+    // In a production environment, this would be done with much more
+    // fine-grained URL rewriting rules.
+    fallback: './dist/docs/index.html'
   });
 };
