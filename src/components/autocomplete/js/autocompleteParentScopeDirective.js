angular/material

commit 8849213ce42fa98169b6fbbb0805feb95a6ba678

fix(autocomplete): re-adds support for custom item names in autocomplete templates
Closes #4667

src/components/autocomplete/js/autocompleteParentScopeDirective.js
@@ -10,17 +10,13 @@ function MdAutocompleteItemScopeDirective($compile, $mdUtil) {
   };
 
   function postLink(scope, element, attr) {
-    var newScope = scope.$mdAutocompleteCtrl.parent.$new();
-    var relevantVariables = ['item', '$index'];
+    var ctrl     = scope.$mdAutocompleteCtrl;
+    var newScope = ctrl.parent.$new();
+    var itemName = ctrl.itemName;
 
     // Watch for changes to our scope's variables and copy them to the new scope
-    angular.forEach(relevantVariables, function(variable){
-      scope.$watch(variable, function(value) {
-        $mdUtil.nextTick(function() {
-          newScope[variable] = value;
-        });
-      });
-    });
+    watchVariable('$index', '$index');
+    watchVariable('item', itemName);
 
     // Recompile the contents with the new/modified scope
     $compile(element.contents())(newScope);
@@ -29,6 +25,18 @@ function MdAutocompleteItemScopeDirective($compile, $mdUtil) {
     if (attr.hasOwnProperty('mdAutocompleteReplace')) {
       element.after(element.contents());
       element.remove();
+    }
+
+    /**
+     * Creates a watcher for variables that are copied from the parent scope
+     * @param variable
+     * @param alias
+     */
+    function watchVariable (variable, alias) {
+      $mdUtil.nextTick(function () {
+        newScope[alias] = scope[variable];
+        scope.$watch(variable, function (value) { newScope[alias] = value; });
+      });
     }
   }
 }

commit 691b0d3b0f0839c62c65db0d959c9bbca69b29cf

fix(mdAutocomplete): Fix scope watch bug.
Commit 8849213ce42fa98169b6fbbb0805feb95a6ba678 introduced a scope
watching bug cauinsg the autocomplete to no longer update it's list
of items as you scrolled (since it re-uses DOM elements). Fix by
swapping nextTick to be inside the watch statement.

Fixes #4713.

src/components/autocomplete/js/autocompleteParentScopeDirective.js
@@ -10,7 +10,7 @@ function MdAutocompleteItemScopeDirective($compile, $mdUtil) {
   };
 
   function postLink(scope, element, attr) {
-    var ctrl     = scope.$mdAutocompleteCtrl;
+    var ctrl = scope.$mdAutocompleteCtrl;
     var newScope = ctrl.parent.$new();
     var itemName = ctrl.itemName;
 
@@ -32,10 +32,13 @@ function MdAutocompleteItemScopeDirective($compile, $mdUtil) {
      * @param variable
      * @param alias
      */
-    function watchVariable (variable, alias) {
-      $mdUtil.nextTick(function () {
-        newScope[alias] = scope[variable];
-        scope.$watch(variable, function (value) { newScope[alias] = value; });
+    function watchVariable(variable, alias) {
+      newScope[alias] = scope[variable];
+
+      scope.$watch(variable, function(value) {
+        $mdUtil.nextTick(function() {
+          newScope[alias] = value;
+        });
       });
     }
   }