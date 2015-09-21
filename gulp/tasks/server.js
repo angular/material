885162b5c45acba04a44bad42f74c273e2a27a6e
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-09-21 08:24:13 -0500

    update(npm): add standard package.json fields
    Fix license warnings from npm
    Add a useful npm 'watch' script to build, watch the docs; opens browser at http://localhost:8080
    Add other standard fields as detailed in https://docs.npmjs.com/files/package.json
    
    Closes #4741.
    
    > Thx @splaktar for PR #4741.

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var LR_PORT = require('../const').LR_PORT;
+var util = require('../util');
 
 exports.task = function() {
-  return gulp.src('.')
+    var openUrl = false;
+    if (typeof util.args.o === 'string' ||
+        typeof util.args.o === 'boolean') {
+        openUrl = util.args.o;
+    }
+    return gulp.src('.')
       .pipe(webserver({
-        host: '0.0.0.0',
-        livereload: true,
-        port: LR_PORT,
-        directoryListing: true
-      }));
+            host: '0.0.0.0',
+            livereload: true,
+            port: LR_PORT,
+            directoryListing: true,
+            open: openUrl
+        }));
 };
