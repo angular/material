928dba360afe57f24a73c0b15e1d0494c49162bf
ML <ml@seedup.io>
2015-09-21 13:00:07 -0400

    check for undefined before trying to compute animation style values, Fixes #4752

--- src/core/util/animation/animateCss.js
+++ src/core/util/animation/animateCss.js
@@ -315,16 +315,18 @@ if (angular.version.minor >= 4) {
 
       function parseMaxTime(str) {
         var maxValue = 0;
-        var values = (str || "").split(/\s*,\s*/);
-        forEach(values, function(value) {
-          // it's always safe to consider only second values and omit `ms` values since
-          // getComputedStyle will always handle the conversion for us
-          if (value.charAt(value.length - 1) == 's') {
-            value = value.substring(0, value.length - 1);
-          }
-          value = parseFloat(value) || 0;
-          maxValue = maxValue ? Math.max(value, maxValue) : value;
-        });
+        if (str) {
+          var values = (str || "").split(/\s*,\s*/);
+          forEach(values, function(value) {
+            // it's always safe to consider only second values and omit `ms` values since
+            // getComputedStyle will always handle the conversion for us
+            if (value.charAt(value.length - 1) == 's') {
+              value = value.substring(0, value.length - 1);
+            }
+            value = parseFloat(value) || 0;
+            maxValue = maxValue ? Math.max(value, maxValue) : value;
+          });
+        }
         return maxValue;
       }
 
