67b4c32ff6f2d68a792a920d9b79f533552b6f80
Topher Fangio <topher@profoundry.us>
2015-09-22 17:11:43 -0500

    fix(mdInput): Fix md-maxlength when used with ng-messages.
    The recently added support for input elements to have multiple
    messages caused a rendering issue for the `md-maxlength`
    character counter.
    
    Update code/CSS to properly position the character counter when
    used with and without ng-messages.
    
    Fixes #4783.
    
    POSSIBLE BREAKING CHANGE - The `<div class="md-char-counter">`
    that is automatically added to the input is now added after
    the input if no messages are found, but inside if they are.
    
    This may cause some styling issues if users provide custom
    CSS. Users may need to update their CSS to take the extra
    HTML into account.
    
    Example rendered HTML:
    
    **Without `ng-messages`:**
    
    ```html
    <label>Label is here</label>
    <input />
    <div class="md-char-counter">10/20</div>
    ```
    
    **With `ng-messages`:**
    
    ```html
    <label>Label is here</label>
    <input />
    <div ng-messages="...">
      <div class="md-char-counter">10/20</div>
      <div ng-message="requried">This is required</div>
    </div>
    ```

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -362,7 +362,25 @@ function mdMaxlengthDirective($animate) {
     // Stop model from trimming. This makes it so whitespace
     // over the maxlength still counts as invalid.
     attr.$set('ngTrim', 'false');
-    input.after(charCountEl);
+
+    var ngMessagesSelectors = [
+      'ng-messages',
+      'data-ng-messages',
+      'x-ng-messages',
+      '[ng-messages]',
+      '[data-ng-messages]',
+      '[x-ng-messages]'
+    ];
+
+    var ngMessages = containerCtrl.element[0].querySelector(ngMessagesSelectors.join(','));
+
+    // If we have an ngMessages container, put the counter at the top; otherwise, put it after the
+    // input so it will be positioned properly in the SCSS
+    if (ngMessages) {
+      angular.element(ngMessages).prepend(charCountEl);
+    } else {
+      input.after(charCountEl);
+    }
 
     ngModelCtrl.$formatters.push(renderCharCount);
     ngModelCtrl.$viewChangeListeners.push(renderCharCount);
