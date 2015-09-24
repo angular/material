b24f55fa2e3c668d031721ddd89984e226f1055c
Tony Childs <tony.childs@gmail.com>
2015-09-24 17:59:59 -0500

    fix(icon): Fix bug that prevented custom default fontset from being applied.
    Fixes #4349. Closes #4829.

--- src/components/icon/js/iconDirective.js
+++ src/components/icon/js/iconDirective.js
@@ -230,25 +230,12 @@ function mdIconDirective($mdIcon, $mdTheming, $mdAria ) {
       return false;
     }
 
-    function prepareForFontIcon () {
+    function prepareForFontIcon() {
       if (!scope.svgIcon && !scope.svgSrc) {
-
         if (scope.fontIcon) {
           element.addClass('md-font ' + scope.fontIcon);
         }
-
-        if (scope.fontSet) {
-          element.addClass($mdIcon.fontSet(scope.fontSet));
-        }
-
-        if (shouldUseDefaultFontSet()) {
-          element.addClass($mdIcon.fontSet());
-        }
-
-      }
-
-      function shouldUseDefaultFontSet() {
-        return !scope.fontIcon && !scope.fontSet;
+        element.addClass($mdIcon.fontSet(scope.fontSet));
       }
     }
   }
