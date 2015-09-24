7009c4e5d87d6a94875361202b55a7e97cccd485
Elad Bezalel <elad.bezalel@gmail.com>
2015-09-24 17:10:08 +0300

    update(switch): fixed switch movement speed
    Closes #4581. Closes #4816.

--- src/components/switch/demoBasicUsage/script.js
+++ src/components/switch/demoBasicUsage/script.js
@@ -6,7 +6,9 @@ angular.module('switchDemo1', ['ngMaterial'])
     cb5: false
   };
 
+  $scope.message = 'false';
+
   $scope.onChange = function(cbState) {
-  	$scope.message = "The switch is now: " + cbState;
+  	$scope.message = cbState;
   };
 });
