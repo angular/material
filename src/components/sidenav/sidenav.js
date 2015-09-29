1a61e2ea882cf340f546808c1073441bac592a6c
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-09-29 09:29:40 -0500

    fix(sidenav): resolve jQuery conflict with  handler
    Fixes #4876.

--- src/components/sidenav/sidenav.js
+++ src/components/sidenav/sidenav.js
@@ -249,7 +249,10 @@ function SidenavDirective($mdMedia, $mdUtil, $mdConstant, $mdTheming, $animate, 
       sidenavCtrl.destroy();
     });
 
-    scope.$on('$destroy', angular.bind(backdrop, backdrop.remove));
+    scope.$on('$destroy', function(){
+      backdrop.remove()
+    });
+
     scope.$watch(isLocked, updateIsLocked);
     scope.$watch('isOpen', updateIsOpen);
 
