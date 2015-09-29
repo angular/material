ab1df4c2cecbaa15d5c771b0deba0261921f861e
Daniel Lochrie <dlochrie@users.noreply.github.com>
2015-09-28 20:45:39 -0500

    Added an internal function to handle `debounce` in demo.
    Please see this [Google Closure compiler PR](https://github.com/google/closure-compiler/pull/1170) for more information/reference.
    
    Basically, the demo code is using `$mdUtil`, which is not part of the public API, and therefore not suitable for the public (docs) reference.
    
    Closes #4888.

--- src/components/sidenav/demoBasicUsage/script.js
+++ src/components/sidenav/demoBasicUsage/script.js
@@ -1,26 +1,40 @@
 angular
   .module('sidenavDemo1', ['ngMaterial'])
   .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $mdUtil, $log) {
-
     $scope.toggleLeft = buildToggler('left');
     $scope.toggleRight = buildToggler('right');
+
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+      var timer;
+
+      return function debounced() {
+        var context = scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
 
     /**
      * Build handler to open/close a SideNav; when animation finishes
      * report completion in console
      */
     function buildToggler(navID) {
-      var debounceFn =  $mdUtil.debounce(function(){
-            $mdSidenav(navID)
-              .toggle()
-              .then(function () {
-                $log.debug("toggle " + navID + " is done");
-              });
-          },200);
-
-      return debounceFn;
+      return debounce(function() {
+        $mdSidenav(navID).
+            toggle().
+            then(function () {
+              $log.debug("toggle " + navID + " is done");
+            });
+      }, 200);
     }
-
   })
   .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
     $scope.close = function () {
