a16a7a4b943f2ee0d50c6d5690ac77dcbcc33923
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-10-13 11:25:14 -0500

    update(demo): sidenav toggle debounce removed
    The toggle debounce [used to address multiple clicks on the same button] causes a delay and is perceived as a performance issue.
    
    *  Use an *immediate* toggle for the right button
    *  Hide the right button with the right sidenav is open using `isOpenRight( )`
    
    References #3721

--- src/components/sidenav/demoBasicUsage/script.js
+++ src/components/sidenav/demoBasicUsage/script.js
@@ -1,8 +1,11 @@
 angular
   .module('sidenavDemo1', ['ngMaterial'])
   .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.toggleLeft = buildToggler('left');
+    $scope.toggleLeft = buildDelayedToggler('left');
     $scope.toggleRight = buildToggler('right');
+    $scope.isOpenRight = function(){
+      return $mdSidenav('right').isOpen();
+    };
 
     /**
      * Supplies a function that will continue to operate until the
@@ -26,14 +29,24 @@ angular
      * Build handler to open/close a SideNav; when animation finishes
      * report completion in console
      */
-    function buildToggler(navID) {
+    function buildDelayedToggler(navID) {
       return debounce(function() {
-        $mdSidenav(navID).
-            toggle().
-            then(function () {
-              $log.debug("toggle " + navID + " is done");
-            });
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
       }, 200);
+    }
+
+    function buildToggler(navID) {
+      return function() {
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }
     }
   })
   .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
