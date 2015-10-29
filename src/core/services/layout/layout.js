4649b93b0ccc9d84278e93ce0f1e8b0250e96f16
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-10-29 09:54:47 -0500

    fix(layout): layout-margin and layout-padding fixes for child containers
    **layout-margin** and **layout-padding** were not being applied to child containers unless the *.flex* class was present. This has been fixed.
    
    * layout-margin and layout-padding now affect immediate children
    * layout-margin suffixes are supported (-sm, -gt-sm, -md, -gt-md, -lg, -gt-lg)
    * layout-padding suffixes are supported (-sm, -gt-sm, -md, -gt-md, -lg, -gt-lg)
    
    > With 'layout-margin', developers may need to override the child container flex sizes to fit properly in `layout="row"` modes. See http://codepen.io/team/AngularMaterial/pen/BoVBaE for details.
    
    Closes #5425. Refs #5014. Refs #5156.

--- src/core/services/layout/layout.js
+++ src/core/services/layout/layout.js
@@ -141,8 +141,22 @@
 
     // Attribute directives with no value(s) and NO breakpoints
 
-    .directive('layoutMargin', attributeWithoutValue('layout-margin'))
     .directive('layoutPadding', attributeWithoutValue('layout-padding'))
+    .directive('layoutPaddingSm', attributeWithoutValue('layout-padding-sm'))
+    .directive('layoutPaddingGtSm', attributeWithoutValue('layout-padding-gt-sm'))
+    .directive('layoutPaddingMd', attributeWithoutValue('layout-padding-md'))
+    .directive('layoutPaddingGtMd', attributeWithoutValue('layout-padding-gt-md'))
+    .directive('layoutPaddingLg', attributeWithoutValue('layout-padding-lg'))
+    .directive('layoutPaddingGtLg', attributeWithoutValue('layout-padding-gt-lg'))
+    
+    .directive('layoutMargin', attributeWithoutValue('layout-margin'))
+    .directive('layoutMarginSm', attributeWithoutValue('layout-margin-sm'))
+    .directive('layoutMarginGtSm', attributeWithoutValue('layout-margin-gt-sm'))
+    .directive('layoutMarginMd', attributeWithoutValue('layout-margin-md'))
+    .directive('layoutMarginGtMd', attributeWithoutValue('layout-margin-gt-md'))
+    .directive('layoutMarginLg', attributeWithoutValue('layout-margin-lg'))
+    .directive('layoutMarginGtLg', attributeWithoutValue('layout-margin-gt-lg'))
+
     .directive('layoutWrap', attributeWithoutValue('layout-wrap'))
     .directive('layoutNoWrap', attributeWithoutValue('layout-no-wrap'))
     .directive('layoutFill', attributeWithoutValue('layout-fill'))
