1946bfefdc31e706f74b62e2c8c71c507dcd415b
Topher Fangio <topher@profoundry.us>
2015-11-03 17:31:49 -0700

    docs($mdToast): Update docs with more info and better style.
    It was not clear that the `$mdToast.simple()` method returned
    an object which also had all of the methods/options available
    from the `build()` and `show()` methods.
    
    * add a short comment to inform developers.
    * update styles to be more readable.
    
    Fixes #2420. Closes #5550.

--- src/components/virtualRepeat/virtual-repeater.js
+++ src/components/virtualRepeat/virtual-repeater.js
@@ -126,19 +126,9 @@ function VirtualRepeatContainerController($$rAF, $parse, $window, $scope, $eleme
   this.sizer = this.scroller.getElementsByClassName('md-virtual-repeat-sizer')[0];
   this.offsetter = this.scroller.getElementsByClassName('md-virtual-repeat-offsetter')[0];
 
-  // $$rAF(angular.bind(this, this.updateSize));
-//
-//   // TODO: Come up with a more robust (But hopefully also quick!) way of
-//   // detecting that we're not visible.
-//   if ($attrs.ngHide) {
-//     $scope.$watch($attrs.ngHide, angular.bind(this, function(hidden) {
-//       if (!hidden) {
-//         $$rAF(angular.bind(this, this.updateSize));
-//       }
-//     }));
-//   }
-
+  // TODO: Come up with a more robust (But hopefully also quick!) way of
   var boundUpdateSize = angular.bind(this, this.updateSize);
+
   $$rAF(function() {
     boundUpdateSize();
 
