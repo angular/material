2760f67e6daeff73aa4a40ae75171d192fabdb70
Ryan Schmukler <rschmukler@gmail.com>
2015-11-09 15:02:01 -0500

    fix(select): fix auto-complete element not being removed from form
    closes #5575

--- src/components/select/select.js
+++ src/components/select/select.js
@@ -227,10 +227,12 @@ function SelectDirective($mdSelect, $mdUtil, $mdTheming, $mdAria, $compile, $par
 
       if (attr.name && formCtrl) {
         var selectEl = element.parent()[0].querySelector('select[name=".' + attr.name + '"]');
-        var controller = angular.element(selectEl).controller();
-        if (controller) {
-          formCtrl.$removeControl(controller);
-        }
+        $mdUtil.nextTick(function() {
+          var controller = angular.element(selectEl).controller('ngModel');
+          if (controller) {
+            formCtrl.$removeControl(controller);
+          }
+        });
       }
 
       if (formCtrl) {
