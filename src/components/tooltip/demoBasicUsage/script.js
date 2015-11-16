b70234e38c96fdf893e6562569765143c456307b
Paul Gschwendtner <paulgschwendtner@me.com>
2015-11-16 07:15:09 +0100

    fix(docs, demo): tooltip mdDelay documented and demo
    *  update docs with default mdDelay == 0 mSecs
    *  update tooltip demo to show md-delay options and affects
    *  remove .md-fab line-height for tooltip fab buttons.
    
    Fixes #6970. Fixes #1847. Closes #5748

--- src/components/tooltip/demoBasicUsage/script.js
+++ src/components/tooltip/demoBasicUsage/script.js
@@ -5,6 +5,11 @@ angular.module('tooltipDemo1', ['ngMaterial'])
     tipDirection : ''
   };
 
+  $scope.demo.delayTooltip = undefined;
+  $scope.$watch('demo.delayTooltip',function(val) {
+    $scope.demo.delayTooltip = parseInt(val, 10) || 0;
+  });
+
   $scope.$watch('demo.tipDirection',function(val) {
     if (val && val.length ) {
       $scope.demo.showTooltip = true;
