f2ab1b3eb6d8f70ded8c2418e5c731a7371a8d21
Michael Prentice <splaktar@gmail.com>
2015-11-18 00:43:11 -0500

    style(layout): fix typo in warnAttrNotSupported
    change from "layout-lt-mdhas been deprecated. Please use a `layout-gt-<xxx>` variant."
    to "layout-lt-md has been deprecated. Please use a `layout-gt-<xxx>` variant."

--- src/core/services/layout/layout.js
+++ src/core/services/layout/layout.js
@@ -353,7 +353,7 @@
   function warnAttrNotSupported(className) {
     var parts = className.split("-");
     return ["$log", function($log) {
-      $log.warn(className + "has been deprecated. Please use a `" + parts[0] + "-gt-<xxx>` variant.");
+      $log.warn(className + " has been deprecated. Please use a `" + parts[0] + "-gt-<xxx>` variant.");
       return angular.noop;
     }];
   }
