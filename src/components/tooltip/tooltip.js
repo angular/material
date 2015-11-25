5ea998769defa239eb36f44780758dc1a3588b70
SanderElias <sanderelias+git@gmail.com>
2015-11-25 10:48:40 +0100

    fix(tooltip): tooltip sometimes not hidden after element is disabled.
    use an mutationobserver to make sure the tooltip is hidden after an element becomes disabled.
    
    fixes: #2602

--- src/components/tooltip/tooltip.js
+++ src/components/tooltip/tooltip.js
@@ -122,6 +122,23 @@ function MdTooltipDirective($timeout, $window, $$rAF, $document, $mdUtil, $mdThe
 
       var ngWindow = angular.element($window);
 
+      // add an mutationObserver when there is support for it
+      // and the need for it in the form of viable host(parent[0])
+      if (parent[0] && 'MutationObserver' in $window) {
+        // use an mutationObserver to tackle #2602
+        var attributeObserver = new MutationObserver(function(mutations) {
+          mutations
+            .forEach(function (mutation) {
+              if (mutation.attributeName === 'disabled' && parent[0].disabled) {
+                setVisible(false);
+                scope.$digest(); // make sure the elements gets updated
+              }
+            });
+        });
+
+        attributeObserver.observe(parent[0], { attributes: true});
+      };
+
       // Store whether the element was focused when the window loses focus.
       var windowBlurHandler = function() {
         elementFocusedOnWindowBlur = document.activeElement === parent[0];
@@ -133,6 +150,7 @@ function MdTooltipDirective($timeout, $window, $$rAF, $document, $mdUtil, $mdThe
       scope.$on('$destroy', function() {
         ngWindow.off('blur', windowBlurHandler);
         ngWindow.off('resize', debouncedOnResize);
+        attributeObserver && attributeObserver.disconnect();
       });
 
       var enterHandler = function(e) {
