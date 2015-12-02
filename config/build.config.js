7acd8684b2a6606a19f8c4538a322c2c4e8d7de0
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-12-02 10:00:38 -0600

    update(core): detect incompatible ngTouch usages
    * add core.spec.js to test ngTouch detection logic
    
    Fixes #6032.

--- config/build.config.js
+++ config/build.config.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var versionFile = __dirname + '/../dist/commit';
 
 module.exports = {
-  ngVersion: '1.4.7',
+  ngVersion: '1.4.8',
   version: pkg.version,
   repository: pkg.repository.url
     .replace(/^git/,'https')
--- src/core/core.js
+++ src/core/core.js
@@ -10,7 +10,24 @@ angular
     'material.core.gestures',
     'material.core.theming'
   ])
-  .config(MdCoreConfigure);
+  .config(MdCoreConfigure)
+  .run(DetectNgTouch);
+
+
+/**
+ * Detect if the ng-Touch module is also being used.
+ * Warn if detected.
+ */
+function DetectNgTouch($log, $injector) {
+  if ( $injector.has('$swipe') ) {
+    var msg = "" +
+      "You are using the ngTouch module. \n" +
+      "Angular Material already has mobile click, tap, and swipe support... \n" +
+      "ngTouch is not supported with Angular Material!";
+    $log.warn(msg);
+  }
+}
+
 
 function MdCoreConfigure($provide, $mdThemingProvider) {
 
