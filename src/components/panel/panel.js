efa36fa1a43ff1759fa6e167587672be2e9a8c57
Erin Coughlan <ecoughlan@google.com>
2016-01-29 13:50:26 -0800

    All changes from Round 1 of commments.

--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -8,14 +8,19 @@ angular
     ])
     .service('$mdPanel', MdPanelService);
 
+
+/***************************************************************************************
+ *                              PUBLIC DOCUMENTATION                                   *
+ ***************************************************************************************/
+
 /**
  * @ngdoc service
  * @name $mdPanel
  * @module material.components.panel
  *
  * @description
- * `$mdPanel` is a general component that can be used for any panels on the screen,
- * such as a tooltip, dialog, or custom popup.
+ * `$mdPanel` is a robust, low-level service for creating floating panels on
+ * the screen. It can be used to implement tooltips, dialogs, pop-ups, etc.
  *
  * @usage
  * <hljs lang="js">
@@ -24,56 +29,42 @@ angular
  *
  *   angular
  *       .module('demoApp', ['ngMaterial'])
- *       .controller('PizzaBuilderController', PizzaBuilderController)
- *       .controller('PizzaDialogController', DialogController);
+ *       .controller('DemoDialogController', DialogController);
  *
- *   function PizzaBuilderController($element, $mdPanel) {
- *     var panelRef;
- *     var toppings = [
- *       'pepperoni',
- *       'sausage',
- *       'ham',
- *       'peppers',
- *       'onions',
- *       'pineapple'
- *     ];
+ *   var panelRef;
  *
- *     function showPizzaPanel($event) {
- *       var config = {
- *         parent: angular.element(document.body),
- *         controller: DialogController,
- *         controllerAs: 'ctrl',
- *         position: $element,
- *         targetEvent: $event,
- *         template:
- *           '<div class="pizza-panel">' +
- *           '  <h2>Choose your favorite topping.</h2>' +
- *           '  <md-list>' +
- *           '    <md-list-item ng-repeat="topping in ctrl.toppings">' +
- *           '      <md-button ng-click="ctrl.favoriteTopping = topping">' +
- *           '        {{ topping }}' +
- *           '      </md-button>' +
- *           '    </md-list-item>' +
- *           '  </md-list>' +
- *           '  <div class="actions">' +
- *           '    <md-button ng-click="ctrl.closeDialog()">CANCEL</md-button>' +
- *           '  </div>' +
- *           '</div>',
- *         clickOutsideToClose: true,
- *         escapeToClose: true,
- *         focusOnOpen: true
- *       }
- *       panelRef = $mdPanel.create(config);
- *       panelRef.open()
- *           .finally(function() {
- *             panelRef = undefined;
- *           });
+ *   function showPanel($event) { *
+ *     var panelPosition = $mdPanelPosition
+ *         .absolute()
+ *         .top('50%')
+ *         .left('50%');
+ *
+ *     var panelAnimation = $mdPanelAnimation
+ *         .targetEvent($event)
+ *         .defaultAnimation('md-panel-animate-fly')
+ *         .closeTo('.show-button');
+ *
+ *     var config = {
+ *       attachTo: angular.element(document.body),
+ *       controller: DialogController,
+ *       controllerAs: 'ctrl',
+ *       position: panelPosition,
+ *       animation: panelAnimation,
+ *       targetEvent: $event,
+ *       template: 'dialog-template.html',
+ *       clickOutsideToClose: true,
+ *       escapeToClose: true,
+ *       focusOnOpen: true
  *     }
+ *     panelRef = $mdPanel.create(config);
+ *     panelRef.open()
+ *         .finally(function() {
+ *           panelRef = undefined;
+ *         });
  *   }
  *
  *   function DialogController($mdPanelRef, toppings) {
  *     var toppings;
- *     var favoriteTopping;
  *
  *     function closeDialog() {
  *       $mdPanelRef.close();
@@ -98,17 +89,13 @@ angular
  *   - `groupName` - `{string=}`: Name of panel groups. This group name is used for
  *     configuring the number of open panels and identifying specific behaviors for
  *     groups. For instance, all tooltips will be identified using the same groupName.
- *   - `maxOpen` - `{number=}` : Maximum number of panel in a specific group that
- *     can be opened at a given time. If multiple panels have the same groupName, but
- *     different values of maxOpen, only the first one will be considered. A warning
- *     will be logged in the console.
  *   - `template` - `{string=}`: HTML template to show in the dialog. This **must** be
  *     trusted HTML with respect to Angular’s
  *     [$sce service](https://docs.angularjs.org/api/ng/service/$sce).
  *   - `templateUrl` - `{string=}`: The URL that will be used as the content of
  *     the panel.
  *   - `controller` - `{(function|string)=}`: The controller to associate with the
- *     panel. The controller will be injected with a reference to the returned
+ *     panel. The controller can inject a reference to the returned
  *     panelRef, which allows the panel to be closed, hidden, and shown.
  *     Any fields passed in through locals or resolve will be bound to the controller.
  *   - `controllerAs` - `{string=}`: An alias to assign the controller to on the
@@ -119,51 +106,12 @@ angular
  *     3.
  *   - `resolve` - `{Object=}`: Similar to locals, except it takes promises as
  *     values. The panel will not open until all of the promises resolve.
- *   - `parent` - `{Element=}`: The element to append the dialog to. Defaults to
+ *   - `attachTo` - `{Element=}`: The element to attach the panel to. Defaults to
  *     appending to the root element of the application.
- *   - `position` - `{(string|Element|Object)=}`: The query selector, DOM element,
- *     or the Rect object that is used to determine the position (top, left, height,
- *     width) of the panel. By default, the panel will originate exactly from the
- *     source.
- *   - `alignX` - `{string=}`: What point of origin to use on the x-axis. There are 4
- *     possible values: start, center, end, justify. This is only necessary if you do
- *     not want the panel to intelligently determine location based on screen edges.
- *       - `start`:
- *         The panel is aligned to the start of the originating element in the
- *         x-direction. This value is the left of the element if the direction is
- *         left-to-right and the right of the element if the direction is
- *         right-to-left.
- *       - `end`:
- *         The panel is aligned to the end of the originating element in the
- *         x-direction. This value is the right of the element if the direction is
- *         left-to-right and the left of the element if the direction is
- *         right-to-left.
- *   - `alignY` - `{string=}`: What point of origin to use on the y-axis. There are 4
- *     possible values: start, center, end, justify. This is only necessary if you do
- *     not want the panel to intelligently determine location based on screen edges.
- *       - `top`:
- *         The panel is aligned to the top of the originating element.
- *       - `bottom`:
- *         The panel is aligned to the bottom of the originating element.
- *   - `overlap` - `{boolean=}`: Whether the panel should overlap the originating
- *     element. This value is true by default.
- *   - `center` - `{boolean=}`: If true, centers the panel in the viewport, both
- *     horizontally and vertically. If this option is set, `alignX` and `alignY` will
- *     be ignored.
- *   - `animation` - `{string=}`: The classname for the type of animation. There are
- *     several built-in animations that you can use:
- *       `md-animate-fly`: The panel flies in and out from the specified elements.
- *       `md-animate-scale`: The panels scales in and out.
- *       `md-animate-none`: No animations.
- *   - `targetEvent` - `{DOMClickEvent=}`: A click's event object. When passed in as
- *     an option, the location of the click will be used as the starting point for the
- *     opening animation of the panel.
- *   - `openFrom` - `{(string|Element|Object)=}`: The query selector, DOM element,
- *     or the Rect object that is used to determine the bounds (top, left, height,
- *     width) from which the panel will originate.
- *   - `closeTo` - `{(string|Element|Object)=}`: The query selector, DOM element, or
- *     the Rect object that is used to determine the bounds (top, left, height, width)
- *     from which the panel will close to.
+ *   - `position` - `{$mdPanelPosition=}`: An $mdPanelPosition object that specifies
+ *     the alignment of the panel. For more information, see $mdPanelPosition.
+ *   - `animation` - `{$mdPanelAnimation=}`: An $mdPanelAnimation object that specifies
+ *     the animation of the panel. For more information, see $mdPanelAnimation.
  *   - `hasBackdrop` - `{boolean=}`: Whether there should be an opaque backdrop behind
  *     the panel. Defaults to false.
  *   - `escapeToClose` - `{boolean=}`: Whether the user can press escape to close the
@@ -184,44 +132,25 @@ angular
  * @returns {$mdPanelRef} panelRef
  */
 
-
-
 /**
- * A service that is used for controlling/displaying panels on the screen.
+ * @ngdoc method
+ * @name $mdPanel#setGroupMaxOpen
+ * @description
+ * Sets the maximum number of panels in a group that can be opened at a given time.
  *
- * @param $rootElement
- * @param $injector
- * @final
- * @constructor
- * @ngInject
+ * @param groupName {string} The name of the group to configure.
+ * @param maxOpen {number} The max number of panels that can be opened.
  */
-function MdPanelService($rootElement, $injector) {
-  // Default config options for the panel.
-  this.defaultConfigOptions_ = {
-    parent: $rootElement
-  };
-
-  this.config_ = this.defaultConfigOptions_;
-  this.injector_ = $injector;
-}
 
 
-MdPanelService.prototype.create = function(opt_config) {
-  var configSettings = opt_config || {};
-  for (var key in configSettings) {
-    if (configSettings.hasOwnProperty(key)) {
-      this.config_[key] = configSettings[key];
-    }
-  }
-
-  return new $mdPanelRef(this.injector_, this.config_);
-}
+/***************************************************************************************
+ *                                   $mdPanelRef                                       *
+ ***************************************************************************************/
 
 
 /**
  * @ngdoc object
  * @name $mdPanelRef
- *
  * @description
  * A reference to a created panel. This reference contains a unique id for the panel,
  * along with the following properties:
@@ -241,21 +170,7 @@ MdPanelService.prototype.create = function(opt_config) {
  *   - `onRemoving` - `{function=}`: Callback function used to announce the
  *     close/hide() action is starting. This allows developers to run custom
  *     animations in parallel the close animations.
- *
- * @param $injector
- * @param config
- *
  */
-function $mdPanelRef($injector, config) {
-  this.mdUtil_ = $injector.get('$mdUtil');
-  this.q_ = $injector.get('$q');
-
-  this.config = config;
-  this.id = 'panel_' + this.mdUtil_.nextUid();
-
-  this.openPromise_;
-}
-
 
 /**
  * @ngdoc method
@@ -263,13 +178,8 @@ function $mdPanelRef($injector, config) {
  * @description
  * If the panel is not visible, opens an already created and configured panel.
  *
- * @returns {promise} A promise that is resolved when the panel is closed.
+ * @returns {angular.$q.Promise} A promise that is resolved when the panel is closed.
  */
-$mdPanelRef.prototype.open = function() {
-  this.openPromise_ = this.q_.defer();
-  return this.openPromise_.promise;
-};
-
 
 /**
  * @ngdoc method
@@ -279,6 +189,177 @@ $mdPanelRef.prototype.open = function() {
  * from `$mdPanelRef#open`. This method destroys the reference to the panel. In order
  * to open the panel again, a new one must be created.
  */
-$mdPanelRef.prototype.close = function() {
+
+
+/***************************************************************************************
+ *                                 $mdPanelPosition                                    *
+ ***************************************************************************************/
+
+
+/**
+ * @ngdoc object
+ * @name $mdPanelPosition
+ * @description
+ * Object for configuring the position of the panel. Examples:
+ *
+ * Centering the panel:
+ * `$mdPanelPosition.absolute().top(‘50%’).left(‘50%’);`
+ *
+ * Overlapping the panel with an element:
+ * `$mdPanelPosition.relativeTo(someElement).top(‘0’).left(‘0’);`
+ *
+ * Aligning the panel with the bottom of an element:
+ * `$mdPanelPosition.relativeTo(someElement);`
+ */
+
+/**
+ * @ngdoc method
+ * @name $mdPanelPosition#absolute
+ * @description
+ * Positions the panel absolutely relative to the parent element. If the parent is
+ * document.body, this is equivalent to positioning the panel absolutely within the
+ * viewport.
+ * @returns {$mdPanelPosition}
+ */
+
+/**
+ * @ngdoc method
+ * @name $mdPanelPosition#relativeTo
+ * @description
+ * Positions the panel relative to a specific element.
+ * @param {angular.JQLite} element Element to position the panel with respect to.
+ * @returns {$mdPanelPosition}
+ */
+
+/**
+ * @ngdoc method
+ * @name $mdPanelPosition#top
+ * @description
+ * Sets the value of `top` for the panel.
+ * @param {string} top Value of `top`.
+ * @returns {$mdPanelPosition}
+ */
+
+/**
+ * @ngdoc method
+ * @name $mdPanelPosition#bottom
+ * @description
+ * Sets the value of `bottom` for the panel.
+ * @param {string} top Value of `bottom`.
+ * @returns {$mdPanelPosition}
+ */
+
+/**
+ * @ngdoc method
+ * @name $mdPanelPosition#left
+ * @description
+ * Sets the value of `left` for the panel.
+ * @param {string} top Value of `left`.
+ * @returns {$mdPanelPosition}
+ */
+
+/**
+ * @ngdoc method
+ * @name $mdPanelPosition#right
+ * @description
+ * Sets the value of `right` for the panel.
+ * @param {string} top Value of `right`.
+ * @returns {$mdPanelPosition}
+ */
+
+/**
+ * @ngdoc method
+ * @name $mdPanelPosition#offsetX
+ * @description
+ * Sets the value of the offset in the x-direction.
+ * @param {string} offsetX
+ * @returns {$mdPanelPosition}
+ */
+
+/**
+ * @ngdoc method
+ * @name $mdPanelPosition#offsetY
+ * @description
+ * Sets the value of the offset in the y-direction.
+ * @param {string} offsetY
+ * @returns {$mdPanelPosition}
+ */
+
+
+/***************************************************************************************
+ *                                   IMPLEMENTATION                                    *
+ ***************************************************************************************/
+
+
+/**
+ * A service that is used for controlling/displaying panels on the screen.
+ * @param {!angular.JQLite} $rootElement
+ * @param {!angular.$injector} $injector
+ * @final @constructor @ngInject
+ */
+function MdPanelService($rootElement, $injector) {
+  // Default config options for the panel.
+  this.defaultConfigOptions_ = {
+    attachTo: $rootElement
+  };
+
+  this.config_ = this.defaultConfigOptions_;
+  this.$injector_ = $injector;
+}
+
+
+/**
+ * Creates a panel with the specified options.
+ * @param {!Object=} opt_config Configuration object for the panel.
+ * @returns {!$mdPanelRef}
+ */
+MdPanelService.prototype.create = function(opt_config) {
+  var configSettings = opt_config || {};
+  angular.forEach(configSettings, function(value, key) {
+    if (configSettings.hasOwnProperty(key)) {
+      this.config_[key] = value;
+    }
+  }, this);
+
+  return new MdPanelRef(this.$injector_, this.config_);
+};
+
+
+/**
+ * A reference to a created panel. This reference contains a unique id for the panel,
+ * along with properties/functions used to control the panel.
+ *
+ * @param {!angular.$injector} $injector
+ * @param {!Object} config
+ * @final @constructor
+ */
+function MdPanelRef($injector, config) {
+  this.$mdUtil_ = $injector.get('$mdUtil');
+  this.$q_ = $injector.get('$q');
+
+  this.config = config;
+  this.id = 'panel_' + this.$mdUtil_.nextUid();
+
+  this.openPromise_;
+}
+
+
+/**
+ * Opens an already created and configured panel. If the panel is already visible,
+ * does nothing.
+ *
+ * @returns {!angular.$q.Promise} A promise that is resolved when the panel is closed.
+ */
+MdPanelRef.prototype.open = function() {
+  this.openPromise_ = this.$q_.defer();
+  return this.openPromise_.promise;
+};
+
+
+/**
+ * Closes the panel.
+ */
+MdPanelRef.prototype.close = function() {
   this.openPromise_.resolve(true);
 };
+
