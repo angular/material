angular/material

commit 09bd5a3b52f29dfe530192099d2c405f204ec3ad

fix(select): multiple no longer sets form to $dirty
closes #3933

src/components/select/select.js
@@ -184,6 +184,12 @@ function SelectDirective($mdSelect, $mdUtil, $mdTheming, $mdAria, $compile, $par
         }
       }
 
+      if (formCtrl) {
+        $mdUtil.nextTick(function() {
+          formCtrl.$setPristine();
+        });
+      }
+
       var originalRender = ngModelCtrl.$render;
       ngModelCtrl.$render = function() {
         originalRender();
@@ -489,6 +495,7 @@ function SelectMenuDirective($parse, $mdUtil, $mdTheming) {
         // reference. This allowed the developer to also push and pop from their array.
         $scope.$watchCollection($attrs.ngModel, function(value) {
           if (validateArray(value)) renderMultiple(value);
+          self.ngModel.$setPristine();
         });
       } else {
         delete ngModel.$validators['md-multiple'];

commit 531bc5c7455dd8d201569560e9a21c78841f53d4

fix(select): fix set form to pristine if ng-model for multiple select is predefined.
Bug introduced in 09bd5a3b52f29dfe530192099d2c405f204ec3ad

Fixes #6556

src/components/select/select.js
@@ -228,9 +228,12 @@ function SelectDirective($mdSelect, $mdUtil, $mdTheming, $mdAria, $compile, $par
         });
       }
 
-      if (formCtrl) {
+      if (formCtrl && angular.isDefined(attr.multiple)) {
         $mdUtil.nextTick(function() {
-          formCtrl.$setPristine();
+          var hasModelValue = ngModelCtrl.$modelValue || ngModelCtrl.$viewValue;
+          if (hasModelValue) {
+            formCtrl.$setPristine();
+          }
         });
       }
 

src/components/select/select.spec.js
@@ -387,6 +387,19 @@ describe('<md-select>', function() {
         expect(selectedOptions(el).length).toBe(0);
       }));
 
+      it('should keep the form pristine when model is predefined', inject(function($rootScope, $timeout, $compile) {
+        $rootScope.model = 2;
+        $rootScope.opts = [1, 2, 3, 4];
+        $compile('<form name="testForm">' +
+          '<md-select ng-model="model" name="multiSelect">' +
+            '<md-option ng-repeat="opt in opts" ng-value="opt"></md-option>' +
+          '</md-select></form>')($rootScope);
+        $rootScope.$digest();
+        $timeout.flush();
+
+        expect($rootScope.testForm.$pristine).toBe(true);
+      }));
+
     });
 
     describe('view->model', function() {
@@ -637,13 +650,25 @@ describe('<md-select>', function() {
           $rootScope.model = [];
           $rootScope.opts = [1, 2, 3, 4];
           $compile('<form name="testForm">' +
-            '<md-select ng-model="model", name="multiSelect" required="required" multiple="multiple">' +
+            '<md-select ng-model="model" name="multiSelect" required="required" multiple="multiple">' +
               '<md-option ng-repeat="opt in opts" ng-value="opt"></md-option>' +
             '</md-select></form>')($rootScope);
           $rootScope.$digest();
           expect($rootScope.testForm.$valid).toBe(false);
       }));
 
+      it('should keep the form pristine when model is predefined', inject(function($rootScope, $timeout, $compile) {
+        $rootScope.model = [1, 2];
+        $rootScope.opts = [1, 2, 3, 4];
+        $compile('<form name="testForm">' +
+          '<md-select ng-model="model" name="multiSelect" multiple="multiple">' +
+            '<md-option ng-repeat="opt in opts" ng-value="opt"></md-option>' +
+          '</md-select></form>')($rootScope);
+        $rootScope.$digest();
+        $timeout.flush();
+
+        expect($rootScope.testForm.$pristine).toBe(true);
+      }));
     });
 
     describe('view->model', function() {