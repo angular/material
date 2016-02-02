105c7ec2573dd8e3984ddb105dc4fb2d4c9739ed
Paul Gschwendtner <paulgschwendtner@me.com>
2016-02-02 21:43:49 +0100

    fix(list): show item content separated to the button
    Fixes #6984 Fixes #6488

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -136,9 +136,19 @@ function mdListItemDirective($mdAria, $mdConstant, $mdUtil, $timeout) {
           container.append(tEl.contents());
           tEl.addClass('md-proxy-focus');
         } else {
-          container = angular.element('<md-button class="md-no-style"><div class="md-list-item-inner"></div></md-button>');
-          copyAttributes(tEl[0], container[0]);
-          container.children().eq(0).append(tEl.contents());
+          // Element which holds the default list-item content.
+          container = angular.element('<div class="md-button md-no-style"><div class="md-list-item-inner"></div></div>');
+
+          // Button which shows ripple and executes primary action.
+          var buttonWrap = angular.element('<md-button class="md-no-style" md-no-focus-style></md-button>');
+          buttonWrap[0].setAttribute('aria-label', tEl[0].textContent);
+          copyAttributes(tEl[0], buttonWrap[0]);
+
+          // Append the button wrap before our list-item content, because it will overlay in relative.
+          container.prepend(buttonWrap);
+          container.children().eq(1).append(tEl.contents());
+          
+          tEl.addClass('md-button-wrap');
         }
 
         tEl[0].setAttribute('tabindex', '-1');
@@ -179,7 +189,11 @@ function mdListItemDirective($mdAria, $mdConstant, $mdUtil, $timeout) {
                 ( tAttrs.ngClick &&
                 isProxiedElement(secondaryItem) )
             )) {
-          secondaryItem.classList.remove('md-secondary');
+          // When using multiple secondary items we need to remove their secondary class to be
+          // orderd correctly in the list-item
+          if (hasSecondaryItemsWrapper) {
+            secondaryItem.classList.remove('md-secondary');
+          }
           tEl.addClass('md-with-secondary');
           container.append(secondaryItem);
         }
