ef0da76e92ad53c788d2f7a87d8f78e6e1720191
Paul Gschwendtner <paulgschwendtner@me.com>
2016-02-07 23:07:15 +0100

    fix(list): apply ripple only once and fix list-inner alignment for multi-line lists.
    - Currently the list applied two ripples to list items with a `ng-click` / `proxy element`.
    - The actual list-item content wasn't aligned correctly when using a multi-line list.
    
    Fixes #7059

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -137,10 +137,15 @@ function mdListItemDirective($mdAria, $mdConstant, $mdUtil, $timeout) {
           tEl.addClass('md-proxy-focus');
         } else {
           // Element which holds the default list-item content.
-          container = angular.element('<div class="md-button md-no-style"><div class="md-list-item-inner"></div></div>');
+          container = angular.element(
+            '<button class="md-button md-no-style"><div class="md-list-item-inner"></div></button>'
+          );
 
           // Button which shows ripple and executes primary action.
-          var buttonWrap = angular.element('<md-button class="md-no-style" md-no-focus-style></md-button>');
+          var buttonWrap = angular.element(
+            '<md-button class="md-no-style" md-no-focus-style></md-button>'
+          );
+
           buttonWrap[0].setAttribute('aria-label', tEl[0].textContent);
           copyAttributes(tEl[0], buttonWrap[0]);
 
@@ -226,7 +231,8 @@ function mdListItemDirective($mdAria, $mdConstant, $mdUtil, $timeout) {
 
         var proxies    = [],
             firstChild = $element[0].firstElementChild,
-            hasClick   = firstChild && hasClickEvent(firstChild);
+            hasClick   = firstChild && firstChild.firstElementChild &&
+                         hasClickEvent(firstChild.firstElementChild);
 
         computeProxies();
         computeClickable();
