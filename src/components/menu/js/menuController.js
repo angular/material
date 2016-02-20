be5226b6745f4bc5bf8f0cbb613ede91e69f7d5d
snaumets <snaumets@gmail.com>
2016-02-19 17:08:11 -0800

    fix(handleMenuItemHover): allow any mdButton to be a focusableTarget
    Allow any mdButton element (`<button>` or `<a>` tag) to take focus when it is hovered over.
    Previously, only a non-disabled button could be a focusable target when being hovered over.
    Now, any non-disabled element with the md-button class can be a focusable target and take focus.
    
    This fix closes issue https://github.com/angular/material/issues/7220.

--- src/components/menu/js/menuController.js
+++ src/components/menu/js/menuController.js
@@ -92,7 +92,7 @@ function MenuController($mdMenu, $attrs, $element, $scope, $mdUtil, $timeout, $r
         nestedMenu.open();
       }
     }, nestedMenu ? 100 : 250);
-    var focusableTarget = event.currentTarget.querySelector('button:not([disabled])');
+    var focusableTarget = event.currentTarget.querySelector('.md-button:not([disabled])');
     focusableTarget && focusableTarget.focus();
   };
 
