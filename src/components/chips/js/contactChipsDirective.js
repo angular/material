4e8eb8f1baa4061d4af7e83da83f3bd8162e1f33
WavedClover497 <wavedclover.git@gmail.com>
2016-02-23 00:14:36 +1100

    docs(contactChipsDemo): Add demo of using a promise in the query func
    When searching for contacts a promise can be returned that contains the matching entries.
    Appended a demonstration of this to the existing contact chips demo.

--- src/components/chips/js/contactChipsDirective.js
+++ src/components/chips/js/contactChipsDirective.js
@@ -21,7 +21,8 @@ angular
  * @param {string=} secondary-placeholder Placeholder text that will be forwarded to the input,
  *    displayed when there is at least on item in the list
  * @param {expression} md-contacts An expression expected to return contacts matching the search
- *    test, `$query`.
+ *    test, `$query`. If this expression involves a promise, a loading bar is displayed while
+ *    for it to resolve.
  * @param {string} md-contact-name The field name of the contact object representing the
  *    contact's name.
  * @param {string} md-contact-email The field name of the contact object representing the
