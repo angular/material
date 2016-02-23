879081fea0d19f23b2b05298ee1e13bf02df7a74
Thomas Burleson <ThomasBurleson@Gmail.com>
2016-02-23 11:22:49 -0600

    update(ci): remove CI testing with Chrome
    *  use of 'sudo' has negative perf impacts with Travis containers
    *  should use SauceLabs to test with Chrome and other browsers.

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -70,7 +70,7 @@ module.exports = function(config) {
     // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
     // - PhantomJS
     // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    browsers: ['Chrome', 'PhantomJS2']
+    browsers: ['Firefox', 'PhantomJS2']
   });
 
 };
