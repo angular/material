ccc1fdb5855eab507de04a55e6b26870744539c1
DevVersion <paulgschwendtner@me.com>
2016-02-28 11:06:48 +0100

    fix(icon): disable e11 focus for SVG icons
    Currently IE11 always sets the SVG icons focusable.
    This can be reproduced by tabbing through the `Getting Started` section of the docs.
    There will be always two tab-steps when iterating through the icon button in IE11.
    
    Fixes #7250
    
      Closes #7321

--- src/components/icon/js/iconService.js
+++ src/components/icon/js/iconService.js
@@ -523,7 +523,8 @@
            'height': '100%',
            'width' : '100%',
            'preserveAspectRatio': 'xMidYMid meet',
-           'viewBox' : this.element.getAttribute('viewBox') || ('0 0 ' + viewBoxSize + ' ' + viewBoxSize)
+           'viewBox' : this.element.getAttribute('viewBox') || ('0 0 ' + viewBoxSize + ' ' + viewBoxSize),
+           'focusable': false // Disable IE11s default behavior to make SVGs focusable
          }, function(val, attr) {
            this.element.setAttribute(attr, val);
          }, this);
