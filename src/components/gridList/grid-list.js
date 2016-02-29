a8b886ab3a9144b31b84453bffc3a557f42383c5
Elad Bezalel <elad.bezalel@gmail.com>
2016-03-01 00:59:05 +0200

    fix(grid-list): validate that row height is defined
    - throwing more informative error about missing row height attribute
    
    fixes #6870
    
      Closes #7348

--- src/components/gridList/grid-list.js
+++ src/components/gridList/grid-list.js
@@ -382,6 +382,10 @@ function GridListDirective($interpolate, $mdConstant, $mdGridLayout, $mdMedia) {
 
     function getRowHeight() {
       var rowHeight = $mdMedia.getResponsiveAttribute(attrs, 'md-row-height');
+      if (!rowHeight) {
+        throw 'md-grid-list: md-row-height attribute was not found';
+      }
+
       switch (getRowMode()) {
         case 'fixed':
           return applyDefaultUnit(rowHeight);
@@ -395,6 +399,10 @@ function GridListDirective($interpolate, $mdConstant, $mdGridLayout, $mdMedia) {
 
     function getRowMode() {
       var rowHeight = $mdMedia.getResponsiveAttribute(attrs, 'md-row-height');
+      if (!rowHeight) {
+        throw 'md-grid-list: md-row-height attribute was not found';
+      }
+
       if (rowHeight == 'fit') {
         return 'fit';
       } else if (rowHeight.indexOf(':') !== -1) {
