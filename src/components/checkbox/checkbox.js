f790e93eb2ed2d6dbefa1074b2a68300ba2f8675
DevVersion <paulgschwendtner@me.com>
2016-03-12 00:03:06 +0100

    fix(checkbox): pointer events disable ripple events too
    We should not disable the ripple events by using `pointer-events: none`. IE11 always focuses the div, instead of the `md-checkbox`, and that's causing issues with ngModel etc.
    
    * redirect the focus, instead of preventing the whole focus / pointer events.
    *  @see https://connect.microsoft.com/IE/feedback/details/1028411/
    
    Fixes #7538. Closes #7541.

--- src/components/checkbox/checkbox.js
+++ src/components/checkbox/checkbox.js
@@ -68,6 +68,7 @@ function MdCheckboxDirective(inputDirective, $mdAria, $mdConstant, $mdTheming, $
   // **********************************************************
 
   function compile (tElement, tAttrs) {
+    var container = tElement.children();
 
     tAttrs.type = 'checkbox';
     tAttrs.tabindex = tAttrs.tabindex || '0';
@@ -79,6 +80,12 @@ function MdCheckboxDirective(inputDirective, $mdAria, $mdConstant, $mdTheming, $
       if (this.hasAttribute('disabled')) {
         event.stopImmediatePropagation();
       }
+    });
+
+    // Redirect focus events to the root element, because IE11 is always focusing the container element instead
+    // of the md-checkbox element. This causes issues when using ngModelOptions: `updateOnBlur`
+    container.on('focus', function() {
+      tElement.focus();
     });
 
     return function postLink(scope, element, attr, ngModelCtrl) {
