ab5450d32b37fbd2d92375b950e26b57151510fc
Anand Patel <apat183@gmail.com>
2016-03-17 10:47:11 -0400

    feat($md-contact-chips): add md-chips feats to md-contact-chips

--- src/components/chips/js/contactChipsDirective.js
+++ src/components/chips/js/contactChipsDirective.js
@@ -1,6 +1,6 @@
 angular
-    .module('material.components.chips')
-    .directive('mdContactChips', MdContactChips);
+  .module('material.components.chips')
+  .directive('mdContactChips', MdContactChips);
 
 /**
  * @ngdoc directive
@@ -30,9 +30,22 @@ angular
  * @param {string} md-contact-image The field name of the contact object representing the
  *    contact's image.
  * @param {boolean=} readonly Disables list manipulation (deleting or adding list items), hiding the input and delete buttons
+ * @param {number=} md-max-chips The maximum number of chips allowed to add through user input.
+ * <br/><br/>The validation property `md-max-chips` can be used when the max chips
+ *  amount is reached.
+ * @param {expression} md-transform-chip An expression of form `myFunction($chip)` that when called
+ *  expects one of the following return values:
+ *    - an object representing the `$chip` input string
+ *    - `undefined` to simply add the `$chip` input string, or
+ *    - `null` to prevent the chip from being appended
  * @param {expression=} md-on-add An expression which will be called when a contact chip is selected.
  * @param {expression=} md-on-remove An expression which will be called when a contact chip is removed
  * @param {expression=} md-on-select An expression which will be called when a contact chip is selected.
+ * @param {string=} delete-hint A string read by screen readers instructing users that pressing
+ *  the delete key will remove the contact chip.
+ * @param {string=} delete-button-label A label for the delete button. Also hidden and read by
+ *  screen readers.
+ * @param {expression=} md-separator-keys An array of key codes used to separate contact chips.
  *
  *
  * @param {expression=} filter-selected Whether to filter selected contacts from the list of
@@ -60,9 +73,14 @@ var MD_CONTACT_CHIPS_TEMPLATE = '\
           ng-model="$mdContactChipsCtrl.contacts"\
           md-require-match="$mdContactChipsCtrl.requireMatch"\
           readonly="$mdContactChipsCtrl.readonly"\
+          md-max-chips="$mdContactChipsCtrl.maxChips"\
+          md-transform-chip="$mdContactChipsCtrl.transformChip($chip)"\
           md-on-add="$mdContactChipsCtrl.onAdd($chip)"\
           md-on-remove="$mdContactChipsCtrl.onRemove($chip)"\
           md-on-select="$mdContactChipsCtrl.onSelect($chip)"\
+          delete-hint="$mdContactChipsCtrl.deleteHint"\
+          delete-button-label="$mdContactChipsCtrl.deleteButtonLabel"\
+          md-separator-keys="$mdContactChipsCtrl.separatorKeys"\
           md-autocomplete-snap>\
           <md-autocomplete\
               md-menu-class="md-contact-chips-suggestions"\
@@ -108,39 +126,44 @@ var MD_CONTACT_CHIPS_TEMPLATE = '\
  * @ngInject
  */
 function MdContactChips($mdTheming, $mdUtil) {
-    return {
-        template: function(element, attrs) {
-            return MD_CONTACT_CHIPS_TEMPLATE;
-        },
-        restrict: 'E',
-        controller: 'MdContactChipsCtrl',
-        controllerAs: '$mdContactChipsCtrl',
-        bindToController: true,
-        compile: compile,
-        scope: {
-            contactQuery: '&mdContacts',
-            placeholder: '@',
-            secondaryPlaceholder: '@',
-            contactName: '@mdContactName',
-            contactImage: '@mdContactImage',
-            contactEmail: '@mdContactEmail',
-            contacts: '=ngModel',
-            readonly: '=readonly',
-            onAdd: '&mdOnAdd',
-            onRemove: '&mdOnRemove',
-            onSelect: '&mdOnSelect',
-            requireMatch: '=?mdRequireMatch',
-            highlightFlags: '@?mdHighlightFlags'
-        }
-    };
-
-    function compile(element, attr) {
-        return function postLink(scope, element, attrs, controllers) {
-
-            $mdUtil.initOptionalProperties(scope, attr);
-            $mdTheming(element);
-
-            element.attr('tabindex', '-1');
-        };
+  return {
+    template: function(element, attrs) {
+      return MD_CONTACT_CHIPS_TEMPLATE;
+    },
+    restrict: 'E',
+    controller: 'MdContactChipsCtrl',
+    controllerAs: '$mdContactChipsCtrl',
+    bindToController: true,
+    compile: compile,
+    scope: {
+      readonly: '=readonly',
+      contactQuery: '&mdContacts',
+      placeholder: '@',
+      secondaryPlaceholder: '@',
+      contactName: '@mdContactName',
+      contactImage: '@mdContactImage',
+      contactEmail: '@mdContactEmail',
+      contacts: '=ngModel',
+      maxChips: '@mdMaxChips',
+      transformChip: '&mdTransformChip',
+      onAdd: '&mdOnAdd',
+      onRemove: '&mdOnRemove',
+      onSelect: '&mdOnSelect',
+      deleteHint: '@',
+      deleteButtonLabel: '@',
+      separatorKeys: '=?mdSeparatorKeys',
+      requireMatch: '=?mdRequireMatch',
+      highlightFlags: '@?mdHighlightFlags'
     }
+  };
+
+  function compile(element, attr) {
+    return function postLink(scope, element, attrs, controllers) {
+
+      $mdUtil.initOptionalProperties(scope, attr);
+      $mdTheming(element);
+
+      element.attr('tabindex', '-1');
+    };
+  }
 }
