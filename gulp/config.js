12ce0debdcc39b1b01938f9ddb63b932e6d40d0a
Thomas Burleson <ThomasBurleson@Gmail.com>
2016-03-21 12:14:16 -0700

    fix(build, layout): remove deprecated print css
    * use `dist/layouts` directory to include all Layout raw SCSS, compiled css, and minifed css.
    * define `print` mediaQuery so the demo backgrounds are white and will print properly
    * `layout.print.css` is no longer valid and should not be included in the SCSS/CSS output
    
    Fixes #7660

--- gulp/config.js
+++ gulp/config.js
@@ -31,6 +31,12 @@ module.exports = {
     'src/core/style/typography.scss',
     'src/core/style/layout.scss'
   ],
+  scssLayoutFiles: [
+    'src/core/style/variables.scss',
+    'src/core/style/mixins.scss',
+    'src/core/style/layout.scss',
+    'src/core/services/layout/layout.scss'
+  ],
   scssLayoutAttributeFiles: [
       'src/core/style/variables.scss',
       'src/core/style/mixins.scss',
