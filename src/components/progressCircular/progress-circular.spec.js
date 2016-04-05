d86efaf81bdc1f85e290f202005f56570ad5dea3
crisbeto <crisbeto@abv.bg>
2016-04-05 22:24:31 +0200

    perf(progress-circular): switch back to keyframe animation for the rotation
    In #7402 I had to fall back to using JS to do the indeterminate rotation, because of IE and Edge which weren't accounting for the element's stroke and caused a wobble in the animation. I managed to work around this issue by specifying the `transform-origin` in all dimensions (x, y and z), as well as using the element's radius as the origin, instead of `center`.
    
    Closes #7901

--- src/components/progressCircular/progress-circular.spec.js
+++ src/components/progressCircular/progress-circular.spec.js
@@ -111,6 +111,11 @@ describe('mdProgressCircular', function() {
     $animate.flush();
   }));
 
+  it('should set the transform origin in all dimensions', function() {
+    var svg = buildIndicator('<md-progress-circular md-diameter="42px"></md-progress-circular>').find('svg').eq(0);
+    expect(svg.css('transform-origin')).toBe('21px 21px 21px');
+  });
+
   /**
    * Build a progressCircular
    */
