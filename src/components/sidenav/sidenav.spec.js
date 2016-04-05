d5900684ffd45ce9aacddfbef0e4df537916b179
Thomas Burleson <ThomasBurleson@Gmail.com>
2016-04-05 15:05:24 -0500

    fix(sidenav): mdSideNav should support deferred or instant component lookups
    Previous API does not support any way to determine if the component exists (has been instantiated) since the response will give a "fake" instance reference.
    The current API supports `$mdSideNav().find(id)`, `$mdSideNav().waitFor(id)`, `$mdSideNav(id)` usages.
    
    This fix will allow code like this:
    
    ```js
    // If the component has already been instantiated
    $scope.isOpenRight = function(){
          return $mdSidenav('right').isOpen();
    };
    
    var right;
    
    // If the component will be created later
    $scope.isOpenRight = function(){
      if ( angular.isUndefined(right) ) {
        $mdSidenav()
          .waitFor('right')
          .then(function(instance){
            right = instance;
          })
      }
      return right ? right.isOpen() : false;
    };
    
    ```

--- src/components/sidenav/sidenav.spec.js
+++ src/components/sidenav/sidenav.spec.js
@@ -5,7 +5,7 @@ describe('mdSidenav', function() {
     var el;
     inject(function($compile, $rootScope) {
       var parent = angular.element('<div>');
-      el = angular.element('<md-sidenav ' + (attrs||'') + '>');
+      el = angular.element('<md-sidenav ' + (attrs || '') + '>');
       parent.append(el);
       $compile(parent)($rootScope);
       $rootScope.$apply();
@@ -72,7 +72,6 @@ describe('mdSidenav', function() {
       expect(backdrop.length).toBe(0);
     }));
 
-
     it('should focus sidenav on open', inject(function($rootScope, $material, $document) {
       jasmine.mockElementFocus(this);
       var el = setup('md-is-open="show"');
@@ -85,11 +84,11 @@ describe('mdSidenav', function() {
     it('should focus child with md-sidenav-focus', inject(function($rootScope, $material, $document, $compile) {
       jasmine.mockElementFocus(this);
       var parent = angular.element('<div>');
-      var markup = '<md-sidenav md-is-open="show">'+
-                      '<md-input-container><label>Label</label>' +
-                        '<input type="text" md-sidenav-focus>' +
-                      '</md-input-container>' +
-                    '<md-sidenav>';
+      var markup = '<md-sidenav md-is-open="show">' +
+        '<md-input-container><label>Label</label>' +
+        '<input type="text" md-sidenav-focus>' +
+        '</md-input-container>' +
+        '<md-sidenav>';
       var sidenavEl = angular.element(markup);
       parent.append(sidenavEl);
       $compile(parent)($rootScope);
@@ -103,11 +102,11 @@ describe('mdSidenav', function() {
     it('should focus child with md-autofocus', inject(function($rootScope, $material, $document, $compile) {
       jasmine.mockElementFocus(this);
       var parent = angular.element('<div>');
-      var markup = '<md-sidenav md-is-open="show">'+
-                      '<md-input-container><label>Label</label>' +
-                        '<input type="text" md-autofocus>' +
-                      '</md-input-container>' +
-                    '<md-sidenav>';
+      var markup = '<md-sidenav md-is-open="show">' +
+        '<md-input-container><label>Label</label>' +
+        '<input type="text" md-autofocus>' +
+        '</md-input-container>' +
+        '<md-sidenav>';
       var sidenavEl = angular.element(markup);
       parent.append(sidenavEl);
       $compile(parent)($rootScope);
@@ -118,16 +117,15 @@ describe('mdSidenav', function() {
       expect($document.activeElement).toBe(focusEl[0]);
     }));
 
-
     it('should focus on last md-sidenav-focus element', inject(function($rootScope, $material, $document, $compile) {
       jasmine.mockElementFocus(this);
       var parent = angular.element('<div>');
-      var markup = '<md-sidenav md-is-open="show">'+
-                      '<md-button md-sidenav-focus>Button</md-button>'+
-                      '<md-input-container><label>Label</label>' +
-                        '<input type="text" md-sidenav-focus>' +
-                      '</md-input-container>' +
-                    '<md-sidenav>';
+      var markup = '<md-sidenav md-is-open="show">' +
+        '<md-button md-sidenav-focus>Button</md-button>' +
+        '<md-input-container><label>Label</label>' +
+        '<input type="text" md-sidenav-focus>' +
+        '</md-input-container>' +
+        '<md-sidenav>';
       var sidenavEl = angular.element(markup);
       parent.append(sidenavEl);
       $compile(parent)($rootScope);
@@ -201,22 +199,27 @@ describe('mdSidenav', function() {
       $material.flushInterimElement();
     }
 
-    beforeEach( inject(function(_$material_,_$rootScope_,_$timeout_) {
-        $material = _$material_;
-        $rootScope = _$rootScope_;
-        $timeout = _$timeout_;
+    beforeEach(inject(function(_$material_, _$rootScope_, _$timeout_) {
+      $material = _$material_;
+      $rootScope = _$rootScope_;
+      $timeout = _$timeout_;
     }));
 
-
-    it('should open(), close(), and toggle() with promises', function () {
+    it('should open(), close(), and toggle() with promises', function() {
       var el = setup('');
       var scope = el.isolateScope();
       var controller = el.controller('mdSidenav');
 
       var openDone = 0, closeDone = 0, toggleDone = 0;
-      var onOpen = function() { openDone++; };
-      var onClose = function() { closeDone++; };
-      var onToggle = function() { toggleDone++; };
+      var onOpen = function() {
+        openDone++;
+      };
+      var onClose = function() {
+        closeDone++;
+      };
+      var onToggle = function() {
+        toggleDone++;
+      };
 
       controller
         .open()
@@ -246,14 +249,17 @@ describe('mdSidenav', function() {
       expect(scope.isOpen).toBe(true);
     });
 
-
-    it('should open() to work multiple times before close()', function () {
+    it('should open() to work multiple times before close()', function() {
       var el = setup('');
       var controller = el.controller('mdSidenav');
 
       var openDone = 0, closeDone = 0;
-      var onOpen = function() { openDone++; };
-      var onClose = function() { closeDone++; };
+      var onOpen = function() {
+        openDone++;
+      };
+      var onClose = function() {
+        closeDone++;
+      };
 
       controller
         .open()
@@ -281,12 +287,10 @@ describe('mdSidenav', function() {
   describe('$mdSidenav Service', function() {
     var $rootScope, $timeout;
 
-
-        beforeEach( inject(function(_$rootScope_,_$timeout_) {
-            $rootScope = _$rootScope_;
-            $timeout = _$timeout_;
-        }));
-
+    beforeEach(inject(function(_$rootScope_, _$timeout_) {
+      $rootScope = _$rootScope_;
+      $timeout = _$timeout_;
+    }));
 
     it('should grab instance', inject(function($mdSidenav) {
       var el = setup('md-component-id="left"');
@@ -337,27 +341,85 @@ describe('mdSidenav', function() {
       expect(instance.isLockedOpen()).toBe(true);
     }));
 
-    it('should find a deferred instantiation', inject(function($mdSidenav) {
-          var instance;
-
-          // Lookup deferred (not existing) instance
-          $mdSidenav('left').then( function(inst) { instance = inst; });
-          expect(instance).toBeUndefined();
-
-          // Instantiate `left` sidenav component
-          var el = setup('md-component-id="left"');
-
-          $timeout.flush();
-          expect(instance).toBeTruthy();
-          expect(instance.isOpen()).toBeFalsy();
-
-          // Lookup instance still available in the component registry
-          instance = undefined;
-          instance = $mdSidenav('left');
-
-          expect(instance).toBeTruthy();
-
-        }));
   });
+
+  it('should find an instantiation using `$mdSidenav(id)`', inject(function($mdSidenav) {
+    var el = setup('md-component-id="left"');
+    $timeout.flush();
+
+    // Lookup instance still available in the component registry
+    var instance = $mdSidenav('left');
+    expect(instance).toBeTruthy();
+  }));
+
+  it('should find a deferred instantiation using `$mdSidenav(id, true)`', inject(function($mdSidenav) {
+    var instance;
+
+    // Lookup deferred (not existing) instance
+    $mdSidenav('left', true).then(function(inst) {
+      instance = inst;
+    });
+    expect(instance).toBeUndefined();
+
+    // Instantiate `left` sidenav component
+    var el = setup('md-component-id="left"');
+    $timeout.flush();
+
+    expect(instance).toBeDefined();
+    expect(instance.isOpen()).toBeFalsy();
+
+    // Lookup instance still available in the component registry
+    instance = $mdSidenav('left', true);
+    expect(instance).toBeTruthy();
+  }));
+
+  it('should find a deferred instantiation using `$mdSidenav().waitFor(id)` ', inject(function($mdSidenav) {
+    var instance;
+
+    // Lookup deferred (not existing) instance
+    $mdSidenav().waitFor('left').then(function(inst) {
+      instance = inst;
+    });
+    expect(instance).toBeUndefined();
+
+    // Instantiate `left` sidenav component
+    var el = setup('md-component-id="left"');
+    $timeout.flush();
+
+    expect(instance).toBeDefined();
+    expect(instance.isOpen()).toBeFalsy();
+
+    // Lookup instance still available in the component registry
+    instance = undefined;
+    instance = $mdSidenav('left');
+
+    expect(instance).toBeTruthy();
+  }));
+
+  it('should not find a lazy instantiation without waiting `$mdSidenav(id)`', inject(function($mdSidenav) {
+    var instance = $mdSidenav('left');
+    expect(instance).toBeUndefined();
+
+    // Instantiate `left` sidenav component
+    var el = setup('md-component-id="left"');
+    $timeout.flush();
+
+    instance = $mdSidenav('left');
+    expect(instance).toBeDefined();
+    expect(instance.isOpen()).toBeFalsy();
+  }));
+
+  it('should not find a lazy instantiation without waiting `$mdSidenav().find(id)`', inject(function($mdSidenav) {
+    var instance = $mdSidenav().find('left');
+    expect(instance).toBeUndefined();
+
+    // Instantiate `left` sidenav component
+    var el = setup('md-component-id="left"');
+    $timeout.flush();
+
+    instance = $mdSidenav().find('left');
+    expect(instance).toBeDefined();
+    expect(instance.isOpen()).toBeFalsy();
+  }));
 
 });
