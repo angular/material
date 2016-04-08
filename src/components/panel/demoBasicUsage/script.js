86e2f0643006615dc0e9b20ba5030d36905f1ec8
Erin Coughlan <ecoughlan@google.com>
2016-04-08 08:51:36 -0700

    fix(panel): Remove async positioning.

--- src/components/panel/demoBasicUsage/script.js
+++ src/components/panel/demoBasicUsage/script.js
@@ -11,8 +11,7 @@ function BasicDemoCtrl($mdPanel) {
 BasicDemoCtrl.prototype.showDialog = function() {
   var position = this._mdPanel.newPanelPosition()
       .absolute()
-      .centerHorizontally()
-      .top();
+      .center();
 
   var config = {
     attachTo: angular.element(document.querySelector('.demo-md-panel')),
