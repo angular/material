cb6940484346272637367204ef2f839d0bca2c91
Thomas Burleson <ThomasBurleson@Gmail.com>
2016-04-11 11:37:42 -0500

    feat(theme): allow global disable of theming generation
    Support config-time disabling of Theming which in turn prevents
    dynamic theme css generation and stylesheet injections.
    
    ```js
    angular
       .module('myApp',['ngMaterial'])
       .config(['$mdThemingProvider', function($mdThemingProvider ){
          $mdThemingProvider.disableTheming();
       }]);
    ```
    
    Refs #943. Closes #7959.

--- src/core/services/theming/theming.js
+++ src/core/services/theming/theming.js
@@ -134,6 +134,7 @@ var generateOnDemand = false;
 
 // Nonce to be added as an attribute to the generated themes style tags.
 var nonce = null;
+var disableTheming = false;
 
 function ThemingProvider($mdColorPalette) {
   PALETTES = { };
@@ -152,6 +153,15 @@ function ThemingProvider($mdColorPalette) {
     definePalette: definePalette,
     extendPalette: extendPalette,
     theme: registerTheme,
+
+    /**
+     * Easy way to disable theming without having to use
+     * `.constant("$MD_THEME_CSS","");` This disables
+     * all dynamic theme style sheet generations and injections...
+     */
+    disableTheming: function() {
+      disableTheming = true;
+    },
 
     setNonce: function(nonceValue) {
       nonce = nonceValue;
@@ -487,7 +497,7 @@ var rulesByType = {};
 function generateAllThemes($injector) {
   var head = document.head;
   var firstChild = head ? head.firstElementChild : null;
-  var themeCss = $injector.has('$MD_THEME_CSS') ? $injector.get('$MD_THEME_CSS') : '';
+  var themeCss = !disableTheming && $injector.has('$MD_THEME_CSS') ? $injector.get('$MD_THEME_CSS') : '';
 
   if ( !firstChild ) return;
   if (themeCss.length === 0) return; // no rules, so no point in running this expensive task
