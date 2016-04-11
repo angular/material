24c4788d952b285ed2e8407c217df7bc0bef8394
Erin Coughlan <ecoughlan@google.com>
2016-04-11 15:56:03 -0700

    update(panel): Add a demo for a menu using the panel.

--- src/components/panel/demoBasicUsage/script.js
+++ src/components/panel/demoBasicUsage/script.js
@@ -5,6 +5,17 @@ angular.module('panelDemo', ['ngMaterial'])
 
 function BasicDemoCtrl($mdPanel) {
   this._mdPanel = $mdPanel;
+
+  this.desserts = [
+    'Apple Pie',
+    'Donut',
+    'Fudge',
+    'Cupcake',
+    'Ice Cream',
+    'Tiramisu'
+  ];
+
+  this.selected = {favoriteDessert: 'Donut'};
 }
 
 
@@ -14,26 +25,139 @@ BasicDemoCtrl.prototype.showDialog = function() {
       .center();
 
   var config = {
-    attachTo: angular.element(document.querySelector('.demo-md-panel')),
+    attachTo: '.demo-md-panel',
     controller: PanelDialogCtrl,
     controllerAs: 'ctrl',
     templateUrl: 'panel.tmpl.html',
     panelClass: 'demo-dialog-example',
     position: position,
     trapFocus: true,
-    zIndex: 150
+    zIndex: 150,
+    clickOutsideToClose: true,
+    escapeToClose: true,
+    focusOnOpen: true
   };
 
-  this._panelRef = this._mdPanel.open(config);
+  this._mdPanel.open(config);
 };
 
 
-// Necessary to pass locals to the dialog template.
+BasicDemoCtrl.prototype.showMenu = function(ev) {
+  var position = this._mdPanel.newPanelPosition()
+      .relativeTo('.demo-menu-open-button')
+      .withPanelXPosition(this._mdPanel.xPosition.ALIGN_START)
+      .withPanelYPosition(this._mdPanel.yPosition.BELOW);
+
+  var config = {
+    attachTo: '.demo-md-panel',
+    controller: PanelMenuCtrl,
+    controllerAs: 'ctrl',
+    template:
+        '<div class="demo-menu-example" ' +
+        '     aria-label="Select your favorite dessert." ' +
+        '     role="listbox">' +
+        '  <div class="demo-menu-item" ' +
+        '       ng-class="{selected : dessert == ctrl.favoriteDessert}" ' +
+        '       aria-selected="{{dessert == ctrl.favoriteDessert}}" ' +
+        '       tabindex="-1" ' +
+        '       role="option" ' +
+        '       ng-repeat="dessert in ctrl.desserts" ' +
+        '       ng-click="ctrl.selectDessert(dessert)"' +
+        '       ng-keydown="ctrl.onKeydown($event, dessert)">' +
+        '    {{ dessert }} ' +
+        '  </div>' +
+        '</div>',
+    panelClass: 'demo-menu-example',
+    position: position,
+    locals: {
+      'selected': this.selected,
+      'desserts': this.desserts
+    },
+    openFrom: ev,
+    clickOutsideToClose: true,
+    escapeToClose: true,
+    focusOnOpen: false
+  };
+
+  this._mdPanel.open(config);
+};
+
+
 function PanelDialogCtrl(mdPanelRef) {
   this._mdPanelRef = mdPanelRef;
 }
 
 
 PanelDialogCtrl.prototype.closeDialog = function() {
-  this._mdPanelRef && this._mdPanelRef.close();
+  this._mdPanelRef && this._mdPanelRef.close().then(function() {
+    angular.element(document.querySelector('.demo-dialog-open-button')).focus();
+  });
+};
+
+
+
+function PanelMenuCtrl(mdPanelRef, $timeout) {
+  this._mdPanelRef = mdPanelRef;
+  this.favoriteDessert = this.selected.favoriteDessert;
+  $timeout(function() {
+    var selected = document.querySelector('.demo-menu-item.selected');
+    if (selected) {
+      angular.element(selected).focus();
+    } else {
+      angular.element(document.querySelectorAll('.demo-menu-item')[0]).focus();
+    }
+  });
+}
+
+
+PanelMenuCtrl.prototype.selectDessert = function(dessert) {
+  this.selected.favoriteDessert = dessert;
+  this._mdPanelRef && this._mdPanelRef.close().then(function() {
+    angular.element(document.querySelector('.demo-menu-open-button')).focus();
+  });
+};
+
+
+PanelMenuCtrl.prototype.onKeydown = function($event, dessert) {
+  var handled;
+  switch ($event.which) {
+    case 38: // Up Arrow.
+      var els = document.querySelectorAll('.demo-menu-item');
+      var index = indexOf(els, document.activeElement);
+      var prevIndex = (index + els.length - 1) % els.length;
+      els[prevIndex].focus();
+      handled = true;
+      break;
+
+    case 40: // Down Arrow.
+      var els = document.querySelectorAll('.demo-menu-item');
+      var index = indexOf(els, document.activeElement);
+      var nextIndex = (index + 1) % els.length;
+      els[nextIndex].focus();
+      handled = true;
+      break;
+
+    case 13: // Enter.
+    case 32: // Space.
+      this.selectDessert(dessert);
+      handled = true;
+      break;
+
+    case 9: // Tab.
+      this._mdPanelRef && this._mdPanelRef.close();
+  }
+
+  if (handled) {
+    $event.preventDefault();
+    $event.stopImmediatePropagation();
+  }
+
+  function indexOf(nodeList, element) {
+    for (var item, i = 0; item = nodeList[i]; i++) {
+      if (item === element) {
+        return i;
+      }
+    }
+    return -1;
+  }
 };
