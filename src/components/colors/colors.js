579ec5327d00d6ed7aaa1d64e6acafc47fe68984
Robert Messerle <rmesserle@google.com>
2016-04-14 14:44:04 -0700

    fix(colors): coverts COLOR_PALETTES to colorPalettes
    Value is reassigned, so it is not a constant.

--- src/components/colors/colors.js
+++ src/components/colors/colors.js
@@ -6,7 +6,7 @@
    *  or one that should be observed and dynamically interpolated.
    */
   var STATIC_COLOR_EXPRESSION = /^{((\s|,)*?["'a-zA-Z-]+?\s*?:\s*?('|")[a-zA-Z0-9-.]*('|"))+\s*}$/;
-  var COLOR_PALETTES = undefined;
+  var colorPalettes = undefined;
 
   /**
    * @ngdoc module
@@ -41,7 +41,7 @@
    *
    */
   function MdColorsService($mdTheming, $mdColorPalette, $mdUtil, $parse) {
-    COLOR_PALETTES = COLOR_PALETTES || Object.keys($mdColorPalette);
+    colorPalettes = colorPalettes || Object.keys($mdColorPalette);
 
     // Publish service instance
     return {
@@ -144,12 +144,12 @@
       // If the next section is one of the palettes we assume it's a two word palette
       // Two word palette can be also written in camelCase, forming camelCase to dash-case
 
-      var isTwoWord = parts.length > 1 && COLOR_PALETTES.indexOf(parts[1]) !== -1;
+      var isTwoWord = parts.length > 1 && colorPalettes.indexOf(parts[1]) !== -1;
       var palette = parts[0].replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 
       if (isTwoWord)  palette = parts[0] + '-' + parts.splice(1, 1);
 
-      if (COLOR_PALETTES.indexOf(palette) === -1) {
+      if (colorPalettes.indexOf(palette) === -1) {
         // If the palette is not in the palette list it's one of primary/accent/warn/background
         var scheme = $mdTheming.THEMES[theme].colors[palette];
         if (!scheme) {
