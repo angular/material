b99e74d9749e1a25e0da65917e1022dfd0ee9b82
neko1235 <neko1235@users.noreply.github.com>
2016-04-14 19:03:33 -0700

    fix(virtualRepeat): Memory leak
    Delete all created elements when the scope is destroyed.
    Move cleanup code to cleanupBlocks().
    Do not clean up used blocks.
    Add unit tests.
    
    Closes #8055. Closes #8056.

--- src/components/virtualRepeat/virtual-repeater.js
+++ src/components/virtualRepeat/virtual-repeater.js
@@ -501,6 +501,8 @@ function VirtualRepeatController($scope, $element, $attrs, $browser, $document, 
   this.blocks = {};
   /** @type {Array<!VirtualRepeatController.Block>} A pool of presently unused blocks. */
   this.pooledBlocks = [];
+
+  $scope.$on('$destroy', angular.bind(this, this.cleanupBlocks_));
 }
 
 
@@ -533,6 +535,14 @@ VirtualRepeatController.prototype.link_ =
   this.repeatListExpression = angular.bind(this, this.repeatListExpression_);
 
   this.container.register(this);
+};
+
+
+/** @private Cleans up unused blocks. */
+VirtualRepeatController.prototype.cleanupBlocks_ = function() {
+  angular.forEach(this.pooledBlocks, function cleanupBlock(block) {
+    block.element.remove();
+  });
 };
 
 
