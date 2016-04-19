9329e36aae852354c5c3ce86cb804ccc81925ecf
Thomas Burleson <ThomasBurleson@Gmail.com>
2016-04-19 10:08:21 -0500

    update(docs): use Angular 1.5.5 for docs

--- config/build.config.js
+++ config/build.config.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var versionFile = __dirname + '/../dist/commit';
 
 module.exports = {
-  ngVersion: '1.4.8',
+  ngVersion: '1.5.5',
   version: pkg.version,
   repository: pkg.repository.url
     .replace(/^git/,'https')
