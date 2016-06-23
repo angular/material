806f949cd69eff116abe25804a0f9abe84480c4f
Topher Fangio <fangiotophia@gmail.com>
2016-06-23 17:15:00 -0500

    fix(site): Fix navigation toggle to properly open on page refresh.
    Currently, the navigation groups on the left would have their icon
    appear to be toggled open, but the actual content would be invisible
    because the element's height was not being properly set.
    
    Fix by setting the height in a `$mdUtil.nextTick()` so that it fires
    at the appropriate time (and can measure the height).
    
    Additionally, add some code to scroll the selected item into view so
    that users know which page they are visiting.
    
    Fixes #8841.

--- docs/app/js/app.js
+++ docs/app/js/app.js
@@ -502,22 +502,44 @@ function(SERVICES, COMPONENTS, DEMOS, PAGES, $location, $rootScope, $http, $wind
             return controller.isOpen($scope.section);
           },
           function (open) {
-            var $ul = $element.find('ul');
+            // We must run this in a next tick so that the getTargetHeight function is correct
+            $mdUtil.nextTick(function() {
+              var $ul = $element.find('ul');
+              var $li = $ul[0].querySelector('a.active');
+              var docsMenuContent = document.querySelector('.docs-menu').parentNode;
+              var targetHeight = open ? getTargetHeight() : 0;
 
-            var targetHeight = open ? getTargetHeight() : 0;
-            $timeout(function () {
-              $ul.css({height: targetHeight + 'px'});
-            }, 0, false);
+              $timeout(function () {
+                // Set the height of the list
+                $ul.css({height: targetHeight + 'px'});
 
-            function getTargetHeight() {
-              var targetHeight;
-              $ul.addClass('no-transition');
-              $ul.css('height', '');
-              targetHeight = $ul.prop('clientHeight');
-              $ul.css('height', 0);
-              $ul.removeClass('no-transition');
-              return targetHeight;
-            }
+                // If we are open and the user has not scrolled the content div; scroll the active
+                // list item into view.
+                if (open && $li && $ul[0].scrollTop === 0) {
+                  $timeout(function() {
+                    var activeHeight = $li.scrollHeight;
+                    var activeOffset = $li.offsetTop;
+                    var parentOffset = $li.offsetParent.offsetTop;
+
+                    // Reduce it a bit (2 list items' height worth) so it doesn't touch the nav
+                    var negativeOffset = activeHeight * 2;
+                    var newScrollTop = activeOffset + parentOffset - negativeOffset;
+
+                    $mdUtil.animateScrollTo(docsMenuContent, newScrollTop);
+                  }, 350, false);
+                }
+              }, 0, false);
+
+              function getTargetHeight() {
+                var targetHeight;
+                $ul.addClass('no-transition');
+                $ul.css('height', '');
+                targetHeight = $ul.prop('clientHeight');
+                $ul.css('height', 0);
+                $ul.removeClass('no-transition');
+                return targetHeight;
+              }
+            }, false);
           }
         );
       });
