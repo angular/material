890c359f5ec47d7b23071276e06db257f10cf175
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-06-26 13:25:48 +0200

    docs(sidenav): add demo for custom sidenav
    * Fixes the ugly scrollbars from the demo (through layout-fill).
      Horizontal Scrollbar now also appeared on Chrome 51.
    
    * Added a second demo for a sidenav, with custom settings (`md-disable-backdrop`)
      Having the disabled backdrop in the first demo, looks weird and seems like a bug.
    
    Closes #8858
    
    Closes #8871

---
+++ src/components/sidenav/demoCustomSidenav/script.js
@@ -0,0 +1,12 @@
+angular
+  .module('sidenavDemo2', ['ngMaterial'])
+  .controller('AppCtrl', function ($scope, $timeout, $mdSidenav) {
+    $scope.toggleLeft = buildToggler('left');
+    $scope.toggleRight = buildToggler('right');
+
+    function buildToggler(componentId) {
+      return function() {
+        $mdSidenav(componentId).toggle();
+      }
+    }
+  });
