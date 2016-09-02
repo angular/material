9012bec77e6298aa55af7fd628bc9bc625f4e361
Frederic Morin <fred.morin@gmail.com>
2016-09-02 14:08:25 -0400

    Update to demo with icons readme Removed text tab from demo with icons Removed custom theme from demo

--- src/components/tabs/demoStaticTabsWithIcons/script.js
+++ src/components/tabs/demoStaticTabsWithIcons/script.js
@@ -3,52 +3,7 @@
 
   angular
       .module('tabsDemoStaticTabs', ['ngMaterial'] )
-      .controller('AppCtrl', AppCtrl)
-	  .config(function($mdThemingProvider){
-
-		$mdThemingProvider.definePalette('canc4isrPrimaryPalette',{
-			'50': 'ffffff',
-			'100': 'ffffff',
-			'200': 'ffffff',
-			'300': 'ffffff',
-			'400': 'ffffff',
-			'500': '40484c',
-			'600': '40c4ff',
-			'700': 'ffffff',
-			'800': 'ffffff',
-			'900': 'ffffff',
-			'A100': '40c4ff',
-			'A200': '40c4ff',
-			'A400': '40c4ff',
-			'A700': '40c4ff',		
-			'contrastDefaultColor': 'light',
-			'contrastDarkColors': ['50', '100', '200', '300', '400', 'A100'],
-			'contrastLightColors': ['50', '100', '200', '300', '400', 'A100'],			
-		});
-		$mdThemingProvider.definePalette('canc4isrAccentPalette',{
-			'50': 'ffffff',
-			'100': 'ffffff',
-			'200': 'ffffff',
-			'300': 'ffffff',
-			'400': 'ffffff',
-			'500': '40484c',
-			'600': 'ffffff',
-			'700': 'ffffff',
-			'800': 'ffffff',
-			'900': 'ffffff',
-			'A100': 'ffffff',
-			'A200': '40c4ff',
-			'A400': 'ffffff',
-			'A700': 'ffffff',	
-			'contrastDefaultColor': 'light',
-			'contrastDarkColors': ['50', '100', '200', '300', '400', 'A100'],
-			'contrastLightColors': ['50', '100', '200', '300', '400', 'A100'],
-		});	  
-	  
-		$mdThemingProvider.theme('default')
-			  .primaryPalette('canc4isrPrimaryPalette')
-			  .accentPalette('canc4isrAccentPalette');
-	  });;
+      .controller('AppCtrl', AppCtrl);
 
   function AppCtrl ( $scope ) {
     $scope.data = {
