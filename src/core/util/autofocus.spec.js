9d434e3e9dd21c7b3ea1d19d20485e9d1fc9b8f4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-09-23 19:35:59 +0200

    fix(autofocus) properly parse attribute expression (#9636)
    The autofocus directive right now works with an empty value because jqLite just toggles the class if the `force` param is `undefined`.
    
    This means if you change it several times to the same undefined value it does incorrectly update the autofocus class (which is required for the focus target)
    
    Also the autofocus directive does now determine the class immediately and does not require a second digest (previously this could cause issues with the $mdCompiler linking).

--- src/core/util/autofocus.spec.js
+++ src/core/util/autofocus.spec.js
@@ -7,6 +7,7 @@ describe('md-autofocus', function() {
   }));
 
   describe('add/removes the proper classes', function() {
+
     it('supports true', function() {
       build('<input id="test" type="text" md-autofocus="true">');
 
@@ -31,6 +32,31 @@ describe('md-autofocus', function() {
 
       // Set the expression to true
       pageScope.$apply('shouldAutoFocus=true');
+      expect(element).toHaveClass('md-autofocus');
+    });
+
+    it('should properly set the class at initialization', inject(function($compile, $rootScope) {
+      pageScope = $rootScope.$new();
+      element = $compile('<input md-autofocus>')(pageScope);
+
+      expect(element).toHaveClass('md-autofocus');
+    }));
+
+    it('does not accidentally toggle the class', function() {
+      build('<input md-autofocus="autofocus">');
+
+      // By default, we assume an empty value for the expression is true
+      expect(element).toHaveClass('md-autofocus');
+
+      // Trigger a second digest, to be able to set the scope binding to undefined later again.
+      pageScope.$apply('autofocus = true');
+
+      expect(element).toHaveClass('md-autofocus');
+
+      // Set the scope binding to undefined again, which can accidentally toggle the class due to
+      // the jqLite toggleClass function, which just toggles the class if the value is undefined.
+      pageScope.$apply('autofocus = undefined');
+
       expect(element).toHaveClass('md-autofocus');
     });
 
