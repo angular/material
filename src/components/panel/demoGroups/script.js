7809bf2a2e5b4f7b44dbbcd8076df81dd16c2589
Brad Richardson <brad@designchanged.us>
2016-10-13 12:53:40 -0400

    feat(panel): Allow panels to be part of multiple groups.
    The groupName parameter has been changed from allowing only a
    string to allowing a string or an array of strings. All of the
    necessary functionality has been changed to accompany the array.
    
    Fixes #9565

--- src/components/panel/demoGroups/script.js
+++ src/components/panel/demoGroups/script.js
@@ -28,24 +28,48 @@
         'Sign Out'
       ]
     };
+    this.tools = {
+      name: 'tools',
+      items: [
+        'Create',
+        'Delete'
+      ]
+    };
+    this.code = {
+      name: 'code',
+      items: [
+        'See Source',
+        'See Commits'
+      ]
+    };
+
+    this.menuTemplate = '' +
+        '<div class="menu-panel" md-whiteframe="4">' +
+        '  <div class="menu-content">' +
+        '    <div class="menu-item" ng-repeat="item in ctrl.items">' +
+        '      <button class="md-button">' +
+        '        <span>{{item}}</span>' +
+        '      </button>' +
+        '    </div>' +
+        '    <md-divider></md-divider>' +
+        '    <div class="menu-item">' +
+        '      <button class="md-button" ng-click="ctrl.closeMenu()">' +
+        '        <span>Close Menu</span>' +
+        '      </button>' +
+        '    </div>' +
+        '  </div>' +
+        '</div>';
+
+    $mdPanel.newPanelGroup('toolbar', {
+      maxOpen: 2
+    });
+
+    $mdPanel.newPanelGroup('menus', {
+      maxOpen: 3
+    });
 
     this.showToolbarMenu = function($event, menu) {
-      var template = '' +
-          '<div class="menu-panel" md-whiteframe="4">' +
-          '  <div class="menu-content">' +
-          '    <div class="menu-item" ng-repeat="item in ctrl.items">' +
-          '      <button class="md-button">' +
-          '        <span>{{item}}</span>' +
-          '      </button>' +
-          '    </div>' +
-          '    <md-divider></md-divider>' +
-          '    <div class="menu-item">' +
-          '      <button class="md-button" ng-click="ctrl.closeMenu()">' +
-          '        <span>Close Menu</span>' +
-          '      </button>' +
-          '    </div>' +
-          '  </div>' +
-          '</div>';
+      var template = this.menuTemplate;
 
       var position = $mdPanel.newPanelPosition()
           .relativeTo($event.srcElement)
@@ -53,10 +77,6 @@
             $mdPanel.xPosition.ALIGN_START,
             $mdPanel.yPosition.BELOW
           );
-
-      $mdPanel.newPanelGroup('toolbar', {
-        maxOpen: 2
-      });
 
       var config = {
         id: 'toolbar_' + menu.name,
@@ -73,11 +93,42 @@
         focusOnOpen: false,
         zIndex: 100,
         propagateContainerEvents: true,
-        groupName: 'toolbar'
+        groupName: ['toolbar', 'menus']
       };
 
       $mdPanel.open(config);
-    }
+    };
+
+    this.showContentMenu = function($event, menu) {
+      var template = this.menuTemplate;
+
+      var position = $mdPanel.newPanelPosition()
+          .relativeTo($event.srcElement)
+          .addPanelPosition(
+            $mdPanel.xPosition.ALIGN_START,
+            $mdPanel.yPosition.BELOW
+          );
+
+      var config = {
+        id: 'content_' + menu.name,
+        attachTo: angular.element(document.body),
+        controller: PanelMenuCtrl,
+        controllerAs: 'ctrl',
+        template: template,
+        position: position,
+        panelClass: 'menu-panel-container',
+        locals: {
+          items: menu.items
+        },
+        openFrom: $event,
+        focusOnOpen: false,
+        zIndex: 100,
+        propagateContainerEvents: true,
+        groupName: 'menus'
+      };
+
+      $mdPanel.open(config);
+    };
   }
 
   function PanelMenuCtrl(mdPanelRef) {
