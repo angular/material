2c286b41d6a4d85f2be88986d1331f84241a9b3a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-10-16 03:54:25 +0200

    update(dialog): remove .md-actions deprecation (#9637)
    * Removes the `.md-actions` deprecation from pre- v1.0.0
    * Simplifies the method to search for a close button
    
    Closes #9633

--- src/components/dialog/dialog.js
+++ src/components/dialog/dialog.js
@@ -725,18 +725,8 @@ function MdDialogProvider($$interimElementProvider) {
       return dialogPopIn(element, options)
         .then(function() {
           lockScreenReader(element, options);
-          warnDeprecatedActions();
           focusOnOpen();
         });
-
-      /**
-       * Check to see if they used the deprecated .md-actions class and log a warning
-       */
-      function warnDeprecatedActions() {
-        if (element[0].querySelector('.md-actions')) {
-          $log.warn('Using a class of md-actions is deprecated, please use <md-dialog-actions>.');
-        }
-      }
 
       /**
        * For alerts, focus on content... otherwise focus on
@@ -755,14 +745,7 @@ function MdDialogProvider($$interimElementProvider) {
          * If we find no actions at all, log a warning to the console.
          */
         function findCloseButton() {
-          var closeButton = element[0].querySelector('.dialog-close');
-
-          if (!closeButton) {
-            var actionButtons = element[0].querySelectorAll('.md-actions button, md-dialog-actions button');
-            closeButton = actionButtons[actionButtons.length - 1];
-          }
-
-          return closeButton;
+          return element[0].querySelector('.dialog-close, md-dialog-actions button:last-child');
         }
       }
     }
