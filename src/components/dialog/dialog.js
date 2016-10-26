b8889b68e710bf28ef6d2adebef12d1f7eb40ac3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-10-26 17:49:54 +0200

    fix(dialog): only restore focus with keyboard interaction
    * Currently the dialog always restores focus to the origin element upon close.
    
    * The focus should be only restored if the dialog was opened with keyboard interaction - Same behavior as with the sidenav closing.
    
    Fixes #7963.

--- src/components/dialog/dialog.js
+++ src/components/dialog/dialog.js
@@ -638,7 +638,7 @@ function MdDialogProvider($$interimElementProvider) {
 
   /* @ngInject */
   function dialogDefaultOptions($mdDialog, $mdAria, $mdUtil, $mdConstant, $animate, $document, $window, $rootElement,
-                                $log, $injector, $mdTheming, $interpolate) {
+                                $log, $injector, $mdTheming, $interpolate, $mdInteraction) {
 
     return {
       hasBackdrop: true,
@@ -798,7 +798,10 @@ function MdDialogProvider($$interimElementProvider) {
         // Exposed cleanup function from the $mdCompiler.
         options.cleanupElement();
 
-        if (!options.$destroy) options.origin.focus();
+        // Restores the focus to the origin element if the last interaction upon opening was a keyboard.
+        if (!options.$destroy && options.originInteraction === 'keyboard') {
+          options.origin.focus();
+        }
       }
     }
 
@@ -850,7 +853,8 @@ function MdDialogProvider($$interimElementProvider) {
           options.openFrom = getBoundingClientRect(getDomElement(options.openFrom));
 
           if ( options.targetEvent ) {
-            options.origin   = getBoundingClientRect(options.targetEvent.target, options.origin);
+            options.origin = getBoundingClientRect(options.targetEvent.target, options.origin);
+            options.originInteraction = $mdInteraction.getLastInteractionType();
           }
 
 
