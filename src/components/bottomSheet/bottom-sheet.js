f6649304e57daa5d070e74ecb6cdea9cc68d3d5c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-11-07 20:34:28 +0100

    docs(bottom-sheet): document bindToController option (#9954)
    * Adds missing documentation for the `bindToController` option inside of the bottomSheet directive / service.
    
    Closes #8811

--- src/components/bottomSheet/bottom-sheet.js
+++ src/components/bottomSheet/bottom-sheet.js
@@ -87,6 +87,7 @@ function MdBottomSheetDirective($mdBottomSheet) {
  *   of 3.
  *   - `clickOutsideToClose` - `{boolean=}`: Whether the user can click outside the bottom sheet to
  *     close it. Default true.
+ *   - `bindToController` - `{boolean=}`: When set to true, the locals will be bound to the controller instance.
  *   - `disableBackdrop` - `{boolean=}`: When set to true, the bottomsheet will not show a backdrop.
  *   - `escapeToClose` - `{boolean=}`: Whether the user can press escape to close the bottom sheet.
  *     Default true.
