2c367f7a219598c09ad8fecc8f66c29acc93e70a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-11-15 23:51:02 +0100

    fix(list): expect aria-label with respect to aria-hidden (#9943)
    * The list item no longer resolves the aria-label text from the content incorrectly (aria-hidden is ignored - b3cb84d3fa9408fb3f7192020eaef0d2c82bd929 fixed this for $mdAria)
    * Now shows ARIA warnings properly for list-items being clickable
    * Fixes incorrect tests with uncompiled buttons due to missing `buttons` module registration.
    
    Fixes #9933.

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -330,7 +330,8 @@ function mdListItemDirective($mdAria, $mdConstant, $mdUtil, $timeout) {
             '<md-button class="md-no-style"></md-button>'
           );
 
-          buttonWrap[0].setAttribute('aria-label', tEl[0].textContent);
+          // Expect the root element to have a label set. If not set, determine the label from the text content.
+          $mdAria.expectWithText(tEl, 'aria-label');
 
           copyAttributes(tEl[0], buttonWrap[0]);
 
