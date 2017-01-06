e8d29db61f7dc9ebe476406f9e3321e792eb3418
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-01-06 13:58:37 +0100

    chore(travis): run tests for angular 1.6 on travis (#10228)
    * chore: run tests for angular 1.6 on travis
    
    * Now runs tests for Angular v1.6.1 on the Travis CI in an optional mode.
    
    Closes #10205
    
    * Install Angular version before building
    
    * Updates for the Saucelabs connector

--- scripts/travis-run-script.sh
+++ scripts/travis-run-script.sh
@@ -3,6 +3,12 @@
 # Terminate the execution if anything fails in the pipeline.
 set -e
 
+# When Travis CI specifies an Angular version, try to install those for tests.
+if [[ -n "$NG_VERSION" ]]; then
+  npm i angular@$NG_VERSION \
+        angular-{animate,aria,messages,mocks,route,sanitize,touch}@$NG_VERSION
+fi
+
 # Run our check to make sure all tests will actually run
 gulp ddescribe-iit
 gulp build
