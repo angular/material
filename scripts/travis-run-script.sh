322dbc8b4540db1d6a817164136d60c856bd2504
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-01-06 13:28:59 +0100

    Install Angular version before building

--- scripts/travis-run-script.sh
+++ scripts/travis-run-script.sh
@@ -3,6 +3,12 @@
 # Terminate the execution if anything fails in the pipeline.
 set -e
 
+# When Travis CI specifies an Angular version, try to install those for tests.
+if [[ -n "$NG_VERSION" ]]; then
+  npm i angular@$NG_VERSION \
+        angular-{animate,aria,messages,mocks,route,sanitize,touch}@$NG_VERSION
+fi
+
 # Run our check to make sure all tests will actually run
 gulp ddescribe-iit
 gulp build
@@ -12,12 +18,6 @@ gulp build
 
 # Wait for the tunnel to be ready
 ./scripts/sauce/wait-tunnel.sh
-
-# When Travis CI specifies an Angular version, try to install those for tests.
-if [[ -n "$NG_VERSION" ]]; then
-  npm i angular@$NG_VERSION \
-        angular-{animate,aria,messages,mocks,route,sanitize,touch}@$NG_VERSION
-fi
 
 gulp karma --config=config/karma-sauce.conf.js --browsers=$BROWSER --reporters='dots'
 
