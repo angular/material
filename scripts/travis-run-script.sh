7c30c10f0cb5d64d4b65ea0ac40cb0fb53f80557
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-01-06 13:23:34 +0100

    chore: run tests for angular 1.6 on travis
    * Now runs tests for Angular v1.6.1 on the Travis CI in an optional mode.
    
    Closes #10205

--- scripts/travis-run-script.sh
+++ scripts/travis-run-script.sh
@@ -13,6 +13,12 @@ gulp build
 # Wait for the tunnel to be ready
 ./scripts/sauce/wait-tunnel.sh
 
+# When Travis CI specifies an Angular version, try to install those for tests.
+if [[ -n "$NG_VERSION" ]]; then
+  npm i angular@$NG_VERSION \
+        angular-{animate,aria,messages,mocks,route,sanitize,touch}@$NG_VERSION
+fi
+
 gulp karma --config=config/karma-sauce.conf.js --browsers=$BROWSER --reporters='dots'
 
 # Shutdown the tunnel
