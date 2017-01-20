angular/material

commit 54a1d0d0575e94faecdc8a469b794843e8279f0b

fix(select): unable to switch between falsy options (#9945)
Fixes #9533.

src/components/select/select.js
@@ -889,7 +889,7 @@ function SelectMenuDirective($parse, $mdUtil, $mdConstant, $mdTheming) {
       var newVal = self.isMultiple ? values : values[0];
       var prevVal = self.ngModel.$modelValue;
 
-      if (usingTrackBy ? !angular.equals(prevVal, newVal) : prevVal != newVal) {
+      if (usingTrackBy ? !angular.equals(prevVal, newVal) : prevVal !== newVal) {
         self.ngModel.$setViewValue(newVal);
         self.ngModel.$render();
       }

src/components/select/select.spec.js
@@ -798,6 +798,15 @@ describe('<md-select>', function() {
         expect($rootScope.model).toBe(4);
       });
 
+      it('should allow switching between falsy options', inject(function($rootScope) {
+        $rootScope.model = false;
+        var el = setupSelect('ng-model="$root.model"', [false, 0]);
+
+        openSelect(el);
+        clickOption(el, 1);
+
+        expect($rootScope.model).toBe(0);
+      }));
     });
   });
 

commit 2b41ab256672fcb9e2989835202fc29523936a5f

fix(select): don't override initial model value
Fixes an issue, introduced in #9945, that caused the select to override the initial model value.

src/components/select/select.js
@@ -889,7 +889,7 @@ function SelectMenuDirective($parse, $mdUtil, $mdConstant, $mdTheming) {
       var newVal = self.isMultiple ? values : values[0];
       var prevVal = self.ngModel.$modelValue;
 
-      if (usingTrackBy ? !angular.equals(prevVal, newVal) : prevVal !== newVal) {
+      if (usingTrackBy ? !angular.equals(prevVal, newVal) : (prevVal + '') !== newVal) {
         self.ngModel.$setViewValue(newVal);
         self.ngModel.$render();
       }

src/components/select/select.spec.js
@@ -807,6 +807,18 @@ describe('<md-select>', function() {
 
         expect($rootScope.model).toBe(0);
       }));
+
+      it('should not override the initial model value', inject(function($rootScope) {
+        $rootScope.model = 2;
+
+        var el = setupSelect('ng-model="$root.model"', ['1', '2', '3']);
+        var selectedOption = selectedOptions(el)[0];
+
+        expect($rootScope.model).toBe(2, 'Expected value not to have been overwritten.');
+        expect(selectedOption).toBeTruthy('Expected an option to be selected.');
+        expect(selectedOption.getAttribute('value')).toBe('2',
+            'Expected the corresponding option to have been selected');
+      }));
     });
   });
 