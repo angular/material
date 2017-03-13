9f198c93af2e883d25bbce9fadf2a3d4ac5f4283
Jakub Kultys <7kuba7@gmail.com>
2017-03-13 20:53:41 +0100

    fix(autocomplete): incorrect evaluation of available space in viewport (#9999)
    The available space beneath a wrap included space above viewport. When heights of the body
    and viewport did not match (e.g. on mobile) dropdown was displayed on the wrong side.
    This change make it so available space is calculated properly.

--- src/components/autocomplete/js/autocompleteController.js
+++ src/components/autocomplete/js/autocompleteController.js
@@ -125,7 +125,7 @@ function MdAutocompleteCtrl ($scope, $element, $mdUtil, $mdConstant, $mdTheming,
 
     // Automatically determine dropdown placement based on available space in viewport.
     if (!position) {
-      position = (top > bot && root.height - hrect.bottom - MENU_PADDING < dropdownHeight) ? 'top' : 'bottom';
+      position = (top > bot && root.height - top - MENU_PADDING < dropdownHeight) ? 'top' : 'bottom';
     }
     // Adjust the width to account for the padding provided by `md-input-container`
     if ($attrs.mdFloatingLabel) {
