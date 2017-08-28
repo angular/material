2e76e108a7ae106043786b1a9bcefa3c874982a8
Elad Bezalel <elad.bezalel@gmail.com>
2017-08-29 01:27:40 +0300

    fix(compiler): removed `@private` to fix dgeni failures

--- src/core/services/compiler/compiler.js
+++ src/core/services/compiler/compiler.js
@@ -93,14 +93,11 @@ function MdCompilerProvider($compileProvider) {
   var provider = this;
 
   /**
-   * @ngdoc method
    * @name  $mdCompilerProvider#respectPreAssignBindingsEnabled
    *
    * @param {boolean=} respected update the respectPreAssignBindingsEnabled state if provided, otherwise just return
    * the current Material preAssignBindingsEnabled state
    * @returns {*} current value if used as getter or itself (chaining) if used as setter
-   *
-   * @kind function
    *
    * @description
    * Call this method to enable/disable whether Material-specific (dialogs/toasts) controllers respect the AngularJS
@@ -142,14 +139,6 @@ function MdCompilerProvider($compileProvider) {
   };
 
   /**
-   * @ngdoc function
-   * @name  getPreAssignBindingsEnabled
-   * @private
-   *
-   * @returns {*} current preAssignBindingsEnabled state
-   *
-   * @kind function
-   *
    * @description
    * This function returns `true` if Material-specific (dialogs/toasts) controllers have bindings pre-assigned in
    * controller constructors and `false` otherwise.
@@ -157,6 +146,8 @@ function MdCompilerProvider($compileProvider) {
    * Note that this doesn't affect directives/components created via regular AngularJS methods which constitute most
    * Material & user-created components; their behavior can be checked via `$compileProvider.preAssignBindingsEnabled()`
    * in AngularJS `>=1.5.10 <1.7.0`.
+   *
+   * @returns {*} current preAssignBindingsEnabled state
    */
   function getPreAssignBindingsEnabled() {
     if (!respectPreAssignBindingsEnabled) {
@@ -260,7 +251,6 @@ function MdCompilerProvider($compileProvider) {
    * Instead of compiling any template, the compiler just fetches an existing HTML element from the DOM and
    * provides a restore function to put the element back it old DOM position.
    * @param {!Object} options Options to be used for the compiler.
-   * @private
    */
   MdCompilerService.prototype._prepareContentElement = function(options) {
 
@@ -281,7 +271,6 @@ function MdCompilerProvider($compileProvider) {
    * Compiles a template by considering all options and waiting for all resolves to be ready.
    * @param {!Object} options Compile options
    * @returns {!Object} Compile data with link function.
-   * @private
    */
   MdCompilerService.prototype._compileTemplate = function(options) {
 
@@ -331,7 +320,6 @@ function MdCompilerProvider($compileProvider) {
    * @param {!JQLite} element Element to be compiled and linked
    * @param {!Object} options Options to be used for linking.
    * @returns {!Object} Compile data with link function.
-   * @private
    */
   MdCompilerService.prototype._compileElement = function(locals, element, options) {
     var self = this;
@@ -379,7 +367,6 @@ function MdCompilerProvider($compileProvider) {
    * @param {!Object} injectLocals Locals to to be provided in the controller DI.
    * @param {!Object} locals Locals to be injected to the controller.
    * @returns {!Object} Created controller instance.
-   * @private
    */
   MdCompilerService.prototype._createController = function(options, injectLocals, locals) {
     var invokeCtrl = this.$controller(options.controller, injectLocals, true, options.controllerAs);
@@ -406,7 +393,6 @@ function MdCompilerProvider($compileProvider) {
    * Elements which were fetched will be restored after use.
    * @param {!Object} options Options to be used for the compilation.
    * @returns {{element: !JQLite, restore: !Function}}
-   * @private
    */
   MdCompilerService.prototype._fetchContentElement = function(options) {
 
