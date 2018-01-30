f180f7277a847ea322837cc6b56ccd01280cfd79
Michael Prentice <splaktar@gmail.com>
2018-01-29 21:21:11 -0500

    docs: update to Angular 1.6.7 (#11096)

--- config/build.config.js
+++ config/build.config.js
@@ -1,7 +1,7 @@
 const pkg = require('../package.json');
 
 module.exports = {
-  ngVersion: '1.5.5',
+  ngVersion: '1.6.7',
   version: pkg.version,
   repository: pkg.repository.url
     .replace(/^git/,'https')
