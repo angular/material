bbfede89a07c6cc391f515944be4834d42dd58d4
Michael Prentice <splaktar@gmail.com>
2018-04-10 18:03:33 -0400

    fix(slider-container): numeric inputs cut off on Firefox and Edge (#11210)
    fix empty docs for md-slider-container
    remove needless flex attributes from md-slider-containers in demo
    fix missing rating in demo
    fix old material spec URL for md-slider
    
    Fixes #9559. Fixes #10909. Fixes #10288. Relates to #10148.

--- src/components/slider/demoBasicUsage/script.js
+++ src/components/slider/demoBasicUsage/script.js
@@ -1,26 +1,24 @@
-
 angular.module('sliderDemo1', ['ngMaterial'])
-  .config(function($mdIconProvider) {
-    $mdIconProvider
-      .iconSet('device', 'img/icons/sets/device-icons.svg', 24);
+  .config(function ($mdIconProvider) {
+    $mdIconProvider.iconSet('device', 'img/icons/sets/device-icons.svg', 24);
   })
-.controller('AppCtrl', function($scope) {
+  .controller('AppCtrl', function ($scope) {
 
-  $scope.color = {
-    red: Math.floor(Math.random() * 255),
-    green: Math.floor(Math.random() * 255),
-    blue: Math.floor(Math.random() * 255)
-  };
+    $scope.color = {
+      red: Math.floor(Math.random() * 255),
+      green: Math.floor(Math.random() * 255),
+      blue: Math.floor(Math.random() * 255)
+    };
 
-  $scope.rating1 = 3;
-  $scope.rating2 = 2;
-  $scope.rating3 = 4;
+    $scope.rating1 = 3;
+    $scope.rating2 = 2;
+    $scope.rating3 = 4;
 
-  $scope.disabled1 = Math.floor(Math.random() * 100);
-  $scope.disabled2 = 0;
-  $scope.disabled3 = 70;
+    $scope.disabled1 = Math.floor(Math.random() * 100);
+    $scope.disabled2 = 0;
+    $scope.disabled3 = 70;
 
-  $scope.invert = Math.floor(Math.random() * 100);
+    $scope.invert = Math.floor(Math.random() * 100);
 
-  $scope.isDisabled = true;
-});
+    $scope.isDisabled = true;
+  });
