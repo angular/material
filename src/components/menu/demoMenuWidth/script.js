c8f7b120088bd9d7f2506301bf2a28086c56e75c
Michael Prentice <splaktar@gmail.com>
2018-07-12 15:45:00 -0400

    docs(menu): fix broken link to spec
    Fixes #9959

--- src/components/menu/demoMenuWidth/script.js
+++ src/components/menu/demoMenuWidth/script.js
@@ -3,7 +3,8 @@ angular.module('menuDemoWidth', ['ngMaterial']).config(function($mdIconProvider)
     .iconSet("call", 'img/icons/sets/communication-icons.svg', 24)
     .iconSet("social", 'img/icons/sets/social-icons.svg', 24);
 }).controller('WidthDemoCtrl', function($mdDialog) {
-  var vm = this;
+  var ctrl = this;
+  ctrl.menuHref = "https://material.io/design/components/menus.html#specs";
 
   this.announceClick = function(index) {
     $mdDialog.show(
