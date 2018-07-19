c6105929b6f5d710ca74342613c51bae478678cb
Michael Prentice <splaktar@gmail.com>
2018-07-18 21:48:33 -0400

    refactor(virtual-repeat,util): lint and JSDoc cleanup

--- src/core/util/util.js
+++ src/core/util/util.js
@@ -655,8 +655,10 @@ function UtilFactory($document, $timeout, $compile, $rootScope, $$mdAnimate, $in
      * nextTick() coalesces all calls within a single frame
      * to minimize $digest thrashing
      *
-     * @param callback
-     * @param digest
+     * @param {Function} callback function to be called after the tick
+     * @param {boolean} digest true to call $rootScope.$digest() after callback
+     * @param scope scope associated with callback. If the scope is destroyed, the callback will
+     *  be skipped.
      * @returns {*}
      */
     nextTick: function(callback, digest, scope) {
