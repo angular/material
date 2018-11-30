f90715cb229e1d32041080c05d064768d3440adb
Michael Prentice <splaktar@gmail.com>
2018-11-29 19:02:00 -0500

    build: update to AngularJS 1.7.5 (#11530)
    to pick up ng-class bug fix

--- config/build.config.js
+++ config/build.config.js
@@ -1,7 +1,7 @@
 const pkg = require('../package.json');
 
 module.exports = {
-  ngVersion: '1.7.4',
+  ngVersion: '1.7.5',
   version: pkg.version,
   repository: pkg.repository.url
     .replace(/^git/,'https')
