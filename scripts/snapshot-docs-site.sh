cc9646a125ef6192b33e180df1540bcec7bf0490
Michael Prentice <splaktar@gmail.com>
2019-01-30 13:15:03 -0500

    chore(ci): use portable sed commands (#11622)
    use commands that should work with GNU and BSD sed
    add more log statements
    
    Relates to #11592.

--- scripts/snapshot-docs-site.sh
+++ scripts/snapshot-docs-site.sh
@@ -8,7 +8,7 @@ function run {
   cd ../
 
   echo "-- Building docs site release..."
-  rm -rf dist
+  rm -rf dist/
   gulp docs --release
 
   echo "-- Cloning code.material.angularjs.org..."
@@ -18,13 +18,17 @@ function run {
   echo "-- Remove previous snapshot..."
   rm -rf code.material.angularjs.org/HEAD
 
-  echo "-- Copying docs site to snapshot..."
-  sed -i "s,http://localhost:8080/angular-material,https://cdn.gitcdn.link/cdn/angular/bower-material/v$VERSION/angular-material,g" dist/docs/docs.js
-  sed -i "s,http://localhost:8080/docs.css,https://material.angularjs.org/$VERSION/docs.css,g" dist/docs/docs.js
-  sed -i "s,base href=\",base href=\"/HEAD,g" dist/docs/index.html
+  echo "-- Applying substitutions to build..."
+  sed -i.bak "s,http://localhost:8080/angular-material,https://cdn.gitcdn.link/cdn/angular/bower-material/v$VERSION/angular-material,g" dist/docs/docs.js
+  sed -i.bak "s,http://localhost:8080/docs.css,https://material.angularjs.org/$VERSION/docs.css,g" dist/docs/docs.js
+  rm dist/docs/docs.js.bak
+  sed -i.bak "s,base href=\",base href=\"/HEAD,g" dist/docs/index.html
+  rm dist/docs/index.html.bak
 
+  echo "-- Copying docs site to snapshot..."
   cp -Rf dist/docs code.material.angularjs.org/HEAD
 
+  echo "-- Configuring Git..."
   commitAuthorName=$(git --no-pager show -s --format='%an' HEAD)
   commitAuthorEmail=$(git --no-pager show -s --format='%ae' HEAD)
 
