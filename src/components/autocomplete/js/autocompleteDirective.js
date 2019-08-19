5b0c9ba9e0e5313925f6a27390559f32028fd95b
Nathanael Bice <nathanael.bice@gmail.com>
2019-08-19 14:27:02 -0400

    fix(autocomplete): Move mouse enter and leave events to container
    Move ng-mouseenter and ng-mouseleave event hooks to autocomplete container.
    This allows the events consider the scrollbar when firing in IE
    
    Fixes #11776

--- src/components/autocomplete/js/autocompleteDirective.js
+++ src/components/autocomplete/js/autocompleteDirective.js
@@ -400,8 +400,6 @@ function MdAutocomplete ($$mdSvgRegistry) {
             <ul class="md-autocomplete-suggestions"\
                 ng-class="::menuClass"\
                 id="ul-{{$mdAutocompleteCtrl.id}}"\
-                ng-mouseenter="$mdAutocompleteCtrl.listEnter()"\
-                ng-mouseleave="$mdAutocompleteCtrl.listLeave()"\
                 ng-mouseup="$mdAutocompleteCtrl.focusInput()"\
                 role="listbox">\
               <li class="md-autocomplete-suggestion" ' + getRepeatType(attr.mdMode) + ' ="item in $mdAutocompleteCtrl.matches"\
@@ -446,6 +444,8 @@ function MdAutocomplete ($$mdSvgRegistry) {
                 ng-hide="$mdAutocompleteCtrl.hidden"\
                 class="md-standard-list-container md-autocomplete-suggestions-container md-whiteframe-z1' + menuContainerClass + '"\
                 ng-class="{ \'md-not-found\': $mdAutocompleteCtrl.notFoundVisible() }"\
+                ng-mouseenter="$mdAutocompleteCtrl.listEnter()"\
+                ng-mouseleave="$mdAutocompleteCtrl.listLeave()"\
                 role="presentation">\
               <div class="md-standard-list-scroller" role="presentation">';
         }
@@ -457,6 +457,8 @@ function MdAutocomplete ($$mdSvgRegistry) {
               ng-hide="$mdAutocompleteCtrl.hidden"\
               class="md-virtual-repeat-container md-autocomplete-suggestions-container md-whiteframe-z1' + menuContainerClass + '"\
               ng-class="{ \'md-not-found\': $mdAutocompleteCtrl.notFoundVisible() }"\
+              ng-mouseenter="$mdAutocompleteCtrl.listEnter()"\
+              ng-mouseleave="$mdAutocompleteCtrl.listLeave()"\
               role="presentation">';
       }
 
