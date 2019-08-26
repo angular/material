838ae3416affc23dba5ee0b6eb9e3ffdc55e87a2
Michael Prentice <splaktar@gmail.com>
2019-08-26 19:12:12 -0400

    chore: update to AngularJS 1.7.8 for docs (#11783)

--- config/build.config.js
+++ config/build.config.js
@@ -1,7 +1,7 @@
 const pkg = require('../package.json');
 
 module.exports = {
-  ngVersion: '1.7.7',
+  ngVersion: '1.7.8',
   version: pkg.version,
   repository: pkg.repository.url
     .replace(/^git/,'https')
