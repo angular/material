1e1298ab846dfc256303cb99f6d392b01ce7aa7b
Michael Prentice <splaktar@gmail.com>
2020-04-11 19:20:22 -0400

    build(docs): speed up rebuilds in development mode
    - don't hit the GitHub API for development mode rebuilds

--- gulp/tasks/docs.js
+++ gulp/tasks/docs.js
@@ -1,6 +1,12 @@
 const gulp = require('gulp');
 const connect = require('gulp-connect');
+const constants = require('../const');
+const IS_DEV = constants.IS_DEV;
 
-exports.dependencies = ['docs-js', 'docs-css', 'docs-demo-scripts', 'build-contributors'];
+if (IS_DEV) {
+  exports.dependencies = ['docs-js', 'docs-css', 'docs-demo-scripts'];
+} else {
+  exports.dependencies = ['docs-js', 'docs-css', 'docs-demo-scripts', 'build-contributors'];
+}
 
 exports.task = function () { gulp.src('.').pipe(connect.reload()); };
