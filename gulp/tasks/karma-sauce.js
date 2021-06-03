4d75a8caabfc0bc02268b46dfbd92f0f114b95d1
Michael Prentice <splaktar@gmail.com>
2021-06-03 15:10:20 -0400

    build: update to Karma v6 and fix security advisories
    - use new Karma APIs
    - remove `socket.io` resolution to fix
      `Can not resolve circular dependency! (Resolving: socketServer -> socketServer)`
    - from: found 79 vulnerabilities (7 low, 59 moderate, 12 high, 1 critical)
    - to: found 20 vulnerabilities (7 low, 5 moderate, 8 high)
    
    Closes #12097. Closes #12101. Closes #12102.

--- gulp/tasks/karma-sauce.js
+++ gulp/tasks/karma-sauce.js
@@ -1,10 +1,22 @@
-const Server = require('karma').Server;
 const ROOT = require('../const').ROOT;
+const karma = require('karma');
+const parseConfig = karma.config.parseConfig;
+const Server = karma.Server;
+const karmaOptions = {
+  logLevel: 'warn'
+};
 
 exports.task = function(done) {
-  const srv = new Server({
-    logLevel: 'warn',
-    configFile: ROOT + '/config/karma-sauce.conf.js'
-  }, done);
-  srv.start();
+  parseConfig(ROOT + '/config/karma-sauce.conf.js', karmaOptions, {
+    promiseConfig: true,
+    throwErrors: true
+  }).then(parsedKarmaConfig => {
+    const server = new Server(parsedKarmaConfig, function(exitCode) {
+      // Immediately exit the process if Karma reported errors, because due to
+      // potential still running tunnel-browsers gulp won't exit properly.
+      // eslint-disable-next-line no-process-exit
+      exitCode === 0 ? done() : process.exit(exitCode);
+    });
+    server.start();
+  });
 };
