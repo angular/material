187226ebf20856e2205e8204724389ce6952bc86
bjarkler <bjarki@google.com>
2021-10-28 19:06:08 +0000

    fix(all): fix Trusted Types violations during initialization (#12128)
    Currently, when Angular Material is loaded in an application that has
    Trusted Types enforced, two violations occur. Both violations are caused
    when a plain string is passed to angular.element since there is no
    guarantee that the string was not derived from user input, which could
    cause XSS. It should be noted that, in this case, neither call to
    angular.element represents a security vulnerability, but this blocks
    Trusted Types adoption in applications that load Angular Material.
    
    To fix the violations, refactor the calls to angular.element to use safe
    DOM operations instead.
    
    This change does not alter any functionality and is fully backwards
    compatible.
    
    (cherry picked from commit 4e354a6ef15362920f23167194e52fb40cb70ea9)

--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -933,8 +933,7 @@ angular
 
 var MD_PANEL_Z_INDEX = 80;
 var MD_PANEL_HIDDEN = '_md-panel-hidden';
-var FOCUS_TRAP_TEMPLATE = angular.element(
-    '<div class="_md-panel-focus-trap" tabindex="0"></div>');
+var FOCUS_TRAP_TEMPLATE;
 
 var _presets = {};
 
@@ -2232,6 +2231,12 @@ MdPanelRef.prototype._configureTrapFocus = function() {
     var element = this.panelEl;
     // Set up elements before and after the panel to capture focus and
     // redirect back into the panel.
+    if (!FOCUS_TRAP_TEMPLATE) {
+      var template = document.createElement('div');
+      template.className = '_md-panel-focus-trap';
+      template.tabIndex = 0;
+      FOCUS_TRAP_TEMPLATE = angular.element(template);
+    }
     this._topFocusTrap = FOCUS_TRAP_TEMPLATE.clone()[0];
     this._bottomFocusTrap = FOCUS_TRAP_TEMPLATE.clone()[0];
 
