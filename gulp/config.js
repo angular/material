angular/material

commit b5a1a026202f9544847f2fba8e039c3abf1cdb1c

build: fix SCSS division deprecation (#12112)
- remove reference to non-existent `color-palette.scss`

gulp/config.js
@@ -95,7 +95,6 @@ module.exports = {
   ],
   themeCore: 'src/core/style/core-theme.scss',
   scssBaseFiles: [
-    'src/core/style/color-palette.scss',
     'src/core/style/_variables.scss',
     'src/components/input/_input-variables.scss',
     'src/core/style/_mixins.scss',

src/core/style/_variables.scss
@@ -1,3 +1,4 @@
+@use "sass:math";
 // Typography
 // ------------------------------
 $font-family: Roboto, 'Helvetica Neue', sans-serif !default;

src/components/bottomSheet/bottom-sheet.scss
@@ -102,7 +102,7 @@ md-bottom-sheet {
 
       /* Mixin for how many grid items to show per row */
       @mixin grid-items-per-row($num, $alignEdges: false) {
-        $width: 100% / $num;
+        $width: math.div(100%, $num);
         flex: 1 1 $width;
         max-width: $width;
 

src/components/button/button.scss
@@ -5,7 +5,7 @@ $button-fab-border-radius: 50% !default;
 $button-icon-border-radius: $button-fab-border-radius !default;
 
 $button-font-size: $body-font-size-base !default;
-$button-font-size-dense: $body-font-size-base * 13/14 !default;
+$button-font-size-dense: math.div($body-font-size-base * 13, 14) !default;
 
 $button-line-height: rem(3.60) !default;
 $button-line-height-dense: rem(3.20) !default;

src/components/card/card.scss
@@ -75,7 +75,7 @@ md-card {
   }
 
   md-card-title {
-    padding: 3 * $card-padding / 2 $card-padding $card-padding;
+    padding: 3 * $card-padding * 0.5 $card-padding $card-padding;
     display: flex;
     flex: 1 1 auto;
     flex-direction: row;
@@ -96,13 +96,13 @@ md-card {
 
       &:only-child {
         .md-subhead {
-          padding-top: 3 * $card-padding / 4;
+          padding-top: 3 * $card-padding * 0.25;
         }
       }
     }
 
     md-card-title-media {
-      margin-top: - $card-padding / 2;
+      margin-top: - $card-padding * 0.5;
 
       .md-media-sm {
         height: 80px;
@@ -145,7 +145,7 @@ md-card {
     &.layout-column {
       .md-button {
         &:not(.md-icon-button) {
-          margin: $baseline-grid / 4 0;
+          margin: $baseline-grid * 0.25 0;
 
           &:first-of-type {
             margin-top: 0;
@@ -157,8 +157,8 @@ md-card {
         }
 
         &.md-icon-button {
-          margin-top: 3 * $baseline-grid / 4;
-          margin-bottom: 3 * $baseline-grid / 4;
+          margin-top: 3 * $baseline-grid * 0.25;
+          margin-bottom: 3 * $baseline-grid * 0.25;
         }
       }
     }
@@ -184,15 +184,15 @@ md-card {
       }
 
       &.md-icon-button {
-        margin-left: 3 * $baseline-grid / 4;
-        margin-right: 3 * $baseline-grid / 4;
+        margin-left: 3 * $baseline-grid * 0.25;
+        margin-right: 3 * $baseline-grid * 0.25;
 
         &:first-of-type {
-          @include rtl-prop(margin-left, margin-right, 3 * $baseline-grid / 2, auto);
+          @include rtl-prop(margin-left, margin-right, 3 * $baseline-grid * 0.5, auto);
         }
 
         &:last-of-type {
-          @include rtl-prop(margin-right, margin-left, 3 * $baseline-grid / 2, auto);
+          @include rtl-prop(margin-right, margin-left, 3 * $baseline-grid * 0.5, auto);
         }
       }
 

src/components/chips/chips.scss
@@ -20,7 +20,7 @@ $contact-chip-name-width: rem(12) !default;
         @include rtl(float, left, right);
         img {
           height: $chip-height;
-          border-radius: $chip-height / 2;
+          border-radius: $chip-height * 0.5;
         }
       }
       .md-contact-name {
@@ -35,7 +35,7 @@ $contact-chip-name-width: rem(12) !default;
   height: ($contact-chip-suggestion-margin * 2) + $contact-chip-suggestion-image-height;
   img {
     height: $contact-chip-suggestion-image-height;
-    border-radius: $contact-chip-suggestion-image-height / 2;
+    border-radius: $contact-chip-suggestion-image-height * 0.5;
     margin-top: $contact-chip-suggestion-margin;
   }
   .md-contact-name {
@@ -87,7 +87,7 @@ md-chips {
 
   md-chip {
     cursor: default;
-    border-radius: $chip-height / 2;
+    border-radius: $chip-height * 0.5;
     display: block;
     height: $chip-height;
     line-height: $chip-height;

src/components/datepicker/datePicker.scss
@@ -65,7 +65,7 @@ md-datepicker {
       // down the error messages more than they should be.
       @include rtl(float, left, right);
       margin-top: $button-left-right-padding * -2;
-      top: $button-left-right-padding * 2 - $md-datepicker-border-bottom-gap / 2;
+      top: $button-left-right-padding * 2 - $md-datepicker-border-bottom-gap * 0.5;
     }
   }
 
@@ -76,7 +76,7 @@ md-datepicker {
   &._md-datepicker-has-calendar-icon {
     > label:not(.md-no-float):not(.md-container-ignore) {
       $width-offset: $md-datepicker-triangle-button-width * 2 + $md-datepicker-button-gap;
-      $offset: $md-datepicker-triangle-button-width / 2;
+      $offset: $md-datepicker-triangle-button-width * 0.5;
       @include rtl(right, $offset, auto);
       @include rtl(left, auto, $offset);
       width: calc(100% - #{$width-offset});
@@ -92,8 +92,8 @@ md-datepicker {
 ._md-datepicker-has-triangle-icon {
   // Leave room for the down-triangle button to "overflow" it's parent without modifying scrollLeft.
   // This prevents the element from shifting right when opening via the triangle button.
-  @include rtl-prop(padding-right, padding-left, $md-datepicker-triangle-button-width / 2, 0);
-  @include rtl-prop(margin-right, margin-left, -$md-datepicker-triangle-button-width / 2, auto);
+  @include rtl-prop(padding-right, padding-left, $md-datepicker-triangle-button-width * 0.5, 0);
+  @include rtl-prop(margin-right, margin-left, -$md-datepicker-triangle-button-width * 0.5, auto);
 }
 
 // Container for the datepicker input.
@@ -194,7 +194,7 @@ md-datepicker {
 .md-datepicker-triangle-button {
   position: absolute;
   @include rtl-prop(right, left, 0, auto);
-  bottom: -$md-date-arrow-size / 2;
+  bottom: -$md-date-arrow-size * 0.5;
 
   // TODO(jelbourn): This position isn't great on all platforms.
   @include rtl(transform, translateX(45%), translateX(-45%));

src/components/fabSpeedDial/fabSpeedDial.scss
@@ -138,7 +138,7 @@ md-fab-speed-dial {
       transition: $swift-ease-in;
 
       // Make the scale animation a bit faster since we are delaying each item
-      transition-duration: $swift-ease-in-duration / 2.1;
+      transition-duration: math.div($swift-ease-in-duration, 2.1);
     }
   }
 }

commit e164c568f92f6f844c78e4e001afdcc2e61e3259

chore(sass): fix module builds
- fixes module builds broken by PR https://github.com/angular/material/pull/12112
- break module imports out into a separate file since it behaves differently than
  Sass variables.
- add a new `hoistScssAtUseStatements()` function to deduplicates @use statements
  and move them to the top as required by Sass.
- start testing the module and closure builds for every PR and don't let breaking
  changes get to `master` before we find them.
- update CircleCI NodeJS image and comments about where to find new images
- update caniuse-lite

.circleci/config.yml
@@ -6,7 +6,7 @@
 # See http://blog.daemonl.com/2016/02/yaml.html
 # To validate changes, use an online parser, eg. http://yaml-online-parser.appspot.com/
 
-# Find the latest version here: https://github.com/CircleCI-Public/circleci-dockerfiles/tree/master/node/images
+# Find the latest version and SHA here: https://hub.docker.com/r/circleci/node/tags
 var_1: &docker_image circleci/node:14.16.1-browsers
 var_2: &cache_key angularjs-material-{{ checksum "package-lock.json" }}
 
@@ -44,7 +44,7 @@ var_7: &store_junit_test_results
     path: ./artifacts/junit
 
 # Branch filter that we can specify for jobs that should only run on publish branches. This filter
-# is used to ensure that not all upstream branches will be published as Github builds
+# is used to ensure that not all upstream branches will be published as GitHub builds
 # (e.g. revert branches, feature branches)
 var_8: &publish_branches_filter
   branches:
@@ -102,6 +102,24 @@ jobs:
       - *restore_cache
       - *npm_install
       - run: npm run build
+      - *save_cache
+
+  build_js_modules:
+    <<: *job_defaults
+    steps:
+      - checkout_and_rebase
+      - *restore_cache
+      - *npm_install
+      - run: npm run build:modules
+      - *save_cache
+
+  build_closure_modules:
+    <<: *job_defaults
+    steps:
+      - checkout_and_rebase
+      - *restore_cache
+      - *npm_install
+      - run: npm run build:closure
       - *save_cache
 
   # ------------------------------------------------------------------------------------------
@@ -180,9 +198,17 @@ workflows:
       - test_angularjs_snapshot:
           requires:
             - build
+      - build_js_modules:
+          requires:
+            - build
+      - build_closure_modules:
+          requires:
+            - build
       - update_and_snapshot_docs:
             filters: *publish_branches_filter
             requires:
               - test_angularjs_1_7
               - test_angularjs_1_8
               - test_angularjs_snapshot
+              - build_js_modules
+              - build_closure_modules

gulp/config.js
@@ -94,7 +94,9 @@ module.exports = {
     'src/core/style/_mixins.scss'
   ],
   themeCore: 'src/core/style/core-theme.scss',
+  scssModules: 'src/core/style/_modules.scss',
   scssBaseFiles: [
+    'src/core/style/_modules.scss',
     'src/core/style/_variables.scss',
     'src/components/input/_input-variables.scss',
     'src/core/style/_mixins.scss',
@@ -105,12 +107,14 @@ module.exports = {
   ],
   scssServicesLayout: 'src/core/services/layout/layout.scss',
   scssLayoutFiles: [
+    'src/core/style/_modules.scss',
     'src/core/style/_variables.scss',
     'src/core/style/_mixins.scss',
     'src/core/style/layout.scss',
     'src/core/services/layout/layout.scss'
   ],
   scssLayoutAttributeFiles: [
+    'src/core/style/_modules.scss',
     'src/core/style/_variables.scss',
     'src/core/style/_mixins.scss',
     'src/core/services/layout/layout-attributes.scss'

gulp/util.js
@@ -202,12 +202,24 @@ function buildModule(module, opts) {
       return fs.readFileSync(fileName, 'utf8').toString();
     }).join('\n');
 
+    let sassModules;
+    // Don't add the Sass modules to core since they get automatically included already.
+    if (name === 'core') {
+      sassModules = '';
+    } else {
+      sassModules = fs.readFileSync(config.scssModules, 'utf8').toString();
+    }
+
     return lazypipe()
     .pipe(insert.prepend, baseStyles)
     .pipe(gulpif, /theme.scss/, rename(name + '-default-theme.scss'), concat(name + '.scss'))
     // Theme files are suffixed with the `default-theme.scss` string.
     // In some cases there are multiple theme SCSS files, which should be concatenated together.
     .pipe(gulpif, /default-theme.scss/, concat(name + '-default-theme.scss'))
+    // We can't prepend these earlier, or they get duplicated in a way that hoistScssAtUseStatements
+    // can't deduplicate them.
+    .pipe(insert.prepend, sassModules)
+    .pipe(utils.hoistScssAtUseStatements)
     .pipe(sass)
     .pipe(dedupeCss)
     .pipe(utils.autoprefix)
@@ -246,7 +258,7 @@ function filterNonCodeFiles() {
   });
 }
 
-// builds the theming related css and provides it as a JS const for angular
+// builds the theming related css and provides it as a JS const for AngularJS
 function themeBuildStream() {
   // Make a copy so that we don't modify the actual config that is used by other functions
   const paths = config.themeBaseFiles.slice(0);

scripts/gulp-utils.js
@@ -289,6 +289,45 @@ exports.hoistScssVariables = function() {
   });
 };
 
+/**
+ * Find Sass @use module imports and ensure that they are at the very top of the Sass prior to
+ * running it through the Sass compiler. This also deduplicates @use statements to avoid errors.
+ */
+exports.hoistScssAtUseStatements = function() {
+  return through2.obj(function(file, enc, next) {
+    const contents = file.contents.toString().split('\n');
+    let lastAtUseLineNumber = -1;
+    const atUseStatements = [];
+
+    let openCount = 0;
+    let closeCount = 0;
+    let openBlock = false;
+
+    for (let currentLineNumber = 0; currentLineNumber < contents.length; ++currentLineNumber) {
+      const line = contents[currentLineNumber];
+
+      if (openBlock || /^\s*@use\s/.test(line) && !/^\s+/.test(line)) {
+        openCount += (line.match(/\(/g) || []).length;
+        closeCount += (line.match(/\)/g) || []).length;
+        openBlock = openCount !== closeCount;
+        // Don't move statements from line 0 to line 0.
+        if (currentLineNumber > 0) {
+          const atUseStatement = contents.splice(currentLineNumber, 1)[0];
+          // Don't write duplicate @use statements to avoid
+          // 'There's already a module with namespace x' errors.
+          if (!atUseStatements.includes(atUseStatement)) {
+            atUseStatements.push(atUseStatement);
+            contents.splice(++lastAtUseLineNumber, 0, atUseStatement);
+          }
+        }
+      }
+    }
+    file.contents = Buffer.from(contents.join('\n'));
+    this.push(file);
+    next();
+  });
+};
+
 exports.cssToNgConstant = function(ngModule, factoryName) {
   return through2.obj(function(file, enc, next) {
 

src/core/style/_modules.scss
@@ -1,0 +1,1 @@
+@use "sass:math";

src/core/style/_variables.scss
@@ -1,4 +1,3 @@
-@use "sass:math";
 // Typography
 // ------------------------------
 $font-family: Roboto, 'Helvetica Neue', sans-serif !default;